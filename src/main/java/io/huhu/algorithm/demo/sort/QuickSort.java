3ff494a971c342bb559faeaf053a0103764be571
chen <18551681083@163.com>
2023-01-04 13:26:57 +0800

    实现快速排序算法

---
+++ src/main/java/io/huhu/algorithm/demo/sort/QuickSort.java
@@ -0,0 +1,32 @@
+package io.huhu.algorithm.demo.sort;
+
+import java.util.Arrays;
+
+/**
+ * 快速排序
+ */
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] a = {8, 10, 2, 3, 6, 1, 5};
+        sort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void sort(int[] arr, int l, int r) {
+        if (l >= r) return;
+        int left = l;
+        int right = r;
+        int pivot = arr[left];
+        while (left < right) {
+            while (left < right && arr[right] >= pivot) right--;
+            if (left < right) arr[left] = arr[right];
+            while (left < right && arr[left] <= pivot) left++;
+            if (left < right) arr[right] = arr[left];
+            if (left >= right) arr[left] = pivot;
+        }
+        sort(arr, l, right - 1);
+        sort(arr, right + 1, r);
+    }
+
+}

4fadd870c6eea3dc4ca10cabe2da2acb619a2530
qchen71 <qchen71@ford.com>
2023-01-04 15:06:42 +0800

    修改选择排序

--- src/main/java/io/huhu/algorithm/demo/sort/SelectionSort.java
+++ src/main/java/io/huhu/algorithm/demo/sort/SelectionSort.java
@@ -9,27 +9,30 @@ public class SelectionSort {
 
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 3, 2, 1};
-        sort(arr);
+        sort(arr, arr.length);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void sort(int... arr) {
-        if (arr.length <= 1) {
-            return;
-        }
-        // 循环n-1次
-        for (int i = 0; i < arr.length - 1; i++) {
-            int minPos = i;
-            // 查找min
-            for (int j = i; j < arr.length; j++) {
-                if (arr[j] < arr[minPos]) {
-                    minPos = j;
+    /**
+     * 从未排序的区间中找到最小的元素
+     * 放到未排序区间的头部
+     */
+    public static void sort(int[] a, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            // 找到未排序区间最小值的下标
+            int m = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[m]) {
+                    m = j;
                 }
             }
-            // 交换元素
-            int tmp = arr[i];
-            arr[i] = arr[minPos];
-            arr[minPos] = tmp;
+            // 将未排序区间的最小值
+            // 和未排序区间的第一个元素交换
+            if (i != m) {
+                int t = a[i];
+                a[i] = a[m];
+                a[m] = t;
+            }
         }
     }
 

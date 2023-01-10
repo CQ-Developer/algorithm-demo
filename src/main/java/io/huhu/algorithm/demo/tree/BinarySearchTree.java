74b3208aa759f2285833fee979ad579de097ecea
chen <18551681083@163.com>
2023-01-10 20:42:43 +0800

    实现二叉查找树 1.插入操作实现

--- src/main/java/io/huhu/algorithm/demo/tree/BinarySearchTree.java
+++ src/main/java/io/huhu/algorithm/demo/tree/BinarySearchTree.java
@@ -24,6 +24,34 @@ public class BinarySearchTree {
         return null;
     }
 
+    /**
+     * 插入代码实现
+     */
+    public void insert(int data) {
+        // 初始化一棵树
+        if (tree == null) {
+            tree = new Node(data);
+            return;
+        }
+        // 树已经初始化过
+        Node p = tree;
+        while (p != null) {
+            if (data > p.data) {
+                if (p.right == null) {
+                    p.right = new Node(data);
+                    return;
+                }
+                p = p.right;
+            } else {
+                if (p.left == null) {
+                    p.left = new Node(data);
+                    return;
+                }
+                p = p.left;
+            }
+        }
+    }
+
     public static class Node {
 
         private int data;

c26b11208f34a4db21d2ff07dab0f87cd6986e30
chen <18551681083@163.com>
2023-01-16 10:26:08 +0800

    定义并查集

---
+++ src/main/java/io/huhu/algorithm/demo/union/LinkedUnionFindSet.java
@@ -0,0 +1,21 @@
+package io.huhu.algorithm.demo.union;
+
+/**
+ * 并查集
+ */
+public class LinkedUnionFindSet {
+
+    /**
+     * 将对象i和对象j所属的集合合并在一起
+     */
+    void union(int x, int j) {
+    }
+
+    /**
+     * 查询对象i和对象j是否属于同一个集合
+     */
+    boolean find(int i, int j) {
+        return false;
+    }
+
+}

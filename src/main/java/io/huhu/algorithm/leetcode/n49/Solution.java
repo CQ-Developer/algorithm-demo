e4798e13c562b72771b26d87ff465e037973262a
chen <18551681083@163.com>
2023-09-06 09:24:28 +0800

    49.字母异位词分组 完成

--- src/main/java/io/huhu/algorithm/leetcode/n49/Solution.java
+++ src/main/java/io/huhu/algorithm/leetcode/n49/Solution.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * <h1>49.字母异位词分组</h1>
@@ -53,63 +55,30 @@ import java.util.Map;
  */
 class Solution {
 
+    /**
+     * <h2>使用排序进行分组</h2>
+     */
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        boolean[] used = new boolean[strs.length];
-        for (int i = 0; i < strs.length; i++) {
-            if (!used[i]) {
-                List<String> list = new ArrayList<>();
-                list.add(strs[i]);
-                used[i] = true;
-                for (int j = i + 1; j < strs.length; j++) {
-                    if (!used[j] && strEquals2(strs[i], strs[j])) {
-                        list.add(strs[j]);
-                        used[j] = true;
-                    }
-                }
-                result.add(list);
-            }
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
         }
-        return result;
+        return new ArrayList<>(map.values());
     }
 
-    // 这种比较方法有些问题 比如 duh ill
-    private int sum(String str) {
-        int sum = 0;
-        for (int i = 0; i < str.length(); i++) {
-            int c = str.charAt(i) - '0';
-            sum += c;
-        }
-        return sum;
-    }
-
-    // 超时
-    private boolean strEquals1(String str1, String str2) {
-        char[] arr1 = str1.toCharArray();
-        Arrays.sort(arr1);
-        char[] arr2 = str2.toCharArray();
-        Arrays.sort(arr2);
-        return Arrays.equals(arr1, arr2);
-    }
-
-    // 超时
-    private boolean strEquals2(String str1, String str2) {
-        Map<Character, Integer> map1 = countCharacter(str1);
-        Map<Character, Integer> map2 = countCharacter(str2);
-        return map1.equals(map2);
-    }
-
-    private Map<Character, Integer> countCharacter(String str) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < str.length(); i++) {
-            map.compute(str.charAt(i), (k, v) -> v == null ? 1 : v + 1);
-        }
-        return map;
-    }
-
-    // todo
-    private boolean strEquals3(String str1, String str2) {
-        return false;
+    /**
+     * <h2>使用 {@link java.util.stream.Stream 流} 解决问题</h2>
+     * <p>这种写法更加优雅，但是性能较差</p>
+     */
+    public List<List<String>> groupAnagrams1(String[] strs) {
+        Map<String, List<String>> map = Stream.of(strs).collect(Collectors.groupingBy(
+                str -> str.chars().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString()));
+        return new ArrayList<>(map.values());
     }
 
 }

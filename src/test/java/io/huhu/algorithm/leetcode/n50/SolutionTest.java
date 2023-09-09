afe09c066907262808826686c530ed6809a95c90
chen <18551681083@163.com>
2023-09-09 22:45:02 +0800

    引入单元测试

---
+++ src/test/java/io/huhu/algorithm/leetcode/n50/SolutionTest.java
@@ -0,0 +1,23 @@
+package io.huhu.algorithm.leetcode.n50;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @Test
+    void myPow() {
+        double result = solution.myPow(2, 3);
+        assertEquals(8, result);
+    }
+
+    @Test
+    void myPow1() {
+        double result = solution.myPow(3, 2);
+        assertEquals(9, result);
+    }
+
+}

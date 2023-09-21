90f7fab514c98c3fd4bc88e79a85553e8ab81000
chen <18551681083@163.com>
2023-09-21 16:53:36 +0800

    优化选择排序算法

--- src/main/java/io/huhu/algorithm/gokking/algorithms/SelectionSort.java
+++ src/main/java/io/huhu/algorithm/gokking/algorithms/SelectionSort.java
@@ -9,30 +9,24 @@ public final class SelectionSort {
      * 时间复杂度<b>O(n<sup>2</sup>)</b>
      *
      * @param arr 数组
-     * @return 升序排列后的数组
      */
-    public static int[] sort(int[] arr) {
+    public static void sort(int[] arr) {
         int len = arr == null ? 0 : arr.length;
-        int[] sorted = new int[len];
-        boolean[] used = new boolean[len];
         for (int i = 0; i < len; i++) {
-            int s = findSmallest(arr, used);
-            used[s] = true;
-            sorted[i] = arr[s];
-        }
-        return sorted;
-    }
-
-    private static int findSmallest(int[] arr, boolean[] used) {
-        int smallest = arr[0];
-        int s = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (!used[i] && arr[i] < smallest) {
-                smallest = arr[i];
-                s = i;
+            // 找到当前元素后方的最小元素
+            int m = i;
+            for (int j = i + 1; j < len; j++) {
+                if (arr[j] < arr[m]) {
+                    m = j;
+                }
+            }
+            // 交换当前元素和后方的最小元素
+            if (i != m) {
+                int t = arr[i];
+                arr[i] = arr[m];
+                arr[m] = t;
             }
         }
-        return s;
     }
 
 }
--- src/test/java/io/huhu/algorithm/gokking/algorithms/SelectionSortTest.java
+++ src/test/java/io/huhu/algorithm/gokking/algorithms/SelectionSortTest.java
@@ -5,34 +5,34 @@ import org.junit.jupiter.api.Test;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SelectionSortTest {
 
     @Test
     void sort_1() {
-        int[] arr = SelectionSort.sort(null);
-        assertArrayEquals(new int[0], arr);
+        assertDoesNotThrow(() -> SelectionSort.sort(null));
     }
 
     @Test
     void sort_2() {
         int[] arr = {};
-        arr = SelectionSort.sort(arr);
+        SelectionSort.sort(arr);
         assertArrayEquals(new int[0], arr);
     }
 
     @Test
     void sort_3() {
         int[] arr = {1};
-        arr = SelectionSort.sort(arr);
+        SelectionSort.sort(arr);
         assertArrayEquals(new int[]{1}, arr);
     }
 
     @Test
     void sort_4() {
         int[] arr = {5, 3, 1, 2, 4};
-        arr = SelectionSort.sort(arr);
+        SelectionSort.sort(arr);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5}, arr);
     }
 
@@ -44,7 +44,7 @@ class SelectionSortTest {
         for (int i = 0; i < n; i++) {
             arr[i] = random.nextInt(n);
         }
-        arr = SelectionSort.sort(arr);
+        SelectionSort.sort(arr);
         for (int i = 0; i < arr.length - 1; i++) {
             assertTrue(arr[i] <= arr[i + 1]);
         }

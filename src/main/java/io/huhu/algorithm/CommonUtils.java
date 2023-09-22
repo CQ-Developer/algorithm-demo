5d5f116c4eb38718c7fc597a63a2ef5302bfbd00
chen <18551681083@163.com>
2023-09-22 11:04:11 +0800

    添加注释

--- src/main/java/io/huhu/algorithm/CommonUtils.java
+++ src/main/java/io/huhu/algorithm/CommonUtils.java
@@ -4,6 +4,11 @@ public final class CommonUtils {
 
     private CommonUtils() {}
 
+    /**
+     * 生成小写字符a到z的数组
+     *
+     * @return a-z字符数组
+     */
     public static char[] sequence_az() {
         char[] az = new char['z' - 'a' + 1];
         for (char c = 'a', i = 0; c <= 'z'; c++) {
@@ -12,6 +17,11 @@ public final class CommonUtils {
         return az;
     }
 
+    /**
+     * 生成大写字符A到Z的数组
+     *
+     * @return A-Z字符数组
+     */
     public static char[] sequence_AZ() {
         char[] AZ = new char['Z' - 'A' + 1];
         for (char c = 'A', i = 0; c <= 'Z'; c++) {
@@ -20,6 +30,11 @@ public final class CommonUtils {
         return AZ;
     }
 
+    /**
+     * 生成数组0到9的数组
+     *
+     * @return 0-9整数数组
+     */
     public static int[] sequence_09() {
         int[] arr = new int[10];
         for (int i = 1; i < 10; i++) {

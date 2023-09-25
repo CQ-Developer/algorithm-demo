27879fa6fafa3b45aace36d2563fcaa26d1fafaa
chen <18551681083@163.com>
2023-09-25 09:44:53 +0800

    补全测试用例

--- src/test/java/io/huhu/gokking/algorithms/BinarySearchTest.java
+++ src/test/java/io/huhu/gokking/algorithms/BinarySearchTest.java
@@ -2,46 +2,51 @@ package io.huhu.gokking.algorithms;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BinarySearchTest {
 
     @Test
     void search_1() {
-        int[] arr = {1, 2, 3, 4, 5};
-        assertEquals(1, BinarySearch.search(arr, 2));
+        assertDoesNotThrow(() -> BinarySearch.search(null, 11));
+        assertEquals(-1, BinarySearch.search(null, 11));
     }
 
     @Test
     void search_2() {
-        int[] arr = {1};
-        assertEquals(0, BinarySearch.search(arr, 1));
+        int[] arr = {};
+        assertDoesNotThrow(() -> BinarySearch.search(arr, 23));
+        assertEquals(-1, BinarySearch.search(arr, 23));
     }
 
     @Test
     void search_3() {
         int[] arr = {1};
-        assertEquals(-1, BinarySearch.search(arr, 2));
+        assertEquals(-1, BinarySearch.search(arr, 18));
+        assertEquals(0, BinarySearch.search(arr, 1));
     }
 
     @Test
     void search_4() {
-        int[] arr = {};
-        assertEquals(-1, BinarySearch.search(arr, 0));
+        int[] arr = {1, 2, 3, 4, 5};
+        assertEquals(-1, BinarySearch.search(arr, 6));
+        assertEquals(1, BinarySearch.search(arr, 2));
     }
 
     @Test
     void search_5() {
-        assertEquals(-1, BinarySearch.search(null, 5));
-    }
-
-    @Test
-    void search_6() {
-        int[] arr = new int[1000000];
+        int[] arr = new int[1000];
+        var rnd = new Random(System.currentTimeMillis());
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
+            arr[i] = rnd.nextInt(10000);
         }
-        assertEquals(999999, BinarySearch.search(arr, 1000000));
+        Arrays.sort(arr);
+        assertEquals(-1, BinarySearch.search(arr, 10001));
+        assertEquals(555, BinarySearch.search(arr, arr[555]));
     }
 
 }

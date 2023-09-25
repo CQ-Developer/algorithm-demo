3a8eb8d542436410280f13eda40c6e63bbcf480a
chen <18551681083@163.com>
2023-09-25 13:17:32 +0800

    优化代码

--- src/main/java/io/huhu/gokking/algorithms/DijkstraSearch.java
+++ src/main/java/io/huhu/gokking/algorithms/DijkstraSearch.java
@@ -1,10 +1,8 @@
 package io.huhu.gokking.algorithms;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.StringJoiner;
 
 import static java.lang.Integer.MAX_VALUE;
 
@@ -12,27 +10,7 @@ public final class DijkstraSearch {
 
     private DijkstraSearch() {}
 
-    /**
-     * <img src="./doc-files/DijkstraSearch.png">
-     */
-    public static int search() {
-        // 初始化节点的邻居关系
-        Map<String, Map<String, Integer>> graph = new HashMap<>();
-        graph.put("start", Map.of("a", 6, "b", 2));
-        graph.put("a", Map.of("end", 1));
-        graph.put("b", Map.of("a", 3, "end", 5));
-        graph.put("end", new HashMap<>());
-        // 初始化节点的消耗
-        Map<String, Integer> costs = new HashMap<>();
-        costs.put("a", 6);
-        costs.put("b", 2);
-        costs.put("end", MAX_VALUE);
-        // 初始化父节点
-        Map<String, String> parents = new HashMap<>();
-        parents.put("a", "start");
-        parents.put("b", "start");
-        parents.put("end", null);
-        // 初始化已处理过的节点
+    public static int search(Map<String, Map<String, Integer>> graph, Map<String, Integer> costs, Map<String, String> parents) {
         Set<String> processed = new HashSet<>();
         // 开始算法流程
         String node = findLowestCostNode(costs, processed);
@@ -49,19 +27,7 @@ public final class DijkstraSearch {
             processed.add(node);
             node = findLowestCostNode(costs, processed);
         }
-        printRoad(parents);
         return costs.get("end");
-    }
-
-    private static void printRoad(Map<String, String> parents) {
-        StringJoiner joiner = new StringJoiner(" <- ");
-        joiner.add("end");
-        String node = parents.get("end");
-        while (node != null) {
-            joiner.add(node);
-            node = parents.get(node);
-        }
-        System.out.println(joiner);
     }
 
     static String findLowestCostNode(Map<String, Integer> costs, Set<String> processed) {
--- src/test/java/io/huhu/gokking/algorithms/DijkstraSearchTest.java
+++ src/test/java/io/huhu/gokking/algorithms/DijkstraSearchTest.java
@@ -2,13 +2,37 @@ package io.huhu.gokking.algorithms;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Integer.MAX_VALUE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DijkstraSearchTest {
 
+    /**
+     * <img src="./doc-files/DijkstraSearch.png">
+     */
     @Test
     void search() {
-        assertEquals(6, DijkstraSearch.search());
+        // 初始化节点的邻居关系
+        Map<String, Map<String, Integer>> graph = new HashMap<>();
+        graph.put("start", Map.of("a", 6, "b", 2));
+        graph.put("a", Map.of("end", 1));
+        graph.put("b", Map.of("a", 3, "end", 5));
+        graph.put("end", new HashMap<>());
+        // 初始化节点的消耗
+        Map<String, Integer> costs = new HashMap<>();
+        costs.put("a", 6);
+        costs.put("b", 2);
+        costs.put("end", MAX_VALUE);
+        // 初始化父节点
+        Map<String, String> parents = new HashMap<>();
+        parents.put("a", "start");
+        parents.put("b", "start");
+        parents.put("end", null);
+        // 初始化已处理过的节点
+        assertEquals(6, DijkstraSearch.search(graph, costs, parents));
     }
 
 }

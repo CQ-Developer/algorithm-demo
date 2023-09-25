4e51e95f060f1180632709554270fedd54ea0b73
chen <18551681083@163.com>
2023-09-25 13:18:32 +0800

    优化代码

--- src/main/java/io/huhu/gokking/algorithms/BreadthFirstSearch.java
+++ src/main/java/io/huhu/gokking/algorithms/BreadthFirstSearch.java
@@ -1,29 +1,18 @@
 package io.huhu.gokking.algorithms;
 
 import java.util.Deque;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public final class BreadthFirstSearch {
 
     private BreadthFirstSearch() {}
 
-    public static boolean search() {
-        // 初始化图
-        var map = HashMap.<String, List<String>>newHashMap(8);
-        map.put("you", List.of("alice", "bob", "claire"));
-        map.put("bob", List.of("anuj", "peggy"));
-        map.put("alice", List.of("peggy"));
-        map.put("claire", List.of("thom", "jonny"));
-        map.put("anuj", List.of());
-        map.put("peggy", List.of());
-        map.put("thom", List.of());
-        map.put("jonny", List.of());
-        // 初始化队列
-        Deque<String> searchQueue = new LinkedList<>(map.get("you"));
+    public static boolean search(Map<String, List<String>> graph) {
+        Deque<String> searchQueue = new LinkedList<>(graph.get("you"));
         Set<String> searched = new HashSet<>();
         // 深度有限搜索
         while (!searchQueue.isEmpty()) {
@@ -34,7 +23,7 @@ public final class BreadthFirstSearch {
                     return true;
                 }
                 else {
-                    searchQueue.addAll(map.get(name));
+                    searchQueue.addAll(graph.get(name));
                 }
             }
         }
--- src/test/java/io/huhu/gokking/algorithms/BreadthFirstSearchTest.java
+++ src/test/java/io/huhu/gokking/algorithms/BreadthFirstSearchTest.java
@@ -2,13 +2,26 @@ package io.huhu.gokking.algorithms;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BreadthFirstSearchTest {
 
     @Test
     void search() {
-        assertTrue(BreadthFirstSearch.search());
+        Map<String, List<String>> graph = new HashMap<>();
+        graph.put("you", List.of("alice", "bob", "claire"));
+        graph.put("bob", List.of("anuj", "peggy"));
+        graph.put("alice", List.of("peggy"));
+        graph.put("claire", List.of("thom", "jonny"));
+        graph.put("anuj", List.of());
+        graph.put("peggy", List.of());
+        graph.put("thom", List.of());
+        graph.put("jonny", List.of());
+        assertTrue(BreadthFirstSearch.search(graph));
     }
 
 }

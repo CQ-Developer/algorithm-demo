422230c99e8db9421b34dd9bb770de774976c401
chen <18551681083@163.com>
2023-09-25 14:35:40 +0800

    狄克斯特拉算法 添加测试用例1 添加测试用例2

--- src/test/java/io/huhu/gokking/algorithms/DijkstraSearchTest.java
+++ src/test/java/io/huhu/gokking/algorithms/DijkstraSearchTest.java
@@ -14,7 +14,7 @@ class DijkstraSearchTest {
      * <img src="./doc-files/DijkstraSearch.png">
      */
     @Test
-    void search() {
+    void search_1() {
         // 初始化节点的邻居关系
         Map<String, Map<String, Integer>> graph = new HashMap<>();
         graph.put("start", Map.of("a", 6, "b", 2));
@@ -33,6 +33,51 @@ class DijkstraSearchTest {
         parents.put("end", null);
         // 初始化已处理过的节点
         assertEquals(6, DijkstraSearch.search(graph, costs, parents));
+    }
+
+    /**
+     * <img src="./doc-files/DijkstraSearch-1.png">
+     */
+    @Test
+    void search_2() {
+        Map<String, Map<String, Integer>> graph = new HashMap<>();
+        graph.put("start", Map.of("a", 5, "b", 2));
+        graph.put("a", Map.of("c", 4, "d", 2));
+        graph.put("b", Map.of("a", 8, "d", 7));
+        graph.put("c", Map.of("d", 6, "end", 3));
+        graph.put("d", Map.of("end", 1));
+        graph.put("end", Map.of());
+        Map<String, Integer> costs = new HashMap<>();
+        costs.put("a", 5);
+        costs.put("b", 2);
+        costs.put("c", MAX_VALUE);
+        costs.put("d", MAX_VALUE);
+        costs.put("end", MAX_VALUE);
+        Map<String, String> parents = new HashMap<>();
+        parents.put("a", "start");
+        parents.put("b", "start");
+        assertEquals(8, DijkstraSearch.search(graph, costs, parents));
+    }
+
+    /**
+     * <img src="./doc-files/DijkstraSearch-2.png">
+     */
+    @Test
+    void search_3() {
+        Map<String, Map<String, Integer>> graph = new HashMap<>();
+        graph.put("start", Map.of("a", 10));
+        graph.put("a", Map.of("b", 20));
+        graph.put("b", Map.of("c", 1, "end", 30));
+        graph.put("c", Map.of("a", 1));
+        graph.put("end", Map.of());
+        Map<String, Integer> costs = new HashMap<>();
+        costs.put("a", 10);
+        costs.put("b", MAX_VALUE);
+        costs.put("c", MAX_VALUE);
+        costs.put("end", MAX_VALUE);
+        Map<String, String> parents = new HashMap<>();
+        parents.put("a", "start");
+        assertEquals(60, DijkstraSearch.search(graph, costs, parents));
     }
 
 }

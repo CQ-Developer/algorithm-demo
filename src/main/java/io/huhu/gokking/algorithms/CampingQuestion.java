1855139e401039d79fce688e5649c6b9b5e135be
chen <18551681083@163.com>
2023-09-27 10:20:56 +0800

    [待完成] 动态规划

---
+++ src/main/java/io/huhu/gokking/algorithms/CampingQuestion.java
@@ -0,0 +1,20 @@
+package io.huhu.gokking.algorithms;
+
+/**
+ * 假设你要去露营。<br>
+ * 你有一个容量为6磅的背包，需要决定携带下面的哪些东西。<br>
+ * 其中每样东西都有相应的价值，价值越大意味着约重要。<br>
+ * 1.水：重3磅，价值10<br>
+ * 2.书：重1磅，价值3<br>
+ * 3.食物：重2磅，价值9<br>
+ * 4.夹克：重2磅，价值5<br>
+ * 5.相机：重1磅，价值6<br>
+ * 请问携带哪些东西时价值最高？
+ */
+public class CampingQuestion {
+
+    public void camping() {
+        // todo 动态规划
+    }
+
+}

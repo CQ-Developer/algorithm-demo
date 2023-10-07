95c0238185faf68132845b2baf1e6acf0ada7322
chen <18551681083@163.com>
2023-10-07 14:50:45 +0800

    [leetcode] 78.子集 添加穷举解法

--- src/main/java/io/huhu/leetcode/n78/Solution.java
+++ src/main/java/io/huhu/leetcode/n78/Solution.java
@@ -41,6 +41,24 @@ import java.util.List;
  */
 class Solution {
 
+    /**
+     * 穷举非常符合直觉：
+     * <pre>{@code
+     * public List<List<Integer>> subsets(int[] nums) {
+     *     List<List<Integer>> result = new ArrayList<>();
+     *     result.add(new ArrayList<>());
+     *     for (int num : nums) {
+     *         int size = result.size();
+     *         for (int i = 0; i < size; i++) {
+     *             List<Integer> sub = new ArrayList<>(result.get(i));
+     *             sub.add(num);
+     *             result.add(sub);
+     *         }
+     *     }
+     *     return result;
+     * }
+     * }</pre>
+     */
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         result.add(List.of());

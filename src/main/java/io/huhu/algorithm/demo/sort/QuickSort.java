934e448d8e4aef483fa5676d3938815d8cd34911
chen <18551681083@163.com>
2023-10-09 10:44:21 +0800

    格式化代码

--- src/main/java/io/huhu/algorithm/demo/sort/QuickSort.java
+++ src/main/java/io/huhu/algorithm/demo/sort/QuickSort.java
@@ -1,7 +1,8 @@
 package io.huhu.algorithm.demo.sort;
 
+import io.huhu.CommonUtils;
+
 import java.util.Arrays;
-import java.util.Random;
 
 /**
  * 快速排序
@@ -9,42 +10,37 @@ import java.util.Random;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] arr = new int[30];
-        var rnd = new Random(System.currentTimeMillis());
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = rnd.nextInt(1000);
-        }
+        int[] arr = CommonUtils.generateArray(20);
         System.out.println(Arrays.toString(arr));
         sort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void sort(int[] arr, int start, int end) {
-        if (start >= end) {
-            return;
+    public static void sort(int[] arr, int s, int e) {
+        if (s < e) {
+            int l = s;
+            int num = arr[l];
+            int r = e;
+            while (l < r) {
+                while (l < r && arr[r] >= num) {
+                    r--;
+                }
+                if (l < r) {
+                    arr[l] = arr[r];
+                }
+                while (l < r && arr[l] <= num) {
+                    l++;
+                }
+                if (l < r) {
+                    arr[r] = arr[l];
+                }
+                if (l >= r) {
+                    arr[l] = num;
+                }
+            }
+            sort(arr, s, r - 1);
+            sort(arr, r + 1, e);
         }
-        int left = start;
-        int pivot = arr[left];
-        int right = end;
-        while (left < right) {
-            while (left < right && arr[right] >= pivot) {
-                right--;
-            }
-            if (left < right) {
-                arr[left] = arr[right];
-            }
-            while (left < right && arr[left] <= pivot) {
-                left++;
-            }
-            if (left < right) {
-                arr[right] = arr[left];
-            }
-            if (left >= right) {
-                arr[left] = pivot;
-            }
-        }
-        sort(arr, start, right - 1);
-        sort(arr, right + 1, end);
     }
 
 }

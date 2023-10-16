414a45d4dcba60d4f2c9738e76a609d28559a91b
chen <18551681083@163.com>
2023-10-16 17:23:42 +0800

    [Algorithms.fourth.edition] 定义MaxPQ优先级队列

---
+++ src/main/java/io/huhu/algorithm/fourth/Edition/MaxPQ.java
@@ -0,0 +1,53 @@
+package io.huhu.algorithm.fourth.Edition;
+
+class MaxPQ<T extends Comparable<T>> {
+
+    /**
+     * 创建一个优先队列
+     */
+    public MaxPQ() {}
+
+    /**
+     * 创建一个初始容量为max的优先队列
+     */
+    public MaxPQ(int max) {}
+
+    /**
+     * 用a中的元素创建一个优先级队列
+     */
+    public MaxPQ(T[] a) {}
+
+    /**
+     * 向优先队列中插入一个元素
+     */
+    public void insert(T t) {}
+
+    /**
+     * 返回最大元素
+     */
+    public T max() {
+        return null;
+    }
+
+    /**
+     * 删除并返回最大元素
+     */
+    public T delMax() {
+        return null;
+    }
+
+    /**
+     * 返回队列是否为空
+     */
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * 返回优先队列中的元素个数
+     */
+    public int size() {
+        return 0;
+    }
+
+}

e90952d35da3646f39b1756f3b88a587e6cf4469
chen <18551681083@163.com>
2023-10-23 16:08:02 +0800

    [leetcode] 81.搜索旋转排序数组II 二分查找算法

--- src/main/java/io/huhu/leetcode/n81/Solution.java
+++ src/main/java/io/huhu/leetcode/n81/Solution.java
@@ -19,17 +19,23 @@ class Solution {
             }
             // 数组的前半部分有序的
             if (nums[start] < nums[mid]) {
+                // target在数组的前半部分
                 if (nums[mid] > target && nums[start] <= target) {
                     end = mid - 1;
-                } else {
+                }
+                // target在数组的后半部分
+                else {
                     start = mid + 1;
                 }
             }
             // 数组的后半部分有序
             else if (nums[start] > nums[mid]) {
+                // target在数组的后半部分
                 if (nums[mid] < target && nums[end] >= target) {
                     start = mid + 1;
-                } else {
+                }
+                // target在数组的前半部分
+                else {
                     end = mid - 1;
                 }
             }

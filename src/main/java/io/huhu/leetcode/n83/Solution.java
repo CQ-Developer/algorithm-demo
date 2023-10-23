ddc41c448c3452567560b32a0d157882077da3b2
chen <18551681083@163.com>
2023-10-23 17:50:21 +0800

    [leetcode] 83.删除排序链表中的重复元素 待完成

---
+++ src/main/java/io/huhu/leetcode/n83/Solution.java
@@ -0,0 +1,19 @@
+package io.huhu.leetcode.n83;
+
+/**
+ * <h1>
+ * 83.删除排序链表中的重复元素
+ * </h1>
+ *
+ * <p>
+ * 给定一个已排序的链表的头head，删除所有重复的元素，使每个元素只出现一次，返回已排序的链表。
+ * </p>
+ */
+class Solution {
+
+    public ListNode deleteDuplicated(ListNode head) {
+        // todo
+        return head;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/n83/SolutionTest.java
@@ -0,0 +1,44 @@
+package io.huhu.leetcode.n83;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class SolutionTest {
+
+    final Solution solution = new Solution();
+
+    @Test
+    void deleteDuplicated1() {
+        var head =
+                new ListNode(1,
+                        new ListNode(1,
+                                new ListNode(2)));
+        head = solution.deleteDuplicated(head);
+        assertEquals(1, head.val);
+        head = head.next;
+        assertEquals(2, head.val);
+        head = head.next;
+        assertNull(head);
+    }
+
+    @Test
+    void deleteDuplicated2() {
+        var head =
+                new ListNode(1,
+                        new ListNode(1,
+                                new ListNode(2,
+                                        new ListNode(3,
+                                                new ListNode(3)))));
+        head = solution.deleteDuplicated(head);
+        assertEquals(1, head.val);
+        head = head.next;
+        assertEquals(2, head.val);
+        head = head.next;
+        assertEquals(3, head.val);
+        head = head.next;
+        assertNull(head);
+    }
+
+}

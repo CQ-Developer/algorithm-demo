c6b8b098fd4053f07e9c74786d9c9335e290b46b
chen <18551681083@163.com>
2023-10-24 21:07:45 +0800

    [leetcode] 86.分割链表 待完成

---
+++ src/main/java/io/huhu/leetcode/n86/Solution.java
@@ -0,0 +1,29 @@
+package io.huhu.leetcode.n86;
+
+/**
+ * <h1>
+ * 86.分割链表
+ * </h1>
+ *
+ * <p>
+ * 给你一个链表的头节点head和一个特定值x，请你对链表进行分隔，使得所有小于x的节点都出现在大于或等于x的节点之前。
+ * 你应当保留两个分区中每个节点的初始相对位置。
+ * </p>
+ *
+ * <p>
+ * 提示：
+ * <br/>
+ * 链表中节点的数目在范围[0,200]内
+ * <br/>
+ * -100 <= Node.val <= 100
+ * <br/>
+ * -200 <= x <= 200
+ * </p>
+ */
+class Solution {
+
+    public ListNode partition(ListNode head, int x) {
+        return null;
+    }
+
+}

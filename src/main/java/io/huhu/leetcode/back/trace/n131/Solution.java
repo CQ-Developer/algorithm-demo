66db4beaa4a2bcaa1dfae2743ef5796dd9a8f48e
chen <18551681083@163.com>
2023-11-11 16:22:46 +0800

    [leetcode] 113.路径总合II 回溯算法

---
+++ src/main/java/io/huhu/leetcode/back/trace/n131/Solution.java
@@ -0,0 +1,22 @@
+package io.huhu.leetcode.back.trace.n131;
+
+import java.util.List;
+
+/**
+ * <a>131.分割回文串</a>
+ * 给你一个字符串s, 请你将s分割成一些子串, 使每个子串都是回文串.
+ * 返回s所有可能的分割方案.
+ * 回文串是正着读和反着读都一样的字符串.
+ */
+class Solution {
+
+    /**
+     * 1 <= s.length <= 16
+     * s仅由小写英文字母组成
+     */
+    public List<List<String>> partition(String s) {
+        // todo 要求使用回溯算法完成
+        return null;
+    }
+
+}

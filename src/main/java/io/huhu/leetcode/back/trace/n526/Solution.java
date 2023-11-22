f206848b92762070656a62baec62bf6f4bd2f30d
chen <18551681083@163.com>
2023-11-22 22:52:58 +0800

    [leetcode] 526.优美的排列 回溯算法 保存中间状态

--- src/main/java/io/huhu/leetcode/back/trace/n526/Solution.java
+++ src/main/java/io/huhu/leetcode/back/trace/n526/Solution.java
@@ -1,5 +1,10 @@
 package io.huhu.leetcode.back.trace.n526;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 /**
  * <a href="https://leetcode.cn/problems/beautiful-arrangement/">526.优美的排列</a>
  * 假设有从1到n的n个整数. 用这些整数构造一个数组perm(下标从1开始), 只要满足下述条件之一, 该数组就是一个优美的排列:
@@ -13,17 +18,14 @@ class Solution {
      * 1 <= n <= 15
      */
     public int countArrangement(int n) {
-        int[] result = {0};
-        dfs(n, new boolean[n], 1, result);
-        return result[0];
+        List<List<Integer>> result = new ArrayList<>();
+        backTrace(n, new boolean[n], new ArrayDeque<>(), result);
+        return result.size();
     }
 
-    /**
-     * 回溯算法
-     */
-    private void dfs(int n, boolean[] used, int b, int[] result) {
-        if (b > n) {
-            result[0]++;
+    private void backTrace(int n, boolean[] used, Deque<Integer> path, List<List<Integer>> result) {
+        if (path.size() == n) {
+            result.add(new ArrayList<>(path));
             return;
         }
         for (int i = 0; i < n; i++) {
@@ -31,11 +33,14 @@ class Solution {
                 continue;
             }
             int a = i + 1;
+            int b = path.size() + 1;
             if (a % b != 0 && b % a != 0) {
                 continue;
             }
             used[i] = true;
-            dfs(n, used, b + 1, result);
+            path.addLast(a);
+            backTrace(n, used, path, result);
+            path.removeLast();
             used[i] = false;
         }
     }

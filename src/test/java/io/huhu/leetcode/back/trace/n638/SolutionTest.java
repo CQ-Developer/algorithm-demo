5f460bc87d3df4e02feacd43583ca3ee038c4b44
chen <18551681083@163.com>
2023-11-24 00:27:22 +0800

    [leetcode] 526.优美的排列 修改单元测试

--- src/test/java/io/huhu/leetcode/back/trace/n638/SolutionTest.java
+++ src/test/java/io/huhu/leetcode/back/trace/n638/SolutionTest.java
@@ -3,9 +3,12 @@ package io.huhu.leetcode.back.trace.n638;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Timeout(value = 3, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
 class SolutionTest {
 
     Solution solution;
@@ -17,26 +20,88 @@ class SolutionTest {
 
     @Test
     void shoppingOffers_1() {
-        List<Integer> price = List.of(2, 5);
-        List<List<Integer>> special = List.of(List.of(3, 0, 5), List.of(1, 2, 10));
-        List<Integer> needs = List.of(3, 2);
+        List<Integer> price = new ArrayList<>();
+        price.add(2);
+        price.add(5);
+        List<Integer> s1 = new ArrayList<>();
+        s1.add(3);
+        s1.add(0);
+        s1.add(5);
+        List<Integer> s2 = new ArrayList<>();
+        s2.add(1);
+        s2.add(2);
+        s2.add(10);
+        List<List<Integer>> special = new ArrayList<>();
+        special.add(s1);
+        special.add(s2);
+        List<Integer> needs = new ArrayList<>();
+        needs.add(3);
+        needs.add(2);
         Assertions.assertEquals(14, solution.shoppingOffers(price, special, needs));
     }
 
     @Test
     void shoppingOffers_2() {
-        List<Integer> price = List.of(2, 3, 4);
-        List<List<Integer>> special = List.of(List.of(1, 1, 0, 4), List.of(2, 2, 1, 9));
-        List<Integer> needs = List.of(1, 2, 1);
-        Assertions.assertEquals(11, solution.shoppingOffers(price, special, needs));
+        List<Integer> price = new ArrayList<>();
+        price.add(2);
+        price.add(3);
+        price.add(4);
+        List<Integer> s1 = new ArrayList<>();
+        s1.add(1);
+        s1.add(1);
+        s1.add(0);
+        s1.add(4);
+        List<Integer> s2 = new ArrayList<>();
+        s2.add(2);
+        s2.add(2);
+        s2.add(1);
+        s2.add(9);
+        List<List<Integer>> special = new ArrayList<>();
+        special.add(s1);
+        special.add(s2);
+        List<Integer> needs = new ArrayList<>();
+        needs.add(1);
+        needs.add(2);
+        needs.add(1);
+        Assertions.assertEquals(11, solution.shoppingOffers(price, new ArrayList<>(special), needs));
     }
 
     @Test
     void shoppingOffers_3() {
-        List<Integer> price = List.of(4, 3, 2, 9, 8, 8);
-        List<List<Integer>> special = List.of(List.of(1, 5, 5, 1, 4, 0, 18), List.of(3, 3, 6, 6, 4, 2, 32));
-        List<Integer> needs = List.of(6, 5, 5, 6, 4, 1);
-        Assertions.assertEquals(11, solution.shoppingOffers(price, special, needs));
+        List<Integer> price = new ArrayList<>();
+        price.add(4);
+        price.add(3);
+        price.add(2);
+        price.add(9);
+        price.add(8);
+        price.add(8);
+        List<Integer> s1 = new ArrayList<>();
+        s1.add(1);
+        s1.add(5);
+        s1.add(5);
+        s1.add(1);
+        s1.add(4);
+        s1.add(0);
+        s1.add(18);
+        List<Integer> s2 = new ArrayList<>();
+        s2.add(3);
+        s2.add(3);
+        s2.add(6);
+        s2.add(6);
+        s2.add(4);
+        s2.add(2);
+        s2.add(32);
+        List<List<Integer>> special = new ArrayList<>();
+        special.add(s1);
+        special.add(s2);
+        List<Integer> needs = new ArrayList<>();
+        needs.add(6);
+        needs.add(5);
+        needs.add(5);
+        needs.add(6);
+        needs.add(4);
+        needs.add(1);
+        Assertions.assertEquals(11, solution.shoppingOffers(price, new ArrayList<>(special), needs));
     }
 
 }

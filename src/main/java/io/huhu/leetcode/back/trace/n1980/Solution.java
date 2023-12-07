9cf04dd9781ae87183dffe58e1a5ff87d2fb29d3
Chen <chenq_developer@126.com>
2023-12-07 13:54:25 +0800

    1980.找出不同的二进制字符串 [状态] 完成

--- src/main/java/io/huhu/leetcode/back/trace/n1980/Solution.java
+++ src/main/java/io/huhu/leetcode/back/trace/n1980/Solution.java
@@ -1,5 +1,8 @@
 package io.huhu.leetcode.back.trace.n1980;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * <a href="https://leetcode.cn/problems/find-unique-binary-string/description/">找出不同的二进制字符串</a>
  * <p>给你一个字符串数组nums, 该数组由n个互不相同的二进制字符串组成, 且每个字符串长度都是n.
@@ -17,7 +20,26 @@ class Solution {
      * </ul>
      */
     public String findDifferentBinaryString(String[] nums) {
-        return null;
+        StringBuilder sb = new StringBuilder("0".repeat(nums.length));
+        dfs(Arrays.asList(nums), sb, 0);
+        return sb.toString();
+    }
+
+    /**
+     * 深度优先遍历 + 回溯算法
+     */
+    private boolean dfs(List<String> nums, StringBuilder sb, int j) {
+        if (!nums.contains(sb.toString())) {
+            return true;
+        }
+        for (int i = j; i < sb.length(); i++) {
+            sb.setCharAt(i, '1');
+            if (dfs(nums, sb, i + 1)) {
+                return false;
+            }
+            sb.setCharAt(i, '0');
+        }
+        return true;
     }
 
 }

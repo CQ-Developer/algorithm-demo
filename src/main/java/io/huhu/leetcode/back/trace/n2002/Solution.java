e7541d875cb65c775ca8652197c97a857b2c37f8
Chen <chenq_developer@126.com>
2023-12-08 14:22:26 +0800

    2002.两个回文子序列长度的最大面积 [状态] 待完成

---
+++ src/main/java/io/huhu/leetcode/back/trace/n2002/Solution.java
@@ -0,0 +1,24 @@
+package io.huhu.leetcode.back.trace.n2002;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-product-of-the-length-of-two-palindromic-subsequences/description/">两个回文子序列长度的最大乘机</a>
+ * <p>给你一个字符串s, 请你找到s中两个不相交回文子序列, 使得它们长度的乘积最大.
+ * 两个子序列在原字符串中如果没有任何相同下标的字符串, 则它们是互不相交的.
+ * <p>请你返回两个回文子序列长度可以达到的最大乘积.
+ * <p>子序列指的是从原字符串中删除若干字符后, 剩余字符不改变顺序而得到的结果.
+ * 如果一个字符串从前往后读和从后往前读一摸一样, 那么这个字符串是一个回文字符串.
+ */
+class Solution {
+
+    /**
+     * <p>提示:
+     * <ul>
+     * <li>2 <= s.length <= 12
+     * <li>s只含有小写英文字母
+     * </ul>
+     */
+    public int maxProduct(String s) {
+        return 0;
+    }
+
+}

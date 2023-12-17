3e34a055adf9ed86f85f989df84d2e3b36db60d0
chen <18551681083@163.com>
2023-12-17 21:22:44 +0800

    wrong

---
+++ src/main/java/io/huhu/leetcode/back/trace/n2597/Solution.java
@@ -0,0 +1,58 @@
+package io.huhu.leetcode.back.trace.n2597;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * <a href="https://leetcode.cn/problems/the-number-of-beautiful-subsets/description/">美丽子集的数目</a>
+ * <p>给你一个由正整数组成的数组nums和一个正整数k
+ * <p>如果nums的子集中, 任意两个整数的绝对差均不等于k, 则认为该子数组是一个美丽子集
+ * <p>返回数组nums中非空且美丽的子集数目
+ * <p>nums的子集定义为: 可以经由nums删除某些元素(也可能不删除)得到的一个数组.
+ * 只有在删除元素时选择的索引不同的情况下, 两个子集才会被视作是不同的子集
+ */
+class Solution {
+
+    private int res;
+
+    /**
+     * 提示:
+     * <ul>
+     * <li>1 <= nums.length <= 20
+     * <li>1 <= nums[i], k <= 1000
+     * </ul>
+     */
+    public int beautifulSubsets(int[] nums, int k) {
+        res = nums.length;
+        for (int i = 2; i < nums.length; i++) {
+            dfs(nums, 0, i, new ArrayDeque<>(), k);
+        }
+        return res;
+    }
+
+    private void dfs(int[] nums, int j, int len, Deque<Integer> path, int k) {
+        if (path.size() == len) {
+            System.out.println(path);
+            res++;
+            return;
+        }
+        for (int i = j; i < nums.length; i++) {
+            if (!path.isEmpty() && check(path, nums[i], k)) {
+                continue;
+            }
+            path.addLast(nums[i]);
+            dfs(nums, i + 1, len, path, k);
+            path.removeLast();
+        }
+    }
+
+    private boolean check(Deque<Integer> path, int num, int k) {
+        for (int i : path) {
+            if (Math.abs(i - num) == k) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/back/trace/n2597/SolutionTest.java
@@ -0,0 +1,30 @@
+package io.huhu.leetcode.back.trace.n2597;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void case_1() {
+        int[] nums = {2, 4, 6};
+        int k = 2;
+        Assertions.assertEquals(4, solution.beautifulSubsets(nums, k));
+    }
+
+    @Test
+    void case_2() {
+        int[] nums = {1};
+        int k = 1;
+        Assertions.assertEquals(1, solution.beautifulSubsets(nums, k));
+    }
+
+}

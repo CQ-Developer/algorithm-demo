63f591c7bbdc8fff15460db5f5577a3adbf5262f
Chen <chenq_developer@126.com>
2023-12-18 17:18:08 +0800

    2698.求一个整数的惩罚数 [状态] 完成 [优化] 不使用Set进行去重, 第一次命中直接返回

--- src/main/java/io/huhu/leetcode/back/trace/n2698/Solution.java
+++ src/main/java/io/huhu/leetcode/back/trace/n2698/Solution.java
@@ -1,8 +1,5 @@
 package io.huhu.leetcode.back.trace.n2698;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * <a href="https://leetcode.cn/problems/find-the-punishment-number-of-an-integer/description/">求一个整数的惩罚数</a>
  * <p>n的惩罚数定义为所有满足以下条件i的数的平方和:
@@ -13,8 +10,6 @@ import java.util.Set;
  */
 class Solution {
 
-    private int res = 1;
-
     /**
      * 提示:
      * <ul>
@@ -22,10 +17,12 @@ class Solution {
      * </ul>
      */
     public int punishmentNumber(int n) {
-        Set<Integer> used = new HashSet<>();
-        for (int i = 2; i <= n; i++) {
-            String s = i * i + "";
-            dfs(s.toCharArray(), 0, i, 0, used);
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            int num = i * i;
+            if (dfs(num + "", 0, i, 0)) {
+                res += num;
+            }
         }
         return res;
     }
@@ -33,17 +30,21 @@ class Solution {
     /**
      * 深度优先遍历 + 回溯算法
      */
-    private void dfs(char[] chars, int s, int target, int sum, Set<Integer> set) {
-        if (s == chars.length && target == sum) {
-            set.add(target);
-            res += target * target;
-            return;
+    private boolean dfs(String s, int j, int target, int sum) {
+        if (j == s.length()) {
+            return target == sum;
         }
-        int n = 0, len = s == 0 ? chars.length - 1 : chars.length;
-        for (int i = s; i < len && !set.contains(target); i++) {
-            n = 10 * n + (chars[i] - '0');
-            dfs(chars, i + 1, target, sum + n, set);
+        int num = 0;
+        for (int i = j; i < s.length(); i++) {
+            num = 10 * num + (s.charAt(i) - '0');
+            if (sum + num > target) {
+                break;
+            }
+            if (dfs(s, i + 1, target, sum + num)) {
+                return true;
+            }
         }
+        return false;
     }
 
 }

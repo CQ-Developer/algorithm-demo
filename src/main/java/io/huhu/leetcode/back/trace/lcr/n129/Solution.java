41dae350dd426abf38a1f1165c984e292d7f66f9
Chen <chenq_developer@126.com>
2023-12-20 17:24:27 +0800

    lcr129.字母迷宫 [状态] 待完成

---
+++ src/main/java/io/huhu/leetcode/back/trace/lcr/n129/Solution.java
@@ -0,0 +1,16 @@
+package io.huhu.leetcode.back.trace.lcr.n129;
+
+/**
+ * <a href="https://leetcode.cn/problems/ju-zhen-zhong-de-lu-jing-lcof/description/">字母迷宫</a>
+ * <p>字母迷宫游戏初始界面记作m*n二维字符串数组grid, 请判断玩家是否能在grid中找出目标单词target
+ * <p>注意: 寻找单词时必须按照字母顺序, 通过水平或垂直方向相邻的单元格内的字母构成,
+ * 同时, 同一个单元格内的字母不允许被重复使用
+ * <p>提示:
+ */
+public class Solution {
+
+    public boolean wordPuzzle(char[][] grid, String target) {
+        return false;
+    }
+
+}

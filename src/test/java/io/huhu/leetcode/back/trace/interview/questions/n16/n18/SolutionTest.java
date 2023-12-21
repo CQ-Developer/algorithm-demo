cc7a154d68b8436c30a48683d2f1c280df00c33c
Chen <chenq_developer@126.com>
2023-12-21 16:51:29 +0800

    [面试题16.18] 括号 [备注] 待完成

---
+++ src/test/java/io/huhu/leetcode/back/trace/interview/questions/n16/n18/SolutionTest.java
@@ -0,0 +1,40 @@
+package io.huhu.leetcode.back.trace.interview.questions.n16.n18;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void case_1() {
+        boolean actual = solution.patternMatching("abba", "dogcatcatdog");
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void case_2() {
+        boolean actual = solution.patternMatching("abba", "dogcatcatfish");
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void case_3() {
+        boolean actual = solution.patternMatching("aaaa", "dogcatcatdog");
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void case_4() {
+        boolean actual = solution.patternMatching("abba", "dogdogdogdog");
+        Assertions.assertTrue(actual);
+    }
+
+}

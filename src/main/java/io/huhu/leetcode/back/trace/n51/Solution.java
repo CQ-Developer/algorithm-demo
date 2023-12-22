53692d2fa7af6406b7e2602fc88079a580f59b5c
Chen <chenq_developer@126.com>
2023-12-22 16:07:42 +0800

    [51] N皇后

---
+++ src/main/java/io/huhu/leetcode/back/trace/n51/Solution.java
@@ -0,0 +1,22 @@
+package io.huhu.leetcode.back.trace.n51;
+
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/n-queens/description/">N皇后</a>
+ * <p>按照国际象棋的规则, 皇后可以攻击与之处在同一行或同一列或同一斜线上的棋子.
+ * <p>N皇后问题研究的是如何将n个皇后放置在nxn的棋盘上, 并且使皇后彼此之间不能相互攻击.
+ * <p>给你一个整数n, 返回所有不同的N皇后问题的解决方案.
+ * <p>每一种解法包含不同的N皇后的棋子放置方案, 该方案中'Q'和'.'分别代表了皇后和空位.
+ * <p>提示:
+ * <ul>
+ * <li>1 <= n <= 9
+ * </ul>
+ */
+class Solution {
+
+    public List<List<String>> solveNQueens(int n) {
+        return null;
+    }
+
+}

ca84ea6d9160b39f4076c4777eed55d2a4387822
Chen <chenq_developer@126.com>
2023-12-27 15:42:27 +0800

    [282] 给表达式添加运算符 [状态] 待完成

---
+++ src/main/java/io/huhu/leetcode/back/trace/n282/Solution.java
@@ -0,0 +1,23 @@
+package io.huhu.leetcode.back.trace.n282;
+
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/expression-add-operators/description/">给表达式添加运算符</a>
+ * <p>给定一个仅包含数字0-9的字符串num和一个目标整数target,
+ * 在num的数字之间添加二元运算符+、-或*, 返回所有能够得到target的表达式
+ * <p>注意, 返回表达式中的操作数不应该包含前导零.
+ * <p>提示:
+ * <ul>
+ * <li>1 <= nums.length <= 10
+ * <li>num 仅含数字
+ * <li>-2<sup>31</sup> <= target <= 2<sup>31</sup>
+ * </ul>
+ */
+class Solution {
+
+    public List<String> addOperators(String num, int target) {
+        return null;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/back/trace/n282/SolutionTest.java
@@ -0,0 +1,39 @@
+package io.huhu.leetcode.back.trace.n282;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void addOperators_1() {
+        List<String> actual = solution.addOperators("123", 6);
+        List<String> expected = List.of("1+2+3", "1*2*3");
+        Assertions.assertIterableEquals(expected, actual);
+    }
+
+    @Test
+    void addOperators_2() {
+        List<String> actual = solution.addOperators("232", 8);
+        List<String> expected = List.of("2*3+2", "2+3*2");
+        Assertions.assertIterableEquals(expected, actual);
+    }
+
+    @Test
+    void addOperators_3() {
+        List<String> actual = solution.addOperators("3456237490", 9191);
+        List<String> expected = List.of();
+        Assertions.assertIterableEquals(expected, actual);
+    }
+
+}

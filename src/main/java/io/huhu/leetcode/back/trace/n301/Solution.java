d26316ebf15c8a81fb490881a2145757e40ac2fe
Chen <chenq_developer@126.com>
2023-12-28 14:11:53 +0800

    [301] 删除无效的括号 [状态] 完成 [备注] 经过了6次提交失败后成功

--- src/main/java/io/huhu/leetcode/back/trace/n301/Solution.java
+++ src/main/java/io/huhu/leetcode/back/trace/n301/Solution.java
@@ -1,6 +1,10 @@
 package io.huhu.leetcode.back.trace.n301;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * <a href="https://leetcode.cn/problems/remove-invalid-parentheses/description/">删除无效的括号</a>
@@ -15,9 +19,55 @@ import java.util.List;
  */
 class Solution {
 
+    private int max = 0;
+
     public List<String> removeInvalidParentheses(String s) {
-        // todo
-        return null;
+        char[] chars = s.toCharArray();
+        int l = 0;
+        while (l < chars.length && chars[l] == ')') {
+            l++;
+        }
+        int r = chars.length - 1;
+        while (r > -1 && chars[r] == '(') {
+            r--;
+        }
+        Set<String> res = new HashSet<>();
+        backTracing(Arrays.copyOfRange(chars, l, r + 1), 0, 0, 0, new StringBuilder(), res);
+        if (res.isEmpty()) {
+            res.add("");
+        }
+        return new ArrayList<>(res);
+    }
+
+    private void backTracing(char[] s, int j, int left, int right, StringBuilder sb, Set<String> res) {
+        if (j == s.length) {
+            if (left == right) {
+                max = Math.max(max, sb.length());
+                if (sb.length() == max) {
+                    res.add(sb.toString());
+                }
+            }
+            return;
+        }
+        if (s[j] == ')' && left == right) {
+            backTracing(s, j + 1, left, right, sb, res);
+            return;
+        }
+        for (int i = j; i < s.length; i++) {
+            int l = left, r = right;
+            if (s[i] == '(') {
+                l++;
+            }
+            if (s[i] == ')') {
+                r++;
+            }
+            if (r > l) {
+                continue;
+            }
+            sb.append(s[i]);
+            backTracing(s, i + 1, l, r, sb, res);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
 
 }
--- src/test/java/io/huhu/leetcode/back/trace/n301/SolutionTest.java
+++ src/test/java/io/huhu/leetcode/back/trace/n301/SolutionTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class SolutionTest {
@@ -18,14 +20,18 @@ class SolutionTest {
     @Test
     void removeInvalidParentheses1() {
         List<String> actual = solution.removeInvalidParentheses("()())()");
-        List<String> expect = List.of("(())()", "()()()");
+        List<String> expect = new ArrayList<>(List.of("(())()", "()()()"));
+        Collections.sort(actual);
+        Collections.sort(expect);
         Assertions.assertIterableEquals(expect, actual);
     }
 
     @Test
     void removeInvalidParentheses2() {
         List<String> actual = solution.removeInvalidParentheses("(a)())()");
-        List<String> expect = List.of("(a())()", "(a)()()");
+        List<String> expect = new ArrayList<>(List.of("(a())()", "(a)()()"));
+        Collections.sort(actual);
+        Collections.sort(expect);
         Assertions.assertIterableEquals(expect, actual);
     }
 
@@ -33,6 +39,50 @@ class SolutionTest {
     void removeInvalidParentheses3() {
         List<String> actual = solution.removeInvalidParentheses(")(");
         List<String> expect = List.of("");
+        Assertions.assertIterableEquals(expect, actual);
+    }
+
+    @Test
+    void removeInvalidParentheses4() {
+        List<String> actual = solution.removeInvalidParentheses("x(");
+        List<String> expect = List.of("x");
+        Assertions.assertIterableEquals(expect, actual);
+    }
+
+    @Test
+    void removeInvalidParentheses5() {
+        List<String> actual = solution.removeInvalidParentheses("(()");
+        List<String> expect = List.of("()");
+        Assertions.assertIterableEquals(expect, actual);
+    }
+
+    @Test
+    void removeInvalidParentheses6() {
+        List<String> actual = solution.removeInvalidParentheses(")d))");
+        List<String> expect = List.of("d");
+        Assertions.assertIterableEquals(expect, actual);
+    }
+
+    @Test
+    void removeInvalidParentheses7() {
+        List<String> actual = solution.removeInvalidParentheses("))()((");
+        List<String> expect = List.of("()");
+        Assertions.assertIterableEquals(expect, actual);
+    }
+
+    @Test
+    void removeInvalidParentheses8() {
+        List<String> actual = solution.removeInvalidParentheses("(");
+        List<String> expect = List.of("");
+        Assertions.assertIterableEquals(expect, actual);
+    }
+
+    @Test
+    void removeInvalidParentheses9() {
+        List<String> actual = solution.removeInvalidParentheses(")t))()()bo)");
+        List<String> expect = new ArrayList<>(List.of("t(()bo)", "t()(bo)", "t()()bo"));
+        Collections.sort(actual);
+        Collections.sort(expect);
         Assertions.assertIterableEquals(expect, actual);
     }
 

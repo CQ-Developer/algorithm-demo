5609ed4412724ec97479c54e1288c4b3895e2536
Chen <chenq_developer@126.com>
2023-12-29 10:12:09 +0800

    [679] 24点游戏 [状态] 完成 [优化] 使用数组代替集合

--- src/main/java/io/huhu/leetcode/back/trace/n679/Solution.java
+++ src/main/java/io/huhu/leetcode/back/trace/n679/Solution.java
@@ -1,10 +1,9 @@
 package io.huhu.leetcode.back.trace.n679;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * <a href="https://leetcode.cn/problems/24-game/description/">24点游戏</a>
+ * <a href="https://leetcode.cn/problems/24-game/description/">
+ * 24点游戏
+ * </a>
  * <p>
  * 给你一个长度为4的整数数组cards. 你有4张卡片, 每张卡片上都包含一个范围在[1,9]的数字.
  * 你应该使用运算符 ['+', '-', '*', '/'] 和括号 '(' 和 ')' 将这些卡片上的数字排列成数学表达式, 以获得值24.
@@ -13,28 +12,38 @@ import java.util.List;
  * 你必须遵守以下规则:
  * </p>
  * <ul>
- *     <li>
- *         除法运算符 '/' 表示实数除法, 而不是整数除法
- *         <li>例如, 4 / (1 - 2 / 3) = 4 / (1 / 3) = 12</li>
- *     </li>
- *     <li>
- *         每个运算都在两个数字之间. 特别是, 不能使用 '-' 作为一元运算符
- *         <li>例如, 如果 cards = [1, 1, 1, 1], 则表达式 "-1-1-1-1" 是不允许的</li>
- *     </li>
- *     <li>
- *         你不能把数字串在一起
- *         <li>例如, 如果 cards = [1, 2, 1, 2], 则表达式 "12+12" 无效</li>
- *     </li>
+ * <li>
+ * 除法运算符 '/' 表示实数除法, 而不是整数除法
+ * <li>
+ * 例如, 4 / (1 - 2 / 3) = 4 / (1 / 3) = 12
+ * </li>
+ * </li>
+ * <li>
+ * 每个运算都在两个数字之间. 特别是, 不能使用 '-' 作为一元运算符
+ * <li>
+ * 例如, 如果 cards = [1, 1, 1, 1], 则表达式 "-1-1-1-1" 是不允许的
+ * </li>
+ * </li>
+ * <li>
+ * 你不能把数字串在一起
+ * <li>
+ * 例如, 如果 cards = [1, 2, 1, 2], 则表达式 "12+12" 无效
+ * </li>
+ * </li>
  * </ul>
  * <p>
- *     如果可以得到这样的表达式, 其计算结果为24, 则返回 true, 否则返回 false.
+ * 如果可以得到这样的表达式, 其计算结果为24, 则返回 true, 否则返回 false.
  * </p>
  * <p>
- *     提示:
+ * 提示:
  * </p>
  * <ul>
- *     <li>cards.length == 4</li>
- *     <li>1 <= cards[i] <= 9</li>
+ * <li>
+ * cards.length == 4
+ * </li>
+ * <li>
+ * 1 <= cards[i] <= 9
+ * </li>
  * </ul>
  */
 class Solution {
@@ -43,62 +52,60 @@ class Solution {
      * 入口
      */
     public boolean judgePoint24(int[] cards) {
-        List<Fraction> list = new ArrayList<>();
-        for (int card : cards) {
-            list.add(new Fraction(card));
+        Fraction[] arr = new Fraction[cards.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new Fraction(cards[i]);
         }
-        return dfs(list);
+        return dfs(arr);
     }
 
-    private boolean dfs(List<Fraction> cards) {
-        if (cards.size() == 1) {
-            return cards.get(0).value() == 24;
+    /**
+     * 深度优先遍历 + 回溯
+     */
+    private boolean dfs(Fraction[] cards) {
+        if (cards.length == 1) {
+            return cards[0].value() == 24;
         }
-        for (int i = 0; i < cards.size(); i++) {
-            for (int j = 0; j < cards.size(); j++) {
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < cards.length; j++) {
                 if (i == j) {
                     continue;
                 }
-                // 拷贝不参与计算的剩余数字
-                List<Fraction> list = copyCards(cards, i, j);
-                // 当前参与计算的2个数字
-                Fraction f1 = cards.get(i);
-                Fraction f2 = cards.get(j);
-                // 进行加法运算
-                list.add(0, f1.plus(f2));
-                if (i < j && dfs(list)) {
-                    return true;
-                }
-                // 进行减法运算
-                list.set(0, f1.minus(f2));
-                if (dfs(list)) {
-                    return true;
-                }
-                // 进行乘法运算
-                list.set(0, f1.multi(f2));
-                if (i < j && dfs(list)) {
-                    return true;
-                }
-                // 进行除法运算
-                if (f2.value() != 0) {
-                    list.set(0, f1.divide(f2));
-                    if (dfs(list)) {
+                Fraction[] arr = copyCards(cards, i, j);
+                if (i < j) {
+                    arr[0] = cards[i].plus(cards[j]);
+                    if (dfs(arr)) {
                         return true;
                     }
+                    arr[0] = cards[i].multi(cards[j]);
+                    if (dfs(arr)) {
+                        return true;
+                    }
+                }
+                if (cards[j].n != 0) {
+                    arr[0] = cards[i].divide(cards[j]);
+                    if (dfs(arr)) {
+                        return true;
+                    }
+                }
+                arr[0] = cards[i].minus(cards[j]);
+                if (dfs(arr)) {
+                    return true;
                 }
             }
         }
         return false;
     }
 
-    private List<Fraction> copyCards(List<Fraction> cards, int i, int j) {
-        List<Fraction> list = new ArrayList<>();
-        for (int k = 0; k < cards.size(); k++) {
+    private Fraction[] copyCards(Fraction[] cards, int i, int j) {
+        int l = 1;
+        Fraction[] res = new Fraction[cards.length - 1];
+        for (int k = 0; k < cards.length; k++) {
             if (k != i && k != j) {
-                list.add(cards.get(k));
+                res[l++] = cards[k];
             }
         }
-        return list;
+        return res;
     }
 
     /**

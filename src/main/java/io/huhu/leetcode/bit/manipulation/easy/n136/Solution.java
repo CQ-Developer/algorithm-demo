6a54700a898d6d4b3fe70ef45a8b68d3d0fc03e4
Chen <chenq_developer@126.com>
2024-01-02 15:53:32 +0800

    nothing here.

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/n136/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/n136/Solution.java
@@ -3,7 +3,7 @@ package io.huhu.leetcode.bit.manipulation.easy.n136;
 /**
  * <a href="https://leetcode.cn/problems/single-number/description/">只出现一次的数字</a>
  */
-public class Solution {
+class Solution {
 
     /**
      * <p>异或运算: 相同为0, 不同为1

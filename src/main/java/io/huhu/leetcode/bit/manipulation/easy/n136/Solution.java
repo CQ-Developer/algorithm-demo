0d158892f25215c654cd2ef64ffa3b9a3ddf91d6
Chen <chenq_developer@126.com>
2024-01-02 15:48:06 +0800

    [136] 只出现一次的数字 [状态] 完成 [优化] 使用异或运算

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/n136/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/n136/Solution.java
@@ -1,25 +1,22 @@
 package io.huhu.leetcode.bit.manipulation.easy.n136;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * <a href="https://leetcode.cn/problems/single-number/description/">只出现一次的数字</a>
  */
 public class Solution {
 
+    /**
+     * <p>异或运算: 相同为0, 不同为1
+     * <p>a ^ a = 0
+     * <p>0 ^ a = a
+     * <p>a ^ b ^ b = a
+     */
     public int singleNumber(int[] nums) {
-        Map<Integer, Integer> bitMap = new HashMap<>(nums.length);
-        for (int num : nums) {
-            int count = bitMap.getOrDefault(num, 0);
-            bitMap.put(num, ++count);
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res ^= nums[i];
         }
-        for (Integer num : bitMap.keySet()) {
-            if (bitMap.get(num) == 1) {
-                return num;
-            }
-        }
-        return -1;
+        return res;
     }
 
 }

12656e285ecc568c2b88c85f6206610d20670702
Chen <chenq_developer@126.com>
2024-01-04 14:02:37 +0800

    [389] 找不同 [状态] 完成 [优化] 使用异或运算

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/n389/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/n389/Solution.java
@@ -1,32 +1,17 @@
 package io.huhu.leetcode.bit.manipulation.easy.n389;
 
-import java.util.Arrays;
-
 /**
  * <a href="https://leetcode.cn/problems/find-the-difference/description/">找不同</a>
  */
 class Solution {
 
-    /**
-     * 入口
-     *
-     * @param s 0 <= s.length <= 1000, 只包含小写字母
-     * @param t t.length == s.length + 1, 只包含小写字母
-     */
     public char findTheDifference(String s, String t) {
-        if (s.isEmpty()) {
-            return t.charAt(0);
+        int res = 0, n = s.length();
+        for (int i = 0; i < n; i++) {
+            res ^= (s.charAt(i) ^ t.charAt(i));
         }
-        char[] a = s.toCharArray();
-        Arrays.sort(a);
-        char[] b = t.toCharArray();
-        Arrays.sort(b);
-        for (int i = 0; i < s.length(); i++) {
-            if (a[i] != b[i]) {
-                return b[i];
-            }
-        }
-        return b[b.length - 1];
+        res ^= t.charAt(n);
+        return (char) res;
     }
 
 }

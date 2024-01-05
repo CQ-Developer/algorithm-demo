2513dfbdbc5987320d220cfebbc1017024e393a4
Chen <chenq_developer@126.com>
2024-01-05 13:43:48 +0800

    [645] 错误的集合 [状态] 完成 [备注] 考虑使用位运算的方式完成

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/n645/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/n645/Solution.java
@@ -6,7 +6,20 @@ package io.huhu.leetcode.bit.manipulation.easy.n645;
 class Solution {
 
     public int[] findErrorNums(int[] nums) {
-        return null;
+        int[] dict = new int[nums.length + 1];
+        for (int i : nums) {
+            dict[i]++;
+        }
+        int[] res = new int[2];
+        for (int i = 1; i < dict.length; i++) {
+            if (dict[i] == 2) {
+                res[0] = i;
+            }
+            if (dict[i] == 0) {
+                res[1] = i;
+            }
+        }
+        return res;
     }
 
 }

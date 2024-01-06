6c84104023e2e8a8bcc4d7dc144148208350c68c
chen <18551681083@163.com>
2024-01-06 20:21:18 +0800

    [762] 二进制表示中质数个计算置位 [状态] 完成 [优化] 直接使用API计算位1的个数

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/n762/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/n762/Solution.java
@@ -8,21 +8,9 @@ class Solution {
     public int countPrimeSetBits(int left, int right) {
         int res = 0;
         for (int i = left; i <= right; i++) {
-            res += (0xa28ac & (1 << countBit(i))) != 0 ? 1 : 0;
+            res += (0xa28ac & (1 << Integer.bitCount(i))) != 0 ? 1 : 0;
         }
         return res;
-    }
-
-    /**
-     * 统计二进制表示中1的个数
-     */
-    private int countBit(int n) {
-        int cnt = 0;
-        while (n != 0) {
-            n &= n - 1;
-            cnt++;
-        }
-        return cnt;
     }
 
 }

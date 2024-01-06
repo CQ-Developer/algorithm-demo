2538aae931c3daa8f3a4e58fc527bc741a632f24
chen <18551681083@163.com>
2024-01-06 19:55:25 +0800

    nothing here.

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/n693/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/n693/Solution.java
@@ -5,6 +5,12 @@ package io.huhu.leetcode.bit.manipulation.easy.n693;
  */
 class Solution {
 
+    /**
+     * 如果 {@code n} 是交替二进制数, 比如 {@code 101},
+     * 那么 {@code n ^ (n >> 1) = 111}.
+     * {@code 0101 >> 1 = 10}, {@code 101 ^ 010 = 111}.
+     * {@code 111 + 1 = 1000}, {@code 0111 & 1000 = 0}.
+     */
     public boolean hasAlternatingBits(int n) {
         int m = n ^ (n >> 1);
         return (m & (m + 1)) == 0;

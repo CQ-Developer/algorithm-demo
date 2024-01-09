0ce6a162d00635e0637839a3836bc512601666ba
Chen <chenq_developer@126.com>
2024-01-09 16:38:42 +0800

    [1763] 最长的美好子字符串 [状态] 完成 [备注] 使用分治思想 [备注] 注意大小写字符的转换方式

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/n1763/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/n1763/Solution.java
@@ -10,44 +10,23 @@ class Solution {
      * <li>1 <= s.length <= 100</li>
      * <li>s只包含大写和小写英文字母</li>
      * </ul>
-     *
-     * <p>一种利用分治思想的更加优秀的解</p>
-     * <pre>{@code
-     * public String longestNiceSubstring(String s) {
-     *     if (s.length() < 2) {
-     *         return "";
-     *     }
-     *     for (int i = 0; i < s.length(); i++) {
-     *         char c = s.charAt(i);
-     *         if ((Character.isUpperCase(c) && !s.contains(String.valueOf(Character.toLowerCase(c))))
-     *          || (Character.isLowerCase(c) && !s.contains(String.valueOf(Character.toUpperCase(c))))) {
-     *             String s1 = longestNiceSubstring(s.substring(0, i));
-     *             String s2 = longestNiceSubstring(s.substring(i + 1));
-     *             return s1.length() >= s2.length() ? s1 : s2;
-     *         }
-     *     }
-     *     return s;
-     * }
-     * }</pre>
+     * <p>分治</p>
+     * <p>注意: 'a' ^ 32 = 'A', 可以通过这种方式快速的进行大小写的转换</p>
      */
     public String longestNiceSubstring(String s) {
-        int pos = 0, len = 0, n = s.length();
-        char[] arr = s.toCharArray();
-        for (int i = 0; i < n; i++) {
-            int lower = 0, upper = 0;
-            for (int j = i; j < n; j++) {
-                if (arr[j] >= 'a') {
-                    lower |= 1 << (arr[j] - 'a');
-                } else {
-                    upper |= 1 << (arr[j] - 'A');
-                }
-                if (lower == upper && j - i + 1 > len) {
-                    pos = i;
-                    len = j - i + 1;
-                }
+        if (s.length() < 2) {
+            return "";
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            c ^= 32;
+            if (!s.contains(String.valueOf(c))) {
+                String s1 = longestNiceSubstring(s.substring(0, i));
+                String s2 = longestNiceSubstring(s.substring(i + 1));
+                return s1.length() >= s2.length() ? s1 : s2;
             }
         }
-        return String.valueOf(arr, pos, len);
+        return s;
     }
 
 }

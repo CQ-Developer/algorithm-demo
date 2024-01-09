80b935c04460c8b1d4eabf243f5c545f567ca842
Chen <chenq_developer@126.com>
2024-01-09 14:58:30 +0800

    [1684] 统计一致字符的数目 [状态] 完成 [思路] 打表

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/n1684/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/n1684/Solution.java
@@ -13,6 +13,32 @@ class Solution {
      * <li>allowed中的字符互不相同</li>
      * <li>words[i]和allowed只包含小写英文字母</li>
      * </ul>
+     * <br/>
+     * <p>补充一种性能更好的算法，但是未使用位运算</p>
+     * <pre>{@code
+     * public int countConsistentStrings(String allowed, String[] words) {
+     *     boolean[] table = new boolean[26];
+     *     for (int i = 0; i < allowed.length(); i++) {
+     *         table[allowed.charAt(i) - 'a'] = true;
+     *     }
+     *     int res = 0;
+     *     for (String word : words) {
+     *         if (check(table, word)) {
+     *             res++;
+     *         }
+     *     }
+     *     return res;
+     * }
+     *
+     * private boolean check(boolean[] table, String word) {
+     *     for (int i = 0; i < word.length(); i++) {
+     *         if (!table[word.charAt(i) - 'a']) {
+     *             return false;
+     *         }
+     *     }
+     *     return true;
+     * }
+     * }</pre>
      */
     public int countConsistentStrings(String allowed, String[] words) {
         int mask = 0;

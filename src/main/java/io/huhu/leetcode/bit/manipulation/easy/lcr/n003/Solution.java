94dbba9ad19f2036e069a8c4b3a91ac8a833964b
Chen <chenq_developer@126.com>
2024-01-10 16:07:17 +0800

    [lcr003] 比特位计数 [状态] 完成 [优化] 使用深度优先遍历 [备注] 参考题解

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/lcr/n003/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/lcr/n003/Solution.java
@@ -1,5 +1,7 @@
 package io.huhu.leetcode.bit.manipulation.easy.lcr.n003;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://leetcode.cn/problems/w3tCBm/description/">比特位计数</a>
  */
@@ -7,10 +9,18 @@ class Solution {
 
     public int[] countBits(int n) {
         int[] a = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            a[i] = a[i >> 1] + (i & 1);
-        }
+        dfs(1, 1, n, a);
         return a;
+    }
+
+    private void dfs(int i, int cnt, int n, int[] a) {
+        if (i > n) {
+            return;
+        }
+        a[i] = cnt;
+        System.out.println(Arrays.toString(a));
+        dfs(i << 1, cnt, n, a);
+        dfs((i << 1) + 1, cnt + 1, n, a);
     }
 
 }

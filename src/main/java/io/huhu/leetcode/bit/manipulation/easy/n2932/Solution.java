b2abb5fcb411269d4903b31ef82daa1b951c1a5b
Chen <chenq_developer@126.com>
2024-01-10 12:15:10 +0800

    [2932] 找出强数对的最大异或值 [状态] 完成

---
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/n2932/Solution.java
@@ -0,0 +1,26 @@
+package io.huhu.leetcode.bit.manipulation.easy.n2932;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-strong-pair-xor-i/description/">找出强数对的最大异或值I</a>
+ */
+class Solution {
+
+    /**
+     * <ul>
+     * <li>1 <= nums.length <= 50</li>
+     * <li>1 <= nums[i] <= 100</li>
+     * </ul>
+     */
+    public int maximumStrongPairXor(int[] nums) {
+        int max = 0;
+        for (int a : nums) {
+            for (int b : nums) {
+                if (Math.abs(a - b) <= Math.min(a, b)) {
+                    max = Math.max(max, a ^ b);
+                }
+            }
+        }
+        return max;
+    }
+
+}

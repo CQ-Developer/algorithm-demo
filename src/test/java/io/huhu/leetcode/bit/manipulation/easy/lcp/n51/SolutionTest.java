e0d35089acf495a0d569d4dfa4e39b436f33d3d2
Chen <chenq_developer@126.com>
2024-01-10 14:10:47 +0800

    [lcp51] 烹饪料理 [状态] 完成 [备注] 参考题解

---
+++ src/test/java/io/huhu/leetcode/bit/manipulation/easy/lcp/n51/SolutionTest.java
@@ -0,0 +1,46 @@
+package io.huhu.leetcode.bit.manipulation.easy.lcp.n51;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void perfectMenu_1() {
+        int[] materials = {3, 2, 4, 1, 2};
+        int[][] cookbooks = {
+                {1, 1, 0, 1, 2},
+                {2, 1, 4, 0, 0},
+                {3, 2, 4, 1, 0}};
+        int[][] attribute = {
+                {3, 2},
+                {2, 4},
+                {7, 6}};
+        int limit = 5;
+        Assertions.assertEquals(7, solution.perfectMenu(materials, cookbooks, attribute, limit));
+    }
+
+    @Test
+    void perfectMenu_2() {
+        int[] materials = {10, 10, 10, 10, 10};
+        int[][] cookbooks = {
+                {1, 1, 1, 1, 1},
+                {3, 3, 3, 3, 3},
+                {10, 10, 10, 10, 10}};
+        int[][] attribute = {
+                {5, 5},
+                {6, 6},
+                {10, 10}};
+        int limit = 1;
+        Assertions.assertEquals(11, solution.perfectMenu(materials, cookbooks, attribute, limit));
+    }
+
+}

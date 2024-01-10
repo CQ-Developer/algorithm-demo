b1703c7146b0fd10572b51019554dcb67eb82017
Chen <chenq_developer@126.com>
2024-01-10 15:21:34 +0800

    [lcr002] 二进制求和 [状态] 完成 [优化] 取消多余的判断

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/lcr/n002/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/lcr/n002/Solution.java
@@ -12,25 +12,13 @@ class Solution {
             int x = i >= 0 ? a.charAt(i) - '0' : 0;
             int y = j >= 0 ? b.charAt(j) - '0' : 0;
             if (x == 1 && y == 1) {
-                if (p == 1) {
-                    sb.append(1);
-                } else {
-                    sb.append(0);
-                    p = 1;
-                }
+                sb.append(p);
+                p = 1;
             } else if (x == 0 && y == 0) {
-                if (p == 1) {
-                    sb.append(1);
-                    p = 0;
-                } else {
-                    sb.append(0);
-                }
+                sb.append(p);
+                p = 0;
             } else {
-                if (p == 1) {
-                    sb.append(0);
-                } else {
-                    sb.append(1);
-                }
+                sb.append(p ^ 1);
             }
             i--;
             j--;

b1505ee54000d6e70f711734f2fe7ec8aad58ddf
Chen <chenq_developer@126.com>
2024-01-11 16:50:20 +0800

    [面试题01.04] 回文排列 [状态] 完成

--- src/main/java/io/huhu/leetcode/bit/manipulation/easy/interview/n01/n04/Solution.java
+++ src/main/java/io/huhu/leetcode/bit/manipulation/easy/interview/n01/n04/Solution.java
@@ -6,15 +6,15 @@ package io.huhu.leetcode.bit.manipulation.easy.interview.n01.n04;
 class Solution {
 
     public boolean canPermutePalindrome(String s) {
-        long l = 0, r = 0;
+        int[] table = new int[128];
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) >= 64) {
-                r ^= 1L << s.charAt(i) - 64;
-            } else {
-                l ^= 1L << s.charAt(i);
-            }
+            table[s.charAt(i)] ^= 1;
         }
-        return Long.bitCount(l) + Long.bitCount(r) <= 1;
+        int sum = 0;
+        for (int n : table) {
+            sum += n;
+        }
+        return sum <= 1;
     }
 
 }

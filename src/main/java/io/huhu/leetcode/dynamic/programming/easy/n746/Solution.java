1bb866e09f604f1fc69a638fd5f774faa25f22b1
Chen <chenq_developer@126.com>
2024-01-17 15:14:28 +0800

    [746] 使用最小花费爬楼梯 [状态] 完成 [优化] 不使用递归

--- src/main/java/io/huhu/leetcode/dynamic/programming/easy/n746/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/easy/n746/Solution.java
@@ -1,30 +1,17 @@
 package io.huhu.leetcode.dynamic.programming.easy.n746;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * <a href="https://leetcode.cn/problems/min-cost-climbing-stairs/description/">使用最小花费爬楼梯</a>
  */
 class Solution {
 
-    private final Map<Integer, Integer> cache = new HashMap<>();
-
     public int minCostClimbingStairs(int[] cost) {
-        return Math.min(dp(cost, 0), dp(cost, 1));
-    }
-
-    private int dp(int[] cost, int n) {
-        if (n >= cost.length) {
-            return 0;
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
         }
-        int cached = cache.getOrDefault(n, -1);
-        if (cached != -1) {
-            return cached;
-        }
-        int min = Math.min(cost[n] + dp(cost, n + 1), cost[n] + dp(cost, n + 2));
-        cache.put(n, min);
-        return min;
+        return dp[n];
     }
 
 }

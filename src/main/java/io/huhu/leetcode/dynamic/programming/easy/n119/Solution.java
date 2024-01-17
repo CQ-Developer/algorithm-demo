bc6bc828d16875f520a0f03423d84b2efe28bf60
Chen <chenq_developer@126.com>
2024-01-17 12:15:55 +0800

    [119] 杨辉三角II [状态] 完成 [优化] 使用数组

--- src/main/java/io/huhu/leetcode/dynamic/programming/easy/n119/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/easy/n119/Solution.java
@@ -1,6 +1,6 @@
 package io.huhu.leetcode.dynamic.programming.easy.n119;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,18 +9,16 @@ import java.util.List;
 class Solution {
 
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> pre;
-        List<Integer> res = List.of(1);
+        Integer[] res = {1};
         for (int i = 1; i <= rowIndex; i++) {
-            pre = res;
-            res = new ArrayList<>(i + 1);
-            res.add(1);
+            Integer[] cur = new Integer[i + 1];
+            cur[0] = cur[i] = 1;
             for (int j = 1; j < i; j++) {
-                res.add(pre.get(j - 1) + pre.get(j));
+                cur[j] = res[j - 1] + res[j];
             }
-            res.add(1);
+            res = cur;
         }
-        return res;
+        return Arrays.asList(res);
     }
 
 }

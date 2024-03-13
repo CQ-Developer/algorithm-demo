87e3ad627ca20e68ae82c3eedf3753da407eada8
Chen <chenq_developer@126.com>
2024-03-13 16:19:43 +0800

    [LCP07] 传递信息 [状态] 待完成

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/easy/lcp07/Solution.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.dynamic.programming.easy.lcp07;
+
+/**
+ * <a href="https://leetcode.cn/problems/chuan-di-xin-xi/description/">传递信息</a>
+ */
+class Solution {
+
+    public int numWays(int n, int[][] relation, int k) {
+        // todo
+        return -1;
+    }
+
+}

604c1e8ad3b20864e8106d4ef3b329a19f273c2c
Chen <chenq_developer@126.com>
2024-03-14 15:01:49 +0800

    [LCR088] 使用最小花费爬楼梯 [状态] 完成 [备注] 优化内存

--- src/main/java/io/huhu/leetcode/dynamic/programming/easy/lcr088/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/easy/lcr088/Solution.java
@@ -7,11 +7,13 @@ class Solution {
 
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        int[] dp = new int[n + 1];
+        int pre = 0, cur = 0;
         for (int i = 2; i <= n; i++) {
-            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+            int next = Math.min(cur + cost[i - 1], pre + cost[i - 2]);
+            pre = cur;
+            cur = next;
         }
-        return dp[n];
+        return cur;
     }
 
 }

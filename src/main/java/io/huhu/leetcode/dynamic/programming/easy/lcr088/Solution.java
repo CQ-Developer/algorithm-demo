b6d48a5e3ea51e1499334180b9b51a5e02bac2c0
Chen <chenq_developer@126.com>
2024-03-14 13:45:21 +0800

    [LCR088] 使用最小花费爬楼梯 [状态] 超时

--- src/main/java/io/huhu/leetcode/dynamic/programming/easy/lcr088/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/easy/lcr088/Solution.java
@@ -1,20 +1,25 @@
 package io.huhu.leetcode.dynamic.programming.easy.lcr088;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <a href="https://leetcode.cn/problems/GzCJIP/description/">使用最小花费爬楼梯</a>
  */
 class Solution {
 
     public int minCostClimbingStairs(int[] cost) {
-        return dfs(-1, cost);
+        return dfs(-1, cost, new HashMap<>());
     }
 
-    private int dfs(int i, int[] cost) {
+    private int dfs(int i, int[] cost, Map<Integer, Integer> cache) {
         if (i >= cost.length) {
             return 0;
         }
         int res = i == -1 ? 0 : cost[i];
-        res += Math.min(dfs(i + 1, cost), dfs(i + 2, cost));
+        int a = cache.getOrDefault(i + 1, dfs(i + 1, cost, cache));
+        int b = cache.getOrDefault(i + 2, dfs(i + 2, cost, cache));
+        res += Math.min(a, b);
         return res;
     }
 

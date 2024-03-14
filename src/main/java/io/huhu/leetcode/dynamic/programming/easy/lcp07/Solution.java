6897dfa9798de406466321c3e64567bedd049ffd
Chen <chenq_developer@126.com>
2024-03-14 09:25:39 +0800

    [LCP07] 传递信息 [状态] 完成 [备注] 使用回溯算法完成并未使用分支算法

--- src/main/java/io/huhu/leetcode/dynamic/programming/easy/lcp07/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/easy/lcp07/Solution.java
@@ -6,8 +6,21 @@ package io.huhu.leetcode.dynamic.programming.easy.lcp07;
 class Solution {
 
     public int numWays(int n, int[][] relation, int k) {
-        // todo
-        return -1;
+        return backTrace(0, n, relation, k);
+    }
+
+    private int backTrace(int i, int n, int[][] relations, int k) {
+        if (k == 0) {
+            return i == n - 1 ? 1 : 0;
+        }
+        int sum = 0;
+        for (int[] relation : relations) {
+            if (relation[0] != i) {
+                continue;
+            }
+            sum += backTrace(relation[1], n, relations, k - 1);
+        }
+        return sum;
     }
 
 }

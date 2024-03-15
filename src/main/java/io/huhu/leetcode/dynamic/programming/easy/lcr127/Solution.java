a1f87da73d0a7a563dd89e2daae76867821da0b3
Chen <chenq_developer@126.com>
2024-03-15 09:52:53 +0800

    [LCR127] 跳跃训练 [算法] 动态规划 [状态] 完成 [优化] 当前值之和前两个值相关

--- src/main/java/io/huhu/leetcode/dynamic/programming/easy/lcr127/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/easy/lcr127/Solution.java
@@ -6,16 +6,13 @@ package io.huhu.leetcode.dynamic.programming.easy.lcr127;
 class Solution {
 
     public int trainWays(int num) {
-        if (num < 2) {
-            return 1;
+        int a = 1, b = 1;
+        for (int i = 2; i < num + 1; i++) {
+            b = a + b;
+            a = b - a;
+            b %= 1000000007;
         }
-        int[] dp = new int[num + 1];
-        dp[0] = dp[1] = 1;
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-            dp[i] %= 1000000007;
-        }
-        return dp[num];
+        return b;
     }
 
 }

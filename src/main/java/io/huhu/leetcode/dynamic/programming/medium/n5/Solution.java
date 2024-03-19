91dd6f844f616057ed102a580c1e76541a215db5
chen <18551681083@163.com>
2024-03-19 16:26:06 +0800

    [5]最长回文串 [状态]完成 [备注]参考题解使用动态规划

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n5/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n5/Solution.java
@@ -6,32 +6,37 @@ package io.huhu.leetcode.dynamic.programming.medium.n5;
 class Solution {
 
     public String longestPalindrome(String s) {
-        String res = "";
-        int max = 0;
         char[] chars = s.toCharArray();
         int n = chars.length;
-        // n - (max >> 1) 表示剩下的字符串长度不可能超过max了，提前结束
-        for (int i = 0; i < n - (max >> 1); i++) {
-            int l = i - 1, r = i + 1, len = 1;
-            while (l >= 0 && chars[l] == chars[i]) {
-                l--;
-                len++;
-            }
-            while (r < n && chars[r] == chars[i]) {
-                r++;
-                len++;
-            }
-            while (l >= 0 && r < n && chars[l] == chars[r]) {
-                l--;
-                r++;
-                len += 2;
-            }
-            if (len > max) {
-                max = len;
-                res = s.substring(l + 1, r);
+        if (n < 2) {
+            return s;
+        }
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        int l = 0, max = 1;
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i < n; i++) {
+                // len = j - i + 1;
+                int j = len + i - 1;
+                if (j >= n) {
+                    break;
+                }
+                if (chars[i] != chars[j]) {
+                    dp[i][j] = false;
+                } else {
+                    // j - i + 1 < 4 表示字符串长度小于4
+                    // 简化为 j - i < 3
+                    dp[i][j] = j - i < 3 || dp[i + 1][j - 1];
+                }
+                if (dp[i][j] && j - i + 1 > max) {
+                    l = i;
+                    max = j - i + 1;
+                }
             }
         }
-        return res;
+        return s.substring(l, l + max);
     }
 
 }

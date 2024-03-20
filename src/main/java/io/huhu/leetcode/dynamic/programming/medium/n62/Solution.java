968e506b20fcf2eddddb1612e56ae8cac8518814
chen <18551681083@163.com>
2024-03-20 13:05:51 +0800

    [62]不同路径 [算法]动态规划 [状态]完成 [备注]参考题解优化空间复杂度

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n62/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n62/Solution.java
@@ -1,30 +1,23 @@
 package io.huhu.leetcode.dynamic.programming.medium.n62;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://leetcode.cn/problems/unique-paths/description/">不同路径</a>
  */
 class Solution {
 
     public int uniquePaths(int m, int n) {
-        // dp[i,j]表示走到该格子的所有走法之和
-        int[][] dp = new int[m][n];
-        // 第一行的每一格只有1种走法
-        for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
-        // 第一列的每一格只有1种走法
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
-        }
-        // 题目规定只能向下或向右走
-        // 所以当前位置[i,j]只能由[i-1,j]或[i,j-1]到达
-        // 即到达当前位置[i,j]的走法 = 到达[i-1,j]的走法 + 到达[i,j-1]的走法
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                // dp[j]相当于到达[i-1,j]的走法
+                // dp[j-1]相当于到达[i,j-1]的走法
+                dp[j] += dp[j - 1];
             }
         }
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 
 }

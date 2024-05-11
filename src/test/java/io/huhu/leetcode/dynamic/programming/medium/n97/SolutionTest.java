ddca32614d07a05fe71e5bdf0b3afbf3c86603a7
chen <18551681083@163.com>
2024-05-11 08:30:07 +0800

    [97]字符串交错 [算法]动态规划 [状态]完成 [备注]完全参考题解

---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/n97/SolutionTest.java
@@ -0,0 +1,40 @@
+package io.huhu.leetcode.dynamic.programming.medium.n97;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void test_1() {
+        String s1 = "aabcc";
+        String s2 = "dbbca";
+        String s3 = "aadbbcbcac";
+        Assertions.assertTrue(solution.isInterleave(s1, s2, s3));
+    }
+
+    @Test
+    void test_2() {
+        String s1 = "aabcc";
+        String s2 = "dbbca";
+        String s3 = "aadbbbaccc";
+        Assertions.assertFalse(solution.isInterleave(s1, s2, s3));
+    }
+
+    @Test
+    void test_3() {
+        String s1 = "";
+        String s2 = "";
+        String s3 = "";
+        Assertions.assertTrue(solution.isInterleave(s1, s2, s3));
+    }
+
+}

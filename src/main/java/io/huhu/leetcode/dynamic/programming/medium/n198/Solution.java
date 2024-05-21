2e3348d423a9b838b50fad88668118795f6b868f
NoahChen <noah.chen@batechworks.com>
2024-05-21 13:21:28 +0800

    [198]打家劫舍 [算法]动态规划 [状态]完成 [备注]参考题解

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n198/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n198/Solution.java
@@ -6,14 +6,13 @@ package io.huhu.leetcode.dynamic.programming.medium.n198;
 class Solution {
 
     public int rob(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n + 1];
-        dp[0]= 0;
-        dp[1] = nums[0];
-        for (int i = 2; i <= n; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i - 1] + dp[i - 2]);
+        int p = 0, c = 0;
+        for (int num : nums) {
+            int t = Math.max(c, p + num);
+            p = c;
+            c = t;
         }
-        return dp[n];
+        return c;
     }
 
 }

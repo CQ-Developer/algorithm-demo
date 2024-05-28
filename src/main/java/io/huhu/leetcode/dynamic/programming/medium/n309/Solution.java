af43943b01fce9ec031233f0cf74f659063fcdea
NoahChen <noah.chen@batechworks.com>
2024-05-28 12:20:05 +0800

    [309]买卖股票的最佳时机含冷冻期 [算法]动态规划 [状态]完成 [备注]参考题解

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n309/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n309/Solution.java
@@ -6,21 +6,18 @@ package io.huhu.leetcode.dynamic.programming.medium.n309;
 class Solution {
 
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        boolean freeze = false;
-        int[] dp = new int[prices.length];
-        for (int i = 1; i < dp.length; i++) {
-            if (freeze) {
-                freeze = false;
-                continue;
-            }
-            if (prices[i] > prices[i - 1]) {
-                dp[i] = dp[i - 1] + prices[i] - prices[i - 1];
-                profit += dp[i];
-                freeze = true;
-            }
+        int n = prices.length;
+        int[][] dp = new int[n][3];
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            // dp[i][0]: 手上持有股票的最大收益
+            // dp[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
+            // dp[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+            dp[i][1] = dp[i - 1][0] + prices[i];
+            dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][2]);
         }
-        return profit;
+        return Math.max(dp[n - 1][1], dp[n - 1][2]);
     }
 
 }

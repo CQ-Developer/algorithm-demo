ab049cf47e67104027f64f82b2b57de388f1ab3a
NoahChen <noah.chen@batechworks.com>
2024-05-31 11:06:32 +0800

    [198]打家劫舍 [算法]动态规划 [状态]完成 [优化]优化O(1)内存

--- src/main/java/io/huhu/leetcode/dynamic/programming/sumary/house/robber/n198/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/sumary/house/robber/n198/Solution.java
@@ -6,20 +6,13 @@ package io.huhu.leetcode.dynamic.programming.sumary.house.robber.n198;
 class Solution {
 
     public int rob(int[] nums) {
-        // 总共有n间房子
-        int n = nums.length;
-        // 偷0间房子的最大收益为0
-        int[] dp = new int[n + 1];
-        // 偷1间房子的最大收益为nums[0]
-        dp[1] = nums[0];
-        // 从偷2间房子开始计算
-        for (int i = 2; i <= n; i++) {
-            // 偷i间房子的最大收益:
-            // 1.当前房子不偷的最大收益
-            // 2.偷当前房子的收益 + 上1间房子没偷的最大收益
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        int p = 0, c = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int t = Math.max(c, p + nums[i]);
+            p = c;
+            c = t;
         }
-        return dp[n];
+        return c;
     }
 
 }

2eba877db03f46f5e5efd3e784cb29d740160f97
chen <18551681083@163.com>
2024-05-31 23:11:56 +0800

    修改目录结构

---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/core/buy/and/sell/stock/n121/SolutionTest.java
@@ -0,0 +1,28 @@
+package io.huhu.leetcode.dynamic.programming.core.buy.and.sell.stock.n121;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setup() {
+        solution = new Solution();
+    }
+
+    @Test
+    void test1() {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        Assertions.assertEquals(5, solution.maxProfit(prices));
+    }
+
+    @Test
+    void test2() {
+        int[] prices = {7, 6, 4, 3, 1};
+        Assertions.assertEquals(0, solution.maxProfit(prices));
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/core/buy/and/sell/stock/n188/SolutionTest.java
@@ -0,0 +1,37 @@
+package io.huhu.leetcode.dynamic.programming.core.buy.and.sell.stock.n188;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setup() {
+        solution = new Solution();
+    }
+
+    @Test
+    void test1() {
+        int k = 2;
+        int[] prices = {2, 4, 1};
+        Assertions.assertEquals(2, solution.maxProfit(k, prices));
+    }
+
+    @Test
+    void test2() {
+        int k = 2;
+        int[] prices = {3, 2, 6, 5, 0, 3};
+        Assertions.assertEquals(7, solution.maxProfit(k, prices));
+    }
+
+    @Test
+    void test3() {
+        int k = 2;
+        int[] prices = {2, 4, 1};
+        Assertions.assertEquals(2, solution.maxProfit(k, prices));
+    }
+
+}

1348044ed4f84e373d662e3960b1f0324c261056
chen <18551681083@163.com>
2024-05-31 23:27:37 +0800

    [337]打家劫舍III [算法]动态规划 [备注]待完成

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/core/house/robber/n337/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.core.house.robber.n337;
+
+/**
+ * <a href="https://leetcode.cn/problems/house-robber-iii/description/">打家劫舍III</a>
+ */
+class Solution {
+
+    public int rob(TreeNode root) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/core/house/robber/n337/SolutionTest.java
@@ -0,0 +1,40 @@
+package io.huhu.leetcode.dynamic.programming.core.house.robber.n337;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setup() {
+        solution = new Solution();
+    }
+
+    @Test
+    void test1() {
+        TreeNode root = new TreeNode(3,
+                new TreeNode(2,
+                        null,
+                        new TreeNode(3)),
+                new TreeNode(3,
+                        null,
+                        new TreeNode(1)));
+        Assertions.assertEquals(7, solution.rob(root));
+    }
+
+    @Test
+    void test2() {
+        TreeNode root = new TreeNode(3,
+                new TreeNode(4,
+                        new TreeNode(1),
+                        new TreeNode(3)),
+                new TreeNode(5,
+                        null,
+                        new TreeNode(1)));
+        Assertions.assertEquals(9, solution.rob(root));
+    }
+
+}

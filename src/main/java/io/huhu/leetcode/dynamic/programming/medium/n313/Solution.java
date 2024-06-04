e8ec14a26fffe3408491addb030f08c8ebf0a808
NoahChen <noah.chen@batechworks.com>
2024-06-04 09:00:27 +0800

    [313]超级丑数 [状态]待完成

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n313/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.medium.n313;
+
+/**
+ * <a href="https://leetcode.cn/problems/super-ugly-number/description/">超级丑数</a>
+ */
+class Solution {
+
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        return 0;
+    }
+
+}

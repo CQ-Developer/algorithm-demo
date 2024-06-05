f6d7310e18d2a986dff0d5c07bffb640ddbcb9d4
NoahChen <noah.chen@batechworks.com>
2024-06-05 17:30:32 +0800

    [322]零钱兑换 [算法]dp [状态]完成 [备注]参考题解

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n322/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n322/Solution.java
@@ -8,29 +8,24 @@ import java.util.Arrays;
 class Solution {
 
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) {
-            return 0;
-        }
-        if (coins.length == 1) {
-            if (coins[0] > amount) {
-                return -1;
+        // dp[i]是凑出金额i需要的最少硬币数量
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        // 凑0元
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int c : coins) {
+                // 注意i和c表示的是金额
+                // 金额不能为负
+                if (c <= i) {
+                    // 如果选额当前硬币的金额c
+                    // dp[i-c]表示加上金额c等于金额i的上次凑出的最少硬币数量
+                    // 因为选择了金额为c的硬币, 所以要在上次的硬币数量后加1
+                    dp[i] = Math.min(dp[i], dp[i - c] + 1);
+                }
             }
-            if (amount % coins[0] == 0) {
-                return amount / coins[0];
-            }
-            return -1;
         }
-        Arrays.sort(coins);
-        int ans = 0;
-        for (int i = coins.length - 1; i >= 0; ) {
-            if (coins[i] > amount) {
-                i--;
-                continue;
-            }
-            amount -= coins[i];
-            ans++;
-        }
-        return ans == 0 ? -1 : ans;
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 
 }

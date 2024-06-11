7cb4e43c66700dbf06def286e71ffc9e97bab9c5
NoahChen <noah.chen@batechworks.com>
2024-06-11 17:27:28 +0800

    [od] 贪吃的猴子 [alg] dfs

---
+++ src/test/java/io/huhu/huawei/od/s200/n3/SolutionTest.java
@@ -0,0 +1,31 @@
+package io.huhu.huawei.od.s200.n3;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void test1() {
+        int len = 7;
+        int[] numbers = {1, 2, 2, 7, 3, 6, 1};
+        int n = 3;
+        System.out.println(10);
+    }
+
+    @Test
+    void test2() {
+        int len = 3;
+        int[] numbers = {1, 2, 3};
+        int n = 3;
+        System.out.println(6);
+    }
+
+    @Test
+    void test3() {
+        int len = 4;
+        int[] numbers = {4, 2, 2, 3};
+        int n = 2;
+        System.out.println(7);
+    }
+
+}

ce9f389dd70fd7081d2a8df074224f2db1aa6e37
Jack <18551681083@163.com>
2024-06-12 23:39:01 +0800

    [od] 欢乐的周末 [alg] dfs [PS] 使用数组记录其实位置 [PS] 减少dfs的参数传递

--- src/main/java/io/huhu/huawei/od/s200/n11/Solution.java
+++ src/main/java/io/huhu/huawei/od/s200/n11/Solution.java
@@ -1,7 +1,5 @@
 package io.huhu.huawei.od.s200.n11;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 class Solution {
@@ -24,8 +22,8 @@ class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // input
-        int m = scanner.nextInt(), n = scanner.nextInt();
-        List<int[]> start = new ArrayList<>();
+        int m = scanner.nextInt(), n = scanner.nextInt(), s = 0;
+        int[][] start = new int[2][];
         int[][] gridA = new int[m][n];
         int[][] gridB = new int[m][n];
         for (int i = 0; i < m; i++) {
@@ -33,14 +31,14 @@ class Solution {
                 int num = scanner.nextInt();
                 gridA[i][j] = gridB[i][j] = num;
                 if (num == 2) {
-                    start.add(new int[]{i, j});
+                    start[s++] = new int[]{i, j};
                 }
             }
         }
         // alg
         int[][] table = new int[m][n];
-        dfs(gridA, m, n, start.get(0)[0], start.get(0)[1], table);
-        dfs(gridB, m, n, start.get(1)[0], start.get(1)[1], table);
+        dfs(gridA, start[0][0], start[0][1], table);
+        dfs(gridB, start[1][0], start[1][1], table);
         // output
         int result = 0;
         for (int i = 0; i < m; i++) {
@@ -53,22 +51,22 @@ class Solution {
         System.out.println(result);
     }
 
-    private static void dfs(int[][] grid, int m, int n, int i, int j, int[][] table) {
+    private static void dfs(int[][] grid, int i, int j, int[][] table) {
         if (grid[i][j] == 3) {
             table[i][j]++;
         }
         grid[i][j] = 1;
         if (i > 0 && grid[i - 1][j] != 1) {
-            dfs(grid, m, n, i - 1, j, table);
+            dfs(grid, i - 1, j, table);
         }
-        if (i + 1 < m && grid[i + 1][j] != 1) {
-            dfs(grid, m, n, i + 1, j, table);
+        if (i + 1 < grid.length && grid[i + 1][j] != 1) {
+            dfs(grid, i + 1, j, table);
         }
         if (j > 0 && grid[i][j - 1] != 1) {
-            dfs(grid, m, n, i, j - 1, table);
+            dfs(grid, i, j - 1, table);
         }
-        if (j + 1 < n && grid[i][j + 1] != 1) {
-            dfs(grid, m, n, i, j + 1, table);
+        if (j + 1 < grid[i].length && grid[i][j + 1] != 1) {
+            dfs(grid, i, j + 1, table);
         }
     }
 

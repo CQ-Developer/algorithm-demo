1cfb00a162f490faede2363cfcc182a610e951b5
NoahChen <noah.chen@batechworks.com>
2024-06-19 10:29:05 +0800

    n376. Wiggle Subsequence alg. dynamic programming PS. refer to answer

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n376/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n376/Solution.java
@@ -10,22 +10,15 @@ class Solution {
      * 0 <= nums[i] <= 1000
      */
     public int wiggleMaxLength(int[] nums) {
-        int n = nums.length;
-        if (n == 1) {
-            return 1;
-        }
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = nums[0] == nums[1] ? 1 : 2;
-        for (int i = 3, j = i - 1; i < n + 1; i++, j++) {
-            int a = nums[j - 2], b = nums[j - 1], c = nums[j];
-            if ((a > b && c > b) || (a < b && c < b) || (a == b && c != b)) {
-                dp[i] = dp[i - 1] + 1;
-            } else {
-                dp[i] = dp[i - 1];
+        int up = 1, down = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            } else if (nums[i] < nums[i - 1]) {
+                down = up + 1;
             }
         }
-        return dp[n];
+        return Math.max(up, down);
     }
 
 }

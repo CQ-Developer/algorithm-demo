04c802749451b840ad8153d15281d1f2f5f808a8
Jack <18551681083@163.com>
2024-06-21 23:01:35 +0800

    n413. Arithmetic Slices alg. dynamic programming

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n413/Solution.java
@@ -0,0 +1,44 @@
+package io.huhu.leetcode.dynamic.programming.medium.n413;
+
+/**
+ * <h1>
+ * <a href="https://leetcode.cn/problems/arithmetic-slices/description/">Arithmetic Slices</a>
+ * </h1>
+ * <p>
+ * An integer array is called arithmetic if it consists of at least
+ * three elements and if the difference between any two consecutive
+ * element is same.
+ * </p>
+ * <p>
+ * For example, [1,3,5,7,9], [7,7,7,7],
+ * and [3,-1,-5,-9] are arithmetic sequence.
+ * </p>
+ * <p>
+ * Given an integer array nums, return the number
+ * of arithmetic subarrays of nums.
+ * </p>
+ * <p>
+ * A subarray is a contiguous subsequence of the array.
+ * </p>
+ */
+class Solution {
+
+    /**
+     * <ul>
+     * <li>1 <= nums.length <= 5000</li>
+     * <li>-1000 <= nums[i] <= 1000</li>
+     * </ul>
+     */
+    public int numberOfArithmeticSlices(int[] nums) {
+        int ans = 0;
+        int[] dp = new int[nums.length];
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                dp[i] = dp[i - 1] + 1;
+                ans += dp[i];
+            }
+        }
+        return ans;
+    }
+
+}

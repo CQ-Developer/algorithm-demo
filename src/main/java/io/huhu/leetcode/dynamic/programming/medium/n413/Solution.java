5ec66b0c0353bdb1d05126e957d43b75c58d4380
Jack <18551681083@163.com>
2024-06-21 23:06:03 +0800

    n413. Arithmetic Slices alg. dynamic programming opt. O(1) memory

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n413/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n413/Solution.java
@@ -30,12 +30,13 @@ class Solution {
      * </ul>
      */
     public int numberOfArithmeticSlices(int[] nums) {
-        int ans = 0;
-        int[] dp = new int[nums.length];
+        int ans = 0, dp = 0;
         for (int i = 2; i < nums.length; i++) {
             if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
-                dp[i] = dp[i - 1] + 1;
-                ans += dp[i];
+                dp += 1;
+                ans += dp;
+            } else {
+                dp = 0;
             }
         }
         return ans;

326481da67587e920f1e5bc4e25421f9f37326a5
NoahChen <noah.chen@batechworks.com>
2024-06-24 17:15:08 +0800

    435. Non-overlapping Intervals ps. init

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n435/Solution.java
@@ -0,0 +1,26 @@
+package io.huhu.leetcode.dynamic.programming.medium.n435;
+
+/**
+ * <h1>
+ * <a href="https://leetcode.cn/problems/non-overlapping-intervals/description/">Non-overlapping Intervals</a>
+ * </h1>
+ * <p>
+ * Given an array of intervals where intervals[i] = [start<sub>i</sub>, end<sub>i</sub>],
+ * return the minimum number of intervals you need to remove to make the rest of the
+ * intervals non-overlapping.
+ * </p>
+ */
+class Solution {
+
+    /**
+     * <ul>
+     * <li>1 <= intervals.length <= 10<sup>5</sup></li>
+     * <li>intervals[i].length == 2</li>
+     * <li>-5 * 10<sup>4</sup> <= start<sub>i</sub> < end<sub>i</sub> <= 5 * 10<sup>4</sup></li>
+     * </ul>
+     */
+    public int eraseOverlapIntervals(int[][] intervals) {
+        return 0;
+    }
+
+}

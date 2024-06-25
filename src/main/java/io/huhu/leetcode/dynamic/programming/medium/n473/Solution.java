664b4817382a4ff19f32b8f8b9171fe4509982f7
Jack <18551681083@163.com>
2024-06-25 23:14:18 +0800

    473. Matchsticks to Square state. init

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n473/Solution.java
@@ -0,0 +1,23 @@
+package io.huhu.leetcode.dynamic.programming.medium.n473;
+
+/**
+ * <h1>
+ * <a href="https://leetcode.cn/problems/matchsticks-to-square/description/">Matchsticks to Square</a>
+ * </h1>
+ * <p>You are given an integer array matchsticks where matchsticks[i] is the length
+ * of the i<sub>th</sub> matchstick. You want to use all the matchsticks to make
+ * one square. You should not break any stick, but you can link them up, and each
+ * matchstick must be used exactly one time.</p>
+ * <p>Return true if you can make this square and false otherwise.</p>
+ */
+class Solution {
+
+    /**
+     * <li>1 <= matchsticks.length <= 15</li>
+     * <li>1 <= matchsticks[i] <= 10<sup>8</sup></li>
+     */
+    public boolean makesquare(int[] matchsticks) {
+        return false;
+    }
+
+}

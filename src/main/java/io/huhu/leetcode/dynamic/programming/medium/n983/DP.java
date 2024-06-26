c2643ecba4d46d4b8d562282da6648a8fbf2065d
NoahChen <noah.chen@batechworks.com>
2024-06-26 18:08:09 +0800

    983. Minimum Cost For Tickets alg. dp init

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n983/DP.java
@@ -0,0 +1,17 @@
+package io.huhu.leetcode.dynamic.programming.medium.n983;
+
+/**
+ * <a href="https://leetcode.cn/problems/minimum-cost-for-tickets/description/">Minimum Cost For Tickets</a>
+ */
+class DP implements Solution {
+
+    @Override
+    public int mincostTickets(int[] days, int[] costs) {
+        int[] dp = new int[365 + 1];
+        for (int i = 1; i < dp.length; i++) {
+            // int a =
+        }
+        return 0;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/n983/DPTest.java
@@ -0,0 +1,25 @@
+package io.huhu.leetcode.dynamic.programming.medium.n983;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DPTest extends SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setup() {
+        solution = new DP();
+    }
+
+    @Test
+    void test1() {
+        case_1(solution);
+    }
+
+    @Test
+    void test2() {
+        case_2(solution);
+    }
+
+}

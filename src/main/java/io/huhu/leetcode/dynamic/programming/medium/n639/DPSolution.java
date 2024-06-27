dca07c188ce419a6d229d065e5340658351333ff
Jack <18551681083@163.com>
2024-06-27 22:30:25 +0800

    639. Decode Ways II alg. dp opt. O(1) space

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n639/DPSolution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n639/DPSolution.java
@@ -5,33 +5,42 @@ final class DPSolution extends Solution {
     @Override
     public int numDecodings(String s) {
         char[] cs = s.toCharArray();
-        int n = cs.length;
-        long[] dp = new long[n + 1];
-        dp[n] = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            if (cs[i] == '0') {
-                continue;
-            }
-            dp[i] = dp[i + 1] * (cs[i] == '*' ? 9 : 1);
-            if (i + 1 >= n) {
-                continue;
-            }
-            if (cs[i] == '*') {
-                if (cs[i + 1] == '*') {
-                    dp[i] += dp[i + 2] * 15;
-                } else {
-                    dp[i] += dp[i + 2] * (cs[i + 1] <= '6' ? 2 : 1);
+        long a = 1, b = 0;
+        for (int i = cs.length - 1; i >= 0; i--) {
+            long c = 0;
+            if (cs[i] != '0') {
+                c = a * (cs[i] == '*' ? 9 : 1);
+                if (i + 1 < cs.length) {
+                    if (cs[i] == '*') {
+                        if (cs[i + 1] == '*') {
+                            c += b * 15;
+                        } else {
+                            if (cs[i + 1] <= '6') {
+                                c += b * 2;
+                            } else {
+                                c += b;
+                            }
+                        }
+                    } else {
+                        if (cs[i + 1] == '*') {
+                            if (cs[i] == '1') {
+                                c += b * 9;
+                            } else if (cs[i] == '2') {
+                                c += b * 6;
+                            }
+                        } else {
+                            if ((cs[i] - '0') * 10 + (cs[i + 1] - '0') <= 26) {
+                                c += b;
+                            }
+                        }
+                    }
                 }
-            } else {
-                if (cs[i + 1] == '*') {
-                    dp[i] += dp[i + 2] * (cs[i] == '1' ? 9 : cs[i] == '2' ? 6 : 0);
-                } else {
-                    dp[i] += dp[i + 2] * ((cs[i] - '0') * 10 + (cs[i + 1] - '0') <= 26 ? 1 : 0);
-                }
+                c %= 1000000007;
             }
-            dp[i] %= 1000000007;
+            b = a;
+            a = c;
         }
-        return (int) dp[0];
+        return (int) a;
     }
 
 }

809c4892a627c808c12300d0ed62f636621ab061
NoahChen <noah.chen@batechworks.com>
2024-06-27 09:08:04 +0800

    91. Decode Ways alg. dfs opt. O(1) space

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n91/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n91/DP.java
@@ -4,20 +4,20 @@ class DP extends Solution {
 
     @Override
     public int numDecodings(String s) {
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[n] = 1;
-        for (int i = n - 1; i >= 0; i--) {
+        int a = 1, b = 0;
+        for (int i = s.length() - 1, c; i >= 0; i--) {
             if (s.charAt(i) == '0') {
-                dp[i] = 0;
+                c = 0;
             } else {
-                dp[i] = dp[i + 1];
+                 c = a;
                 if (i + 1 < s.length() && ((s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0')) <= 26) {
-                    dp[i] += dp[i + 2];
+                    c += b;
                 }
             }
+            b = a;
+            a = c;
         }
-        return dp[0];
+        return a;
     }
 
 }

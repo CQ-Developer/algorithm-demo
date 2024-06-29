583c399e2405a541f9ce924362a5dd0ffd2157b6
Jack <18551681083@163.com>
2024-06-29 22:39:08 +0800

    64. Minimum Path Sum alg. dfs + mem

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n64/DFSSolution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n64/DFSSolution.java
@@ -1,33 +1,33 @@
 package io.huhu.leetcode.dynamic.programming.medium.n64;
 
-import java.util.Arrays;
-
 final class DFSSolution extends Solution {
 
     @Override
     public int minPathSum(int[][] grid) {
-        int[][] mem = new int[grid.length][grid[0].length];
-        for (int[] m : mem) {
-            Arrays.fill(m, -1);
+        int[][] f = new int[grid.length][grid[0].length];
+        for (int i = 0; i < f.length; i++) {
+            for (int j = 0; j < f[i].length; j++) {
+                f[i][j] = -1;
+            }
         }
-        return dfs(grid, 0, 0, mem);
+        return dfs(grid, 0, 0, f);
     }
 
-    private int dfs(int[][] grid, int i, int j, int[][] mem) {
+    private int dfs(int[][] grid, int i, int j, int[][] f) {
+        if (f[i][j] != -1) {
+            return f[i][j];
+        }
         if (i == grid.length - 1 && j == grid[i].length - 1) {
             return grid[i][j];
         }
-        if (mem[i][j] != -1) {
-            return mem[i][j];
-        }
         int ans = Integer.MAX_VALUE;
         if (i + 1 < grid.length) {
-            ans = Math.min(ans, dfs(grid, i + 1, j, mem));
+            ans = Math.min(ans, dfs(grid, i + 1, j, f));
         }
         if (j + 1 < grid[i].length) {
-            ans = Math.min(ans, dfs(grid, i, j + 1, mem));
+            ans = Math.min(ans, dfs(grid, i, j + 1, f));
         }
-        return mem[i][j] = ans + grid[i][j];
+        return f[i][j] = ans + grid[i][j];
     }
 
 }

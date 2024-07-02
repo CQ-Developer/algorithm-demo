f7d354476b2197bf4c653b2d8fc0f5e8f44dcf89
NoahChen <noah.chen@batechworks.com>
2024-07-02 09:13:21 +0800

    1143. Longest Common Subsequence alg. dfs

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n1143/DFSSolution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n1143/DFSSolution.java
@@ -4,18 +4,19 @@ final class DFSSolution extends Solution {
 
     @Override
     public int longestCommonSubsequence(String text1, String text2) {
-        char[] s1 = text1.toCharArray(), s2 = text2.toCharArray();
-        return dfs(s1, s1.length, s2, s2.length);
+        char[] c1 = text1.toCharArray();
+        char[] c2 = text2.toCharArray();
+        return dfs(c1, c1.length, c2, c2.length);
     }
 
-    private int dfs(char[] s1, int len1, char[] s2, int len2) {
-        if (len1 == 0 || len2 == 0) {
+    private int dfs(char[] s1, int i, char[] s2, int j) {
+        if (i == 0 || j == 0) {
             return 0;
         }
-        if (s1[len1 - 1] == s2[len2 - 1]) {
-            return dfs(s1, len1 - 1, s2, len2 - 1) + 1;
+        if (s1[i - 1] == s2[j - 1]) {
+            return dfs(s1, i - 1, s2, j - 1) + 1;
         }
-        return Math.max(dfs(s1, len1 - 1, s2, len2), dfs(s1, len1, s2, len2 - 1));
+        return Math.max(dfs(s1, i - 1, s2, j), dfs(s1, i, s2, j - 1));
     }
 
 }

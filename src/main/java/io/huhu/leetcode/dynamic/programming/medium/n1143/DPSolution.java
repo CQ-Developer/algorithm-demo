1c9b2b6f670917498407d6d73c1e3fe4bf75a22b
NoahChen <noah.chen@batechworks.com>
2024-07-02 11:53:23 +0800

    1143. Longest Common Subsequence alg. dp opt. O(1) space

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n1143/DPSolution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n1143/DPSolution.java
@@ -4,18 +4,24 @@ final class DPSolution extends Solution {
 
     @Override
     public int longestCommonSubsequence(String text1, String text2) {
+        if (text1.length() < text2.length()) {
+            return longestCommonSubsequence(text2, text1);
+        }
         int n = text1.length(), m = text2.length();
-        int[][] f = new int[n + 1][m + 1];
+        int[] f = new int[m + 1];
         for (int i = 1; i <= n; i++) {
+            int leftup = 0;
             for (int j = 1; j <= m; j++) {
+                int t = f[j];
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    f[i][j] = f[i - 1][j - 1] + 1;
+                    f[j] = 1 + leftup;
                 } else {
-                    f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+                    f[j] = Math.max(f[j - 1], f[j]);
                 }
+                leftup = t;
             }
         }
-        return f[n][m];
+        return f[m];
     }
 
 }

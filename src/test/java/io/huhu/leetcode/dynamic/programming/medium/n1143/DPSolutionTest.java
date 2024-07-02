635205b816a48943f2b1f7746c536e9824f76397
NoahChen <noah.chen@batechworks.com>
2024-07-02 10:13:59 +0800

    1143. Longest Common Subsequence alg. dp

---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/n1143/DPSolutionTest.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.dynamic.programming.medium.n1143;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class DPSolutionTest extends SolutionTest {
+
+    @Override
+    @BeforeEach
+    void setup() {
+        solution = new DPSolution();
+    }
+
+}

6f1b7c403e5f370fbada38d0afc54f41f03dc8e7
NoahChen <noah.chen@batechworks.com>
2024-07-02 11:59:21 +0800

    1143. Longest Common Subsequence alg. dp opt. use char[] to make faster

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n1143/DPSolution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n1143/DPSolution.java
@@ -4,16 +4,20 @@ final class DPSolution extends Solution {
 
     @Override
     public int longestCommonSubsequence(String text1, String text2) {
-        if (text1.length() < text2.length()) {
-            return longestCommonSubsequence(text2, text1);
+        if (text1.length() > text2.length()) {
+            return dp(text1.toCharArray(), text2.toCharArray());
         }
-        int n = text1.length(), m = text2.length();
+        return dp(text2.toCharArray(), text1.toCharArray());
+    }
+
+    private int dp(char[] s1, char[] s2) {
+        int n = s1.length, m = s2.length;
         int[] f = new int[m + 1];
         for (int i = 1; i <= n; i++) {
             int leftup = 0;
             for (int j = 1; j <= m; j++) {
                 int t = f[j];
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                if (s1[i - 1] == s2[j - 1]) {
                     f[j] = 1 + leftup;
                 } else {
                     f[j] = Math.max(f[j - 1], f[j]);

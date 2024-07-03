f1835d4c4ce37c938dd9e46f9960ffb17ec2b77f
NoahChen <noah.chen@batechworks.com>
2024-07-03 11:30:18 +0800

    516. Longest Palindromic Subsequence alg. dp

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n516/DPSolution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n516/DPSolution.java
@@ -4,7 +4,23 @@ final class DPSolution extends Solution {
 
     @Override
     public int longestPalindromeSubseq(String s) {
-        return 0;
+        char[] c = s.toCharArray();
+        int n = c.length;
+        int[][] f = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            f[i][i] = 1;
+            if (i + 1 < n) {
+                f[i][i + 1] = c[i] == c[i + 1] ? 2 : 1;
+            }
+            for (int j = i + 2; j < n; j++) {
+                if (c[i] == c[j]) {
+                    f[i][j] = f[i + 1][j - 1] + 2;
+                } else {
+                    f[i][j] = Math.max(f[i + 1][j], f[i][j - 1]);
+                }
+            }
+        }
+        return f[0][n - 1];
     }
 
 }

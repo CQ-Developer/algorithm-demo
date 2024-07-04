5854fa1203a21fa544a7aef06152b7eb96e13979
NoahChen <noah.chen@batechworks.com>
2024-07-04 17:14:35 +0800

    115. Longest Subsequences alg. dp opt. from right to left

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n115/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n115/Solution.java
@@ -13,13 +13,10 @@ class Solution {
         int[] f = new int[n + 1];
         f[0] = 1;
         for (int i = 1; i <= m; i++) {
-            int leftup = f[0];
-            for (int j = 1; j <= n; j++) {
-                int x = f[j];
+            for (int j = n; j >= 1; j--) {
                 if (a[i - 1] == b[j - 1]) {
-                    f[j] += leftup;
+                    f[j] += f[j - 1];
                 }
-                leftup = x;
             }
         }
         return f[n];

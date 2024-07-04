a2c6fd3320b4b519c7498fc84d970cb2e744bde6
NoahChen <noah.chen@batechworks.com>
2024-07-04 10:28:04 +0800

    516. Longest Palindromic Subsequence alg. dp & O(1) space

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n516/DPSolution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n516/DPSolution.java
@@ -6,21 +6,24 @@ final class DPSolution extends Solution {
     public int longestPalindromeSubseq(String s) {
         char[] c = s.toCharArray();
         int n = c.length;
-        int[][] f = new int[n][n];
-        for (int i = n - 1; i >= 0; i--) {
-            f[i][i] = 1;
+        int[] f = new int[n];
+        for (int i = n - 1, leftDown = 0; i >= 0; i--) {
+            f[i] = 1;
             if (i + 1 < n) {
-                f[i][i + 1] = c[i] == c[i + 1] ? 2 : 1;
+                leftDown = f[i + 1];
+                f[i + 1] = c[i] == c[i + 1] ? 2 : 1;
             }
             for (int j = i + 2; j < n; j++) {
+                int t = f[j];
                 if (c[i] == c[j]) {
-                    f[i][j] = f[i + 1][j - 1] + 2;
+                    f[j] = leftDown + 2;
                 } else {
-                    f[i][j] = Math.max(f[i + 1][j], f[i][j - 1]);
+                    f[j] = Math.max(f[j], f[j - 1]);
                 }
+                leftDown = t;
             }
         }
-        return f[0][n - 1];
+        return f[n - 1];
     }
 
 }

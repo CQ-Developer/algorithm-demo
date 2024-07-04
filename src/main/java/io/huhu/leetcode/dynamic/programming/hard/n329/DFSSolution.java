7688d6a9c28ece595d5a9ddb2c211bddb38ed7a8
NoahChen <noah.chen@batechworks.com>
2024-07-04 15:45:38 +0800

    329. Longest Increasing Path in a Matrix alg. dfs + mem

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n329/DFSSolution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n329/DFSSolution.java
@@ -5,11 +5,6 @@ final class DFSSolution extends Solution {
     @Override
     public int longestIncreasingPath(int[][] matrix) {
         int[][] mem = new int[matrix.length][matrix[0].length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                mem[i][j] = -1;
-            }
-        }
         int ans = 0;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -23,25 +18,23 @@ final class DFSSolution extends Solution {
         if (matrix[i][j] == -1) {
             return 0;
         }
-        if (mem[i][j] != -1) {
+        if (mem[i][j] != 0) {
             return mem[i][j];
         }
-        int num = matrix[i][j], ans = 1;
-        matrix[i][j] = -1;
-        if (i + 1 < matrix.length && matrix[i + 1][j] > num) {
-            ans = Math.max(ans, 1 + dfs(mem, matrix, i + 1, j));
+        int ans = 0;
+        if (i + 1 < matrix.length && matrix[i + 1][j] > matrix[i][j]) {
+            ans = Math.max(ans, dfs(mem, matrix, i + 1, j));
         }
-        if (i - 1 >= 0 && matrix[i - 1][j] > num) {
-            ans = Math.max(ans, 1 + dfs(mem, matrix, i - 1, j));
+        if (i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j]) {
+            ans = Math.max(ans, dfs(mem, matrix, i - 1, j));
         }
-        if (j + 1 < matrix[i].length && matrix[i][j + 1] > num) {
-            ans = Math.max(ans, 1 + dfs(mem, matrix, i, j + 1));
+        if (j + 1 < matrix[i].length && matrix[i][j + 1] > matrix[i][j]) {
+            ans = Math.max(ans, dfs(mem, matrix, i, j + 1));
         }
-        if (j - 1 >= 0 && matrix[i][j - 1] > num) {
-            ans = Math.max(ans, 1 + dfs(mem, matrix, i, j - 1));
+        if (j - 1 >= 0 && matrix[i][j - 1] > matrix[i][j]) {
+            ans = Math.max(ans, dfs(mem, matrix, i, j - 1));
         }
-        matrix[i][j] = num;
-        return mem[i][j] = ans;
+        return mem[i][j] = ans + 1;
     }
 
 }

6f05416529a3ac869a3d0d6cd6f1537934fb2347
NoahChen <noah.chen@batechworks.com>
2024-07-05 09:27:51 +0800

    97. Interleaving String alg: dfs + mem

---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/n97/DFSTest.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.dynamic.programming.medium.n97;
+
+import org.junit.jupiter.api.BeforeEach;
+
+final class DFSTest extends SolutionTest {
+
+    @Override
+    @BeforeEach
+    void setUp() {
+        solution = new DFS();
+    }
+
+}

8a040508fd456299263c41aba7777f775b84a429
NoahChen <noah.chen@batechworks.com>
2024-07-05 11:16:17 +0800

    97. Interleaving String alg: dp opt: space compression

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n97/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n97/DP.java
@@ -9,26 +9,21 @@ final class DP implements Solution {
         if (m + n != c3.length) {
             return false;
         }
-        boolean[][] f = new boolean[m + 1][n + 1];
-        f[0][0] = true;
-        for (int i = 1; i <= m; i++) {
-            if (c1[i - 1] != c3[i - 1]) {
-                break;
-            }
-            f[i][0] = true;
-        }
+        boolean[] f = new boolean[n + 1];
+        f[0] = true;
         for (int j = 1; j <= n; j++) {
             if (c2[j - 1] != c3[j - 1]) {
                 break;
             }
-            f[0][j] = true;
+            f[j] = true;
         }
         for (int i = 1; i <= m; i++) {
+            f[0] = c1[i - 1] == c3[i - 1] && f[0];
             for (int j = 1; j <= n; j++) {
-                f[i][j] = (c1[i - 1] == c3[i + j - 1] && f[i - 1][j]) || (c2[j - 1] == c3[i + j - 1] && f[i][j - 1]);
+                f[j] = (c1[i - 1] == c3[i + j - 1] && f[j]) || (c2[j - 1] == c3[i + j - 1] && f[j - 1]);
             }
         }
-        return f[m][n];
+        return f[n];
     }
 
 }

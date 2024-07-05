11c18475bdabf78c2abb7a792b2da18f374d5c04
NoahChen <noah.chen@batechworks.com>
2024-07-05 11:20:23 +0800

    nothing here.

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n97/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n97/Solution.java
@@ -7,6 +7,17 @@ package io.huhu.leetcode.dynamic.programming.medium.n97;
  */
 interface Solution {
 
+    /**
+     * <li>
+     * 0 <= s1.length, s2.length <= 100
+     * </li>
+     * <li>
+     * 0 <= s3.length <= 200
+     * </li>
+     * <li>
+     * s1, s2, s3 consist of lowercase English letters
+     * </li>
+     */
     boolean isInterleave(String s1, String s2, String s3);
 
 }

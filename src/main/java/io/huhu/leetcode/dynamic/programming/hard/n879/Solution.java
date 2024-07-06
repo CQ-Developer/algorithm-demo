4f87eb83a6e13e57c77116b7c43569d0c8bb6b27
Jack <18551681083@163.com>
2024-07-06 10:53:45 +0800

    879. Profitable Schemes sta: init

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n879/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.hard.n879;
+
+/**
+ * <a href="https://leetcode.cn/problems/profitable-schemes/description/">
+ * Profitable Schemes
+ * </a>
+ */
+interface Solution {
+
+    int profitableSchemes(int n, int minProfit, int[] group, int[] profit);
+
+}

078d53577ca9c44be2036b77179b29b9fe92c021
Jack <18551681083@163.com>
2024-07-06 10:11:56 +0800

    474. Ones and Zeroes alg: dp opt: space compression

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n474/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n474/DP.java
@@ -4,29 +4,24 @@ final class DP implements Solution {
 
     @Override
     public int findMaxForm(String[] strs, int m, int n) {
-        int l = strs.length;
-        int[][][] f = new int[l + 1][m + 1][n + 1];
-        for (int i = l - 1; i >= 0; i--) {
+        int[][] f = new int[m + 1][n + 1];
+        for (String s : strs) {
             int z = 0, o = 0;
-            for (int x = 0; x < strs[i].length(); x++) {
-                char c = strs[i].charAt(x);
+            for (char c : s.toCharArray()) {
                 if (c == '0') {
-                    o++;
-                }
-                if (c == '1') {
                     z++;
                 }
+                if (c == '1') {
+                    o++;
+                }
             }
-            for (int j = 0; j <= m; j++) {
-                for (int k = 0; k <= n; k++) {
-                    f[i][j][k] = f[i + 1][j][k];
-                    if (j >= o && k >= z && f[i + 1][j - o][k - z] >= f[i][j][k]) {
-                        f[i][j][k] = f[i + 1][j - o][k - z] + 1;
-                    }
+            for (int i = m; i >= z; i--) {
+                for (int j = n; j >= o; j--) {
+                    f[i][j] = Math.max(f[i][j], f[i - z][j - o] + 1);
                 }
             }
         }
-        return f[0][m][n];
+        return f[m][n];
     }
 
 }

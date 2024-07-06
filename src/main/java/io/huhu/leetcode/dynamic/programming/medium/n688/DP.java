291cfaaaf3b63ae092f6b2bb3d3113a1af56d75e
Jack <18551681083@163.com>
2024-07-06 21:47:04 +0800

    688. Knight Probability in Chessboard alg: dp opt: space compression

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n688/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n688/DP.java
@@ -5,24 +5,27 @@ final class DP implements Solution {
     @Override
     public double knightProbability(int n, int k, int row, int column) {
         int[][] ds = {{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
-        double[][][] f = new double[k + 1][n][n];
-        for (int x = 0; x <= k; x++) {
+        double[][] f = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                f[i][j] = 1;
+            }
+        }
+        for (int x = 1; x <= k; x++) {
+            double[][] c = new double[n][n];
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (x == 0) {
-                        f[0][i][j] = 1;
-                    } else {
-                        for (int[] d : ds) {
-                            int a = i + d[0], b = j + d[1];
-                            if (a >= 0 && a < n && b >= 0 && b < n) {
-                                f[x][i][j] += f[x - 1][a][b] / 8;
-                            }
+                    for (int[] d : ds) {
+                        int a = i + d[0], b = j + d[1];
+                        if (a >= 0 && a < n && b >= 0 && b < n) {
+                            c[i][j] += f[a][b] / 8;
                         }
                     }
                 }
             }
+            f = c;
         }
-        return f[k][row][column];
+        return f[row][column];
     }
 
 }

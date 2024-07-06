4c78df632d53ef2d0188da872ad92003721c2e97
Jack <18551681083@163.com>
2024-07-06 20:45:27 +0800

    688. Knight Probability in Chessboard sta: init

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n688/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.medium.n688;
+
+/**
+ * <a href="https://leetcode.cn/problems/knight-probability-in-chessboard/description/">
+ * Knight Probability in Chessboard
+ * </a>
+ */
+interface Solution {
+
+    double knightProbability(int n, int k, int row, int column);
+
+}

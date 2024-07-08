5b5ecac8cf9061d4309644ef12a7ea2e653ed6f9
NoahChen <noah.chen@batechworks.com>
2024-07-08 16:20:41 +0800

    87. Scramble String alg: dp

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n87/DP.java
@@ -0,0 +1,32 @@
+package io.huhu.leetcode.dynamic.programming.hard.n87;
+
+final class DP implements Solution {
+
+    @Override
+    public boolean isScramble(String s1, String s2) {
+        char[] c1 = s1.toCharArray(), c2 = s2.toCharArray();
+        int m = c1.length;
+        boolean[][][] f = new boolean[m][m][m + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                f[i][j][1] = c1[i] == c2[j];
+            }
+        }
+        for (int n = 2; n <= m; n++) {
+            for (int i = 0; i <= m - n; i++) {
+                for (int j = 0; j <= m - n; j++) {
+                    boolean ans = false;
+                    for (int k = 1; k < n && !ans; k++) {
+                        ans = f[i][j][k] && f[i + k][j + k][n - k];
+                    }
+                    for (int k = 1; k < n && !ans; k++) {
+                        ans = f[i][j + n - k][k] && f[i + k][j][n - k];
+                    }
+                    f[i][j][n] = ans;
+                }
+            }
+        }
+        return f[0][0][m];
+    }
+
+}

9e5f7a6a7a3c0a3850ab90f7e0cdbe3739f7be44
NoahChen <noah.chen@batechworks.com>
2024-07-08 16:34:24 +0800

    nothing here.

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n87/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n87/Solution.java
@@ -7,6 +7,17 @@ package io.huhu.leetcode.dynamic.programming.hard.n87;
  */
 interface Solution {
 
+    /**
+     * <li>
+     * s1.length == s2.length
+     * </li>
+     * <li>
+     * 1 <= s1.length <= 30
+     * </li>
+     * <li>
+     * s1 and s2 consist of lowercase English letters
+     * </li>
+     */
     boolean isScramble(String s1, String s2);
 
 }

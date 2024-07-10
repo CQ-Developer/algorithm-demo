07cdffeee1b7eb19aeb2c995454cbeedfd2549de
Jack <18551681083@163.com>
2024-07-10 18:59:37 +0800

    2141. Maximum Running Time of N Computers alg: greedy + binary search

--- src/main/java/io/huhu/leetcode/binary/search/hard/n2141/Greedy.java
+++ src/main/java/io/huhu/leetcode/binary/search/hard/n2141/Greedy.java
@@ -4,7 +4,41 @@ final class Greedy implements Solution {
 
     @Override
     public long maxRunTime(int n, int[] batteries) {
-        return 0;
+        long sum = 0;
+        int max = 0;
+        for (int battery : batteries) {
+            sum += battery;
+            max = Integer.max(max, battery);
+        }
+        if (sum > (long) max * n) {
+            return sum / n;
+        }
+        int l = 0, r = max, ans = 0;
+        while (l <= r) {
+            int m = l + ((r - l) >> 1);
+            if (f(batteries, n, m)) {
+                ans = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean f(int[] batteries, int n, int m) {
+        long sum = 0;
+        for (int battery : batteries) {
+            if (battery > m) {
+                n--;
+            } else {
+                sum += battery;
+            }
+            if (sum >= (long) n * m) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
--- src/test/java/io/huhu/leetcode/binary/search/hard/n2141/GreedyTest.java
+++ src/test/java/io/huhu/leetcode/binary/search/hard/n2141/GreedyTest.java
@@ -1,8 +1,11 @@
 package io.huhu.leetcode.binary.search.hard.n2141;
 
+import org.junit.jupiter.api.BeforeEach;
+
 class GreedyTest extends SolutionTest {
 
     @Override
+    @BeforeEach
     void setup() {
         solution = new Greedy();
     }

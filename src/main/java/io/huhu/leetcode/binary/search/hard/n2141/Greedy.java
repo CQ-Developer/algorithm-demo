32caf161c59fa1742a79943f57a2e3797402cf7f
NoahChen <noah.chen@batechworks.com>
2024-07-10 17:27:04 +0800

    2141. Maximum Running Time of N Computers alg: greedy sta: init

---
+++ src/main/java/io/huhu/leetcode/binary/search/hard/n2141/Greedy.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.binary.search.hard.n2141;
+
+final class Greedy implements Solution {
+
+    @Override
+    public long maxRunTime(int n, int[] batteries) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/binary/search/hard/n2141/GreedyTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.binary.search.hard.n2141;
+
+class GreedyTest extends SolutionTest {
+
+    @Override
+    void setup() {
+        solution = new Greedy();
+    }
+
+}

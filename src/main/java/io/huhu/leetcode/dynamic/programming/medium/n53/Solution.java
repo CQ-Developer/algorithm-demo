c0d1e56dad086320cf4e54f9912d6fb60fb56540
NoahChen <noah.chen@batechworks.com>
2024-07-11 13:16:41 +0800

    53. Maximum Subarray alg: dp opt: space compression

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n53/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n53/Solution.java
@@ -9,13 +9,9 @@ class Solution {
 
     public int maxSubArray(int[] nums) {
         int ans = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i - 1] + nums[i] > nums[i]) {
-                nums[i] += nums[i - 1];
-            }
-            if (nums[i] > ans) {
-                ans = nums[i];
-            }
+        for (int i = 1, pre = nums[0]; i < nums.length; i++) {
+            pre = Math.max(pre + nums[i], nums[i]);
+            ans = Integer.max(ans, pre);
         }
         return ans;
     }

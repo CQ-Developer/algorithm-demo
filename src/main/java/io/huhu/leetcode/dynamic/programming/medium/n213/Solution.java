91babbfa8e8310b55f4ad09567ddb2344ab93493
Jack <18551681083@163.com>
2024-07-11 19:18:06 +0800

    213. House Robber II alg: dp

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n213/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n213/Solution.java
@@ -3,24 +3,25 @@ package io.huhu.leetcode.dynamic.programming.medium.n213;
 import java.util.Arrays;
 
 /**
- * <a href="https://leetcode.cn/problems/house-robber-ii/description/">打家劫舍II</a>
+ * <a href="https://leetcode.cn/problems/house-robber-ii/description/">
+ * House Robber II
+ * </a>
  */
 class Solution {
 
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 1) {
+        if (nums.length == 1) {
             return nums[0];
         }
-        int a = doRob(Arrays.copyOfRange(nums, 0, n - 1));
-        int b = doRob(Arrays.copyOfRange(nums, 1, n));
-        return Math.max(a, b);
+        int a = doRob(Arrays.copyOfRange(nums, 0, nums.length - 1));
+        int b = doRob(Arrays.copyOfRange(nums, 1, nums.length));
+        return Integer.max(a, b);
     }
 
-    public int doRob(int[] nums) {
+    private int doRob(int[] nums) {
         int p = 0, c = 0;
         for (int n : nums) {
-            int t = Math.max(c, p + n);
+            int t = Integer.max(c, n + p);
             p = c;
             c = t;
         }

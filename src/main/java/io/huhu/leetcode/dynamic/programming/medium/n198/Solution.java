349c3c6728c579205d6944d6b24f68c04796688a
NoahChen <noah.chen@batechworks.com>
2024-07-11 17:16:31 +0800

    198. House Robber alg: dp opt: better

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n198/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n198/Solution.java
@@ -8,13 +8,13 @@ package io.huhu.leetcode.dynamic.programming.medium.n198;
 class Solution {
 
     public int rob(int[] nums) {
-        int ans = nums[0];
-        for (int i = 2, p1 = nums[0], p2 = 0; i <= nums.length; i++) {
-            ans = Integer.max(p1, nums[i - 1] + p2);
-            p2 = p1;
-            p1 = ans;
+        int p = 0, c = 0;
+        for (int num : nums) {
+            int t = Integer.max(c, p + num);
+            p = c;
+            c = t;
         }
-        return ans;
+        return c;
     }
 
 }

716063e5670f80271e4266b773027cf0b4dd5fa0
Jack <18551681083@163.com>
2024-07-11 19:08:25 +0800

    918. Maximum Sum Circular Subarray alg: dp

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n918/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n918/DP.java
@@ -4,7 +4,15 @@ class DP implements Solution {
 
     @Override
     public int maxSubarraySumCircular(int[] nums) {
-        return 0;
+        int max = nums[0], min = nums[0], all = nums[0];
+        for (int i = 1, maxP = nums[0], minP = nums[0]; i < nums.length; i++) {
+            all += nums[i];
+            maxP = Integer.max(nums[i], nums[i] + maxP);
+            max = Integer.max(max, maxP);
+            minP = Integer.min(nums[i], nums[i] + minP);
+            min = Integer.min(min, minP);
+        }
+        return min == all ? max : Integer.max(max, all - min);
     }
 
 }

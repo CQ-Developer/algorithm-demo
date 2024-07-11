bf2212b63d81886e0b29300692f5a9802aa5a641
NoahChen <noah.chen@batechworks.com>
2024-07-11 17:07:32 +0800

    198. House Robber alg: dp

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n198/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n198/Solution.java
@@ -1,31 +1,20 @@
 package io.huhu.leetcode.dynamic.programming.medium.n198;
 
 /**
- * <a href="https://leetcode.cn/problems/house-robber/description/">打家劫舍</a>
+ * <a href="https://leetcode.cn/problems/house-robber/description/">
+ * House Robber
+ * </a>
  */
 class Solution {
 
-    /**
-     * <pre>{@code
-     * public int rob(int[] nums) {
-     *     int[] dp = new int[nums.length + 1];
-     *     dp[0] = 0;
-     *     dp[1] = nums[0];
-     *     for (int i = 2; i < dp.length; i++) {
-     *         dp[i] = Math.max(dp[i - 1], nums[i - 1] + dp[i - 2]);
-     *     }
-     *     return dp[nums.length];
-     * }
-     * }</pre>
-     */
     public int rob(int[] nums) {
-        int p = 0, c = 0;
-        for (int num : nums) {
-            int t = Math.max(c, p + num);
-            p = c;
-            c = t;
+        int n = nums.length;
+        int[] f = new int[n + 1];
+        f[1] = nums[0];
+        for (int i = 2; i <= n; i++) {
+            f[i] = Integer.max(f[i - 1], nums[i - 1] + f[i - 2]);
         }
-        return c;
+        return f[n];
     }
 
 }

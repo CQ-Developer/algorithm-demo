d0a6e421590f093a08f46853a28e2b035bb6b6c0
Jack <18551681083@163.com>
2024-07-11 21:50:52 +0800

    213. House Robber II alg: dp opt: don't copy array

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n213/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n213/Solution.java
@@ -1,7 +1,5 @@
 package io.huhu.leetcode.dynamic.programming.medium.n213;
 
-import java.util.Arrays;
-
 /**
  * <a href="https://leetcode.cn/problems/house-robber-ii/description/">
  * House Robber II
@@ -13,15 +11,24 @@ class Solution {
         if (nums.length == 1) {
             return nums[0];
         }
-        int a = doRob(Arrays.copyOfRange(nums, 0, nums.length - 1));
-        int b = doRob(Arrays.copyOfRange(nums, 1, nums.length));
+        int a = doRob(nums, 0, nums.length - 2);
+        int b = doRob(nums, 1, nums.length - 1);
         return Integer.max(a, b);
     }
 
-    private int doRob(int[] nums) {
+    private int doRob(int[] nums, int l, int r) {
+        if (l > r) {
+            return 0;
+        }
+        if (l == r) {
+            return nums[l];
+        }
+        if (l + 1 == r) {
+            return Integer.max(nums[l], nums[r]);
+        }
         int p = 0, c = 0;
-        for (int n : nums) {
-            int t = Integer.max(c, n + p);
+        while (l <= r) {
+            int t = Integer.max(c, nums[l++] + p);
             p = c;
             c = t;
         }

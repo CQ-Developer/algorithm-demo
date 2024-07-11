98a54502b5ebc99b2fbf16c1152725a72f94e6c0
NoahChen <noah.chen@batechworks.com>
2024-07-11 17:13:10 +0800

    198. House Robber alg: dp opt: space compression

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n198/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n198/Solution.java
@@ -8,13 +8,13 @@ package io.huhu.leetcode.dynamic.programming.medium.n198;
 class Solution {
 
     public int rob(int[] nums) {
-        int n = nums.length;
-        int[] f = new int[n + 1];
-        f[1] = nums[0];
-        for (int i = 2; i <= n; i++) {
-            f[i] = Integer.max(f[i - 1], nums[i - 1] + f[i - 2]);
+        int ans = nums[0];
+        for (int i = 2, p1 = nums[0], p2 = 0; i <= nums.length; i++) {
+            ans = Integer.max(p1, nums[i - 1] + p2);
+            p2 = p1;
+            p1 = ans;
         }
-        return f[n];
+        return ans;
     }
 
 }

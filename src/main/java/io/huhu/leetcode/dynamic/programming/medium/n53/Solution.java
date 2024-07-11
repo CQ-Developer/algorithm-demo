6943ff46526ebfcb671cab6522b48a49470d5339
NoahChen <noah.chen@batechworks.com>
2024-07-11 11:07:42 +0800

    53. Maximum Subarray alg: dp

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n53/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n53/Solution.java
@@ -8,7 +8,16 @@ package io.huhu.leetcode.dynamic.programming.medium.n53;
 class Solution {
 
     public int maxSubArray(int[] nums) {
-        return 0;
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] + nums[i] > nums[i]) {
+                nums[i] += nums[i - 1];
+            }
+            if (nums[i] > ans) {
+                ans = nums[i];
+            }
+        }
+        return ans;
     }
 
 }

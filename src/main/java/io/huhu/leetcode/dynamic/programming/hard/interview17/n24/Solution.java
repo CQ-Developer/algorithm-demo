a456fa1908c9734450da16f70685b6ae4e05fa1e
Jack <18551681083@163.com>
2024-07-12 00:03:33 +0800

    interview17.24. Max Submatrix LCCI

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/interview17/n24/Solution.java
@@ -0,0 +1,15 @@
+package io.huhu.leetcode.dynamic.programming.hard.interview17.n24;
+
+/**
+ * <a href="https://leetcode.cn/problems/max-submatrix-lcci/description/">
+ * Max Submatrix LCCI
+ * </a>
+ */
+class Solution {
+
+    public int[] getMaxMatrix(int[][] matrix) {
+        // todo
+        return null;
+    }
+
+}

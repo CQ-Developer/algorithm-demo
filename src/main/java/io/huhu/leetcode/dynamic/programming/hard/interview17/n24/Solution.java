c4e21e9a774ccb7a17d9b14e3494f9fc9bf42961
NoahChen <noah.chen@batechworks.com>
2024-07-12 09:18:23 +0800

    interview17.24. Max Submatrix LCCI alg: dp

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/interview17/n24/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/interview17/n24/Solution.java
@@ -8,8 +8,31 @@ package io.huhu.leetcode.dynamic.programming.hard.interview17.n24;
 class Solution {
 
     public int[] getMaxMatrix(int[][] matrix) {
-        // todo
-        return null;
+        int a = 0, b = 0, c = 0, d = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < matrix.length; i++) {
+            int[] nums = new int[matrix[i].length];
+            for (int j = i; j < matrix.length; j++) {
+                int pre = Integer.MIN_VALUE;
+                for (int l = 0, r = 0; r < matrix[j].length; r++) {
+                    nums[r] += matrix[j][r];
+                    if (pre >= 0) {
+                        pre += nums[r];
+                    } else {
+                        pre = nums[r];
+                        l = r;
+                    }
+                    if (pre > max) {
+                        max = pre;
+                        a = i;
+                        b = l;
+                        c = j;
+                        d = r;
+                    }
+                }
+            }
+        }
+        return new int[]{a, b, c, d};
     }
 
 }

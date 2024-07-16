e373a4da5cdbb6a6bc4c94390379d04aecbde1dc
NoahChen <noah.chen@batechworks.com>
2024-07-16 09:12:31 +0800

    可以选择数组连续一段反转一次的情况下, 子数组的最大累加和 dp

--- src/main/java/io/huhu/leetcode/classic/other/n2/DP.java
+++ src/main/java/io/huhu/leetcode/classic/other/n2/DP.java
@@ -4,7 +4,19 @@ class DP implements Code {
 
     @Override
     public int maximumSumOfSubarray(int[] nums) {
-        return 0;
+        int n = nums.length;
+        int[] start = new int[n];
+        start[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            start[i] = Integer.max(nums[i], nums[i] + start[i + 1]);
+        }
+        int ans = start[0], end = nums[0], max = nums[0];
+        for (int i = 1; i < n; i++) {
+            ans = Integer.max(ans, max + start[i]);
+            end = Integer.max(nums[i], nums[i] + end);
+            max = Integer.max(max, end);
+        }
+        return Integer.max(ans, max);
     }
 
 }

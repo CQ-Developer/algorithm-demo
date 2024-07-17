51537bcd22b5017d7199e7301a6e90784090b421
NoahChen <noah.chen@batechworks.com>
2024-07-17 09:25:39 +0800

    300.  Longest Increasing Subsequence alg: dp

---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/n300/DPTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.dynamic.programming.medium.n300;
+
+class DPTest extends SolutionTest {
+
+    @Override
+    protected Solution getSolution() {
+        return new DP();
+    }
+
+}

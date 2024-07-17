dbd758f1553e480432aa43e039ad90dac8ae146e
NoahChen <noah.chen@batechworks.com>
2024-07-17 16:22:23 +0800

    300. Longest Increasing Subsequence alg: binary search

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n300/BinarySearch.java
@@ -0,0 +1,34 @@
+package io.huhu.leetcode.dynamic.programming.medium.n300;
+
+class BinarySearch implements Solution {
+
+    @Override
+    public int lengthOfLIS(int[] nums) {
+        int len = 0;
+        int[] f = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int j = binarySearch(f, len, nums[i]);
+            if (j == -1) {
+                f[len++] = nums[i];
+            } else {
+                f[j] = nums[i];
+            }
+        }
+        return len;
+    }
+
+    private int binarySearch(int[] f, int len, int num) {
+        int l = 0, r = len - 1, ans = -1;
+        while (l <= r) {
+            int m = (l + r) >> 1;
+            if (num <= f[m]) {
+                ans = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return ans;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/n300/BinarySearchTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.dynamic.programming.medium.n300;
+
+class BinarySearchTest extends SolutionTest {
+
+    @Override
+    protected Solution getSolution() {
+        return new BinarySearch();
+    }
+
+}

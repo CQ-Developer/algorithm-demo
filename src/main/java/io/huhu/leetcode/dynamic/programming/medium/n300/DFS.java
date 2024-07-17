c44ae14ae51f6c828c02b28ed95ee79724c6850f
NoahChen <noah.chen@batechworks.com>
2024-07-17 15:53:39 +0800

    300. Longest Increasing Subsequence alg: dfs + mem

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n300/DFS.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n300/DFS.java
@@ -5,20 +5,24 @@ class DFS implements Solution {
     @Override
     public int lengthOfLIS(int[] nums) {
         int ans = 0;
+        int[] f = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            ans = Integer.max(ans, dfs(nums, i));
+            ans = Integer.max(ans, dfs(nums, i, f));
         }
         return ans;
     }
 
-    private int dfs(int[] nums, int i) {
+    private int dfs(int[] nums, int i, int[] f) {
+        if (f[i] != 0) {
+            return f[i];
+        }
         int ans = 0;
         for (int j = 0; j < i; j++) {
             if (nums[j] < nums[i]) {
-                ans = Integer.max(ans, dfs(nums, j));
+                ans = Integer.max(ans, dfs(nums, j, f));
             }
         }
-        return ans + 1;
+        return f[i] = ans + 1;
     }
 
 }

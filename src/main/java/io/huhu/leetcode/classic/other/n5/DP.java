133c68ab958e103092f7e1c9c9904efc5f52ea93
NoahChen <noah.chen@batechworks.com>
2024-07-23 15:05:02 +0800

    P1048. 采药 alg: dp opt: space compression

--- src/main/java/io/huhu/leetcode/classic/other/n5/DP.java
+++ src/main/java/io/huhu/leetcode/classic/other/n5/DP.java
@@ -4,16 +4,13 @@ class DP implements Code {
 
     @Override
     public int solution(int[][] arr, int t) {
-        int[][] dp = new int[arr.length + 1][t + 1];
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 0; j < dp[i].length; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j - arr[i - 1][0] >= 0) {
-                    dp[i][j] = Integer.max(dp[i][j], dp[i - 1][j - arr[i - 1][0]] + arr[i - 1][1]);
-                }
+        int[] dp = new int[t + 1];
+        for (int i = 1; i <= arr.length; i++) {
+            for (int j = t; j >= arr[i - 1][0]; j--) {
+                dp[j] = Integer.max(dp[j], dp[j - arr[i - 1][0]] + arr[i - 1][1]);
             }
         }
-        return dp[arr.length][t];
+        return dp[t];
     }
 
 }

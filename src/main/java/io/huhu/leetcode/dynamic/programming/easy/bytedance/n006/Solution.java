b42d5b4e52807e7bb8c4c79fb5f9d2f09d0c8f76
chen <18551681083@163.com>
2024-07-24 19:06:29 +0800

    nothing here.

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/easy/bytedance/n006/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.easy.bytedance.n006;
+
+/**
+ * <a href="https://leetcode.cn/problems/tJau2o/">
+ * 夏季特惠
+ * </a>
+ */
+interface Solution {
+
+    int buy(int n, int x, int[] price, int[] discount, int[] happy);
+
+}

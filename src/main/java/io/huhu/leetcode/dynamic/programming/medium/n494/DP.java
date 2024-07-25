cba95ade069db5c44b1ec6f7ee6462c18ab9f6f3
NoahChen <noah.chen@batechworks.com>
2024-07-25 17:47:20 +0800

    nothing here

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n494/DP.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.dynamic.programming.medium.n494;
+
+class DP implements Solution {
+
+    @Override
+    public int findTargetSumWays(int[] nums, int target) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}

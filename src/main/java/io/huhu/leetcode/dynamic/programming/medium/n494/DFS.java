1edb8582cb106ada644aa4018afb9a9fc8268c07
NoahChen <noah.chen@batechworks.com>
2024-07-25 13:57:23 +0800

    494. Target Sum alg: dfs opt: memory search

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n494/DFS.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n494/DFS.java
@@ -1,17 +1,26 @@
 package io.huhu.leetcode.dynamic.programming.medium.n494;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class DFS implements Solution {
 
     @Override
     public int findTargetSumWays(int[] nums, int target) {
-        return dfs(nums, 0, target);
+        return dfs(nums, 0, target, new HashMap<>());
     }
 
-    private int dfs(int[] nums, int i, int j) {
+    private int dfs(int[] nums, int i, int j, Map<Integer, Map<Integer, Integer>> mem) {
         if (i == nums.length) {
             return j == 0 ? 1 : 0;
         }
-        return dfs(nums, i + 1, j + nums[i]) + dfs(nums, i + 1, j - nums[i]);
+        if (mem.containsKey(i) && mem.get(i).containsKey(j)) {
+            return mem.get(i).get(j);
+        }
+        int ans = dfs(nums, i + 1, j + nums[i], mem) + dfs(nums, i + 1, j - nums[i], mem);
+        mem.putIfAbsent(i, new HashMap<>());
+        mem.get(i).put(j, ans);
+        return ans;
     }
 
 }
---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/n494/DPTest.java
@@ -0,0 +1,9 @@
+package io.huhu.leetcode.dynamic.programming.medium.n494;
+
+class DPTest extends SolutionTest {
+
+    @Override
+    protected Solution getSolution() {
+        return new DP();
+    }
+}

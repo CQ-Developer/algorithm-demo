daed95d3097f5c99f577ae7309de55a25c456e3a
NoahChen <noah.chen@batechworks.com>
2024-07-25 09:08:25 +0800

    nothing here.

--- src/main/java/io/huhu/leetcode/classic/other/n5/DP.java
+++ src/main/java/io/huhu/leetcode/classic/other/n5/DP.java
@@ -5,9 +5,9 @@ class DP implements Code {
     @Override
     public int solution(int[][] arr, int t) {
         int[] dp = new int[t + 1];
-        for (int i = 1; i <= arr.length; i++) {
-            for (int j = t; j >= arr[i - 1][0]; j--) {
-                dp[j] = Integer.max(dp[j], dp[j - arr[i - 1][0]] + arr[i - 1][1]);
+        for (int[] nums : arr) {
+            for (int j = t; j >= nums[0]; j--) {
+                dp[j] = Integer.max(dp[j], dp[j - nums[0]] + nums[1]);
             }
         }
         return dp[t];

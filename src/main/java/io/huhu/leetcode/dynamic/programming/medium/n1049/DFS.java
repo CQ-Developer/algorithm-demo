ec406f09859407ea4516305edb3fc46baa96383f
NoahChen <noah.chen@batechworks.com>
2024-07-26 17:30:33 +0800

    nothing here.

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n1049/DFS.java
@@ -0,0 +1,14 @@
+package io.huhu.leetcode.dynamic.programming.medium.n1049;
+
+class DFS implements Solution {
+
+    @Override
+    public int lastStoneWeightII(int[] stones) {
+        return 0;
+    }
+
+    private int dfs(int[] stones, boolean[] used, int i, int sum) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/n1049/DFSTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.dynamic.programming.medium.n1049;
+
+class DFSTest extends SolutionTest {
+
+    @Override
+    protected Solution getSolution() {
+        return new DFS();
+    }
+
+}

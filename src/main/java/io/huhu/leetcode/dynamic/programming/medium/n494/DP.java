cbd22223f0b55f550ce3aaac7e6c371304597051
NoahChen <noah.chen@batechworks.com>
2024-07-26 08:47:01 +0800

    494. Target Sum alg: dp

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n494/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n494/DP.java
@@ -4,7 +4,27 @@ class DP implements Solution {
 
     @Override
     public int findTargetSumWays(int[] nums, int target) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        int s = 0;
+        for (int num : nums) {
+            s += num;
+        }
+        if (target < -s || target > s) {
+            return 0;
+        }
+        int n = nums.length, m = s << 1;
+        int[][] dp = new int[n + 1][m + 1];
+        dp[n][target + s] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = -s; j <= s; j++) {
+                if (j + nums[i] + s <= m) {
+                    dp[i][j + s] += dp[i + 1][j + nums[i] + s];
+                }
+                if (j - nums[i] + s >= 0) {
+                    dp[i][j + s] += dp[i + 1][j - nums[i] + s];
+                }
+            }
+        }
+        return dp[0][s];
     }
 
 }

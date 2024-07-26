657b55461812ea912b9cad8ab67b400d5383ee1f
NoahChen <noah.chen@batechworks.com>
2024-07-26 10:31:52 +0800

    494. Target Sum alg: dp opt: Knapsack problem + space compression

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n494/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n494/DP.java
@@ -11,21 +11,18 @@ class DP implements Solution {
         if (s < target || ((target & 1) ^ (s & 1)) == 1) {
             return 0;
         }
-        int n = nums.length, m = (target + s) >> 1;
+        int m = (target + s) >> 1;
         if (m < 0) {
             return 0;
         }
-        int[][] dp = new int[n + 1][m + 1];
-        dp[0][0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j - nums[i - 1] >= 0) {
-                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
-                }
+        int[] dp = new int[m + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int j = m; j >= num; j--) {
+                dp[j] += dp[j - num];
             }
         }
-        return dp[n][m];
+        return dp[m];
     }
 
 }

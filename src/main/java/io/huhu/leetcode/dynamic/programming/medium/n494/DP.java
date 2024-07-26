f68e4a579e089cd06f93ab43117eb005e0f5db31
NoahChen <noah.chen@batechworks.com>
2024-07-26 10:04:57 +0800

    494. Target Sum alg: dp opt: 01背包

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n494/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n494/DP.java
@@ -8,23 +8,24 @@ class DP implements Solution {
         for (int num : nums) {
             s += num;
         }
-        if (target < -s || target > s) {
+        if (s < target || ((target & 1) ^ (s & 1)) == 1) {
             return 0;
         }
-        int n = nums.length, m = s << 1;
+        int n = nums.length, m = (target + s) >> 1;
+        if (m < 0) {
+            return 0;
+        }
         int[][] dp = new int[n + 1][m + 1];
-        dp[n][target + s] = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = -s; j <= s; j++) {
-                if (j + nums[i] + s <= m) {
-                    dp[i][j + s] += dp[i + 1][j + nums[i] + s];
-                }
-                if (j - nums[i] + s >= 0) {
-                    dp[i][j + s] += dp[i + 1][j - nums[i] + s];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j - nums[i - 1] >= 0) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
                 }
             }
         }
-        return dp[0][s];
+        return dp[n][m];
     }
 
 }

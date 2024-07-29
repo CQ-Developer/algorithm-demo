08d2715623594334a6c7b009aec1fe763f6990c2
NoahChen <noah.chen@batechworks.com>
2024-07-29 17:12:52 +0800

    nothing here.

--- src/main/java/io/huhu/leetcode/classic/other/n6/Main.java
+++ src/main/java/io/huhu/leetcode/classic/other/n6/Main.java
@@ -11,13 +11,31 @@ class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // n -> 预算
         int n = scanner.nextInt(), m = scanner.nextInt();
-        int[] v = new int[m], p = new int[m], q = new int[m];
-        for (int i = 0; i < m; i++) {
+        // v -> 价格
+        // p -> 重要度
+        // q -> 主件
+        int[] v = new int[m + 1], p = new int[m + 1], fan = new int[m + 1];
+        boolean[] main = new boolean[m + 1];
+        int[][] follower = new int[m + 1][2];
+        for (int i = 1; i <= m; i++) {
             v[i] = scanner.nextInt();
             p[i] = scanner.nextInt();
-            q[i] = scanner.nextInt();
+            int q = scanner.nextInt();
+            // todo
+            p[i] *= v[i];
+            main[i] = q == 0;
+            if (!main[i]) {
+                follower[q][fan[q]++] = i;
+            }
         }
+        int ans = dfs(n, m, v, p, main, fan, follower);
+        System.out.println(ans);
+    }
+
+    private static int dfs(int n, int m, int[] v, int[] p, boolean[] main, int[] fan, int[][] follower) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 }

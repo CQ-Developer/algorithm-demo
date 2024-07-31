e83b19e222eb9eaa1b43f161a9f6437c056dddb1
NoahChen <noah.chen@batechworks.com>
2024-07-31 18:03:17 +0800

    2218. Maximum Value of K Coins From Piles alg: dp opt: space compression

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n2218/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n2218/DP.java
@@ -6,24 +6,20 @@ class DP implements Solution {
 
     @Override
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-        int n = piles.size();
-        int[][] dp = new int[n + 1][k + 1];
-        for (int i = 1; i <= n; i++) {
-            List<Integer> list = piles.get(i - 1);
-            int m = Integer.min(list.size(), k);
-            int[] sum = new int[m + 1];
-            for (int j = 0, s = 0; j < m; j++) {
-                s += list.get(j);
-                sum[j + 1] = s;
+        int[] dp = new int[k + 1];
+        for (List<Integer> pile : piles) {
+            int n = Integer.min(pile.size(), k);
+            int[] sum = new int[n + 1];
+            for (int j = 0, s = 0; j < n; j++) {
+                sum[j + 1] = s += pile.get(j);
             }
-            for (int j = 0; j <= k; j++) {
-                dp[i][j] = dp[i - 1][j];
-                for (int l = 1; l <= Integer.min(j, m); l++) {
-                    dp[i][j] = Integer.max(dp[i][j], dp[i - 1][j - l] + sum[l]);
+            for (int j = k; j > 0; j--) {
+                for (int l = 1; l <= Integer.min(j, n); l++) {
+                    dp[j] = Integer.max(dp[j], dp[j - l] + sum[l]);
                 }
             }
         }
-        return dp[n][k];
+        return dp[k];
     }
 
 }

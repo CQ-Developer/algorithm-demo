92c45b226c3b92bc1dbdcb0a038a4b1f2389cdcd
NoahChen <noah.chen@batechworks.com>
2024-08-01 09:23:40 +0800

    P1616. 疯狂的采药 alg: dp + sc

--- src/main/java/io/huhu/leetcode/classic/other/n8/DP.java
+++ src/main/java/io/huhu/leetcode/classic/other/n8/DP.java
@@ -10,21 +10,18 @@ class DP {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt(), n = sc.nextInt();
-        int[] t = new int[n + 1], v = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
+        int[] t = new int[n], v = new int[n];
+        for (int i = 0; i < n; i++) {
             t[i] = sc.nextInt();
             v[i] = sc.nextInt();
         }
-        long[][] dp = new long[n + 1][m + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j - t[i] >= 0) {
-                    dp[i][j] = Long.max(dp[i][j], dp[i][j - t[i]] + v[i]);
-                }
+        long[] dp = new long[m + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = t[i]; j <= m; j++) {
+                dp[j] = Long.max(dp[j], dp[j - t[i]] + v[i]);
             }
         }
-        System.out.println(dp[n][m]);
+        System.out.println(dp[m]);
     }
 
 }

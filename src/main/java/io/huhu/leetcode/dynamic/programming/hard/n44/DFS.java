67e85c35e718f1b9da76da0354535568bdc6a022
chen <18551681083@163.com>
2024-08-02 22:59:21 +0800

    n44. Wildcard Matching alg: dfs opt: mem search

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n44/DFS.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n44/DFS.java
@@ -4,25 +4,32 @@ class DFS implements Solution {
 
     @Override
     public boolean isMatch(String s, String p) {
-        return dfs(s.toCharArray(), p.toCharArray(), 0, 0);
+        char[] c1 = s.toCharArray(), c2 = p.toCharArray();
+        return dfs(c1, c2, 0, 0, new int[c1.length + 1][c2.length + 1]);
     }
 
-    private boolean dfs(char[] s, char[] p, int i, int j) {
+    private boolean dfs(char[] s, char[] p, int i, int j, int[][] f) {
+        if (f[i][j] != 0) {
+            return f[i][j] == 1;
+        }
+        boolean ans;
         if (i == s.length) {
             if (j == p.length) {
-                return true;
+                ans = true;
             } else {
-                return p[j] == '*' && dfs(s, p, i, j + 1);
+                ans = p[j] == '*' && dfs(s, p, i, j + 1, f);
             }
         } else if (j == p.length) {
-            return false;
+            ans = false;
         } else {
             if (p[j] != '*') {
-                return (p[j] == s[i] || p[j] == '?') && dfs(s, p, i + 1, j + 1);
+                ans = (p[j] == s[i] || p[j] == '?') && dfs(s, p, i + 1, j + 1, f);
             } else {
-                return dfs(s, p, i + 1, j) || dfs(s, p, i, j + 1);
+                ans = dfs(s, p, i + 1, j, f) || dfs(s, p, i, j + 1, f);
             }
         }
+        f[i][j] = ans ? 1 : -1;
+        return ans;
     }
 
 }

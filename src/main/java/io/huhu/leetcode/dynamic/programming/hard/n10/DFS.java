42313bd104c23f4803eb17497925b66001c2bb75
NoahChen <noah.chen@batechworks.com>
2024-08-02 09:11:48 +0800

    10. Regular Expression Matching alg: dfs opt: mem search

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n10/DFS.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n10/DFS.java
@@ -4,22 +4,28 @@ class DFS implements Solution {
 
     @Override
     public boolean isMatch(String s, String p) {
-        return dfs(s.toCharArray(), p.toCharArray(), 0, 0);
+        char[] c1 = s.toCharArray(), c2 = p.toCharArray();
+        return dfs(c1, c2, new int[c1.length + 1][c2.length + 1], 0, 0);
     }
 
-    private boolean dfs(char[] s, char[] p, int i, int j) {
+    private boolean dfs(char[] s, char[] p, int[][] f, int i, int j) {
+        if (f[i][j] != 0) {
+            return f[i][j] == 1;
+        }
+        boolean ans;
         if (i == s.length) {
-            return j == p.length || j + 1 < p.length && p[j + 1] == '*' && dfs(s, p, i, j + 2);
+            ans = j == p.length || j + 1 < p.length && p[j + 1] == '*' && dfs(s, p, f, i, j + 2);
+        } else if (j == p.length) {
+            ans = false;
+        } else {
+            if (j + 1 == p.length || p[j + 1] != '*') {
+                ans = (s[i] == p[j] || p[j] == '.') && dfs(s, p, f, i + 1, j + 1);
+            } else {
+                ans = dfs(s, p, f, i, j + 2) || ((s[i] == p[j] || p[j] == '.') && dfs(s, p, f, i + 1, j));
+            }
         }
-        if (j == p.length) {
-            return false;
-        }
-        if (j + 1 == p.length || p[j + 1] != '*') {
-            return (s[i] == p[j] || p[j] == '.') && dfs(s, p, i + 1, j + 1);
-        }
-        boolean a = dfs(s, p, i, j + 2);
-        boolean b = (s[i] == p[j] || p[j] == '.') && dfs(s, p, i + 1, j);
-        return a || b;
+        f[i][j] = ans ? 1 : -1;
+        return ans;
     }
 
 }

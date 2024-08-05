f705495708297f2486979e93ee3e48f86a03d78f
NoahChen <noah.chen@batechworks.com>
2024-08-05 09:03:45 +0800

    P1776. 宝物筛选 alg: dp opt: space compression

--- src/main/java/io/huhu/leetcode/classic/other/n9/DP.java
+++ src/main/java/io/huhu/leetcode/classic/other/n9/DP.java
@@ -10,24 +10,21 @@ class DP {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), W = sc.nextInt();
-        int[] v = new int[n + 1], w = new int[n + 1], m = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
+        int[] v = new int[n], w = new int[n], m = new int[n];
+        for (int i = 0; i < n; i++) {
             v[i] = sc.nextInt();
             w[i] = sc.nextInt();
             m[i] = sc.nextInt();
         }
-        int[][] dp = new int[n + 1][W + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= W; j++) {
-                dp[i][j] = dp[i - 1][j];
-                for (int k = 1; k <= m[i]; k++) {
-                    if (j - w[i] * k >= 0) {
-                        dp[i][j] = Integer.max(dp[i][j], dp[i - 1][j - w[i] * k] + v[i] * k);
-                    }
+        int[] dp = new int[W + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = W; j >= 0; j--) {
+                for (int k = 1; k <= m[i] && j >= w[i] * k; k++) {
+                    dp[j] = Integer.max(dp[j], dp[j - w[i] * k] + v[i] * k);
                 }
             }
         }
-        System.out.println(dp[n][W]);
+        System.out.println(dp[W]);
     }
 
 }

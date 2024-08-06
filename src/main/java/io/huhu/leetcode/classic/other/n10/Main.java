29052081c33964fbb3e2d1441b84f2e276caeadb
NoahChen <noah.chen@batechworks.com>
2024-08-06 17:03:25 +0800

    P1833. 樱花 alg: dp

--- src/main/java/io/huhu/leetcode/classic/other/n10/Main.java
+++ src/main/java/io/huhu/leetcode/classic/other/n10/Main.java
@@ -1,7 +1,5 @@
 package io.huhu.leetcode.classic.other.n10;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -9,33 +7,35 @@ import java.util.Scanner;
  */
 class Main {
 
+    static final int[] C = new int[100001];
+    static final int[] V = new int[100001];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String t1 = sc.next(), t2 = sc.next();
-        int t = toMin(t2) - toMin(t1), n = sc.nextInt();
-        List<Integer> costs = new ArrayList<>(), values = new ArrayList<>();
+        int time = toMin(t2) - toMin(t1), n = sc.nextInt(), m = 0;
         for (int i = 1; i <= n; i++) {
-            int c = sc.nextInt(), v = sc.nextInt(), cnt = sc.nextInt();
-            if (cnt == 0) {
-                cnt = 1000;
+            int c = sc.nextInt(), v = sc.nextInt(), k = sc.nextInt();
+            if (k == 0) {
+                k = 1000;
             }
-            for (int j = 1; j <= cnt; j <<= 1) {
-                costs.add(j * c);
-                values.add(j * v);
-                cnt -= j;
+            for (int b = 1; b <= k; b <<= 1) {
+                C[++m] = c * b;
+                V[m] = v * b;
+                k -= b;
             }
-            if (cnt > 0) {
-                costs.add(cnt * c);
-                values.add(cnt * v);
-            }
-        }
-        int[] dp = new int[t + 1];
-        for (int i = 0; i < values.size(); i++) {
-            for (int j = t; j >= costs.get(i); j--) {
-                dp[j] = Integer.max(dp[j], dp[j - costs.get(i)] + values.get(i));
+            if (k > 0) {
+                C[++m] = c * k;
+                V[m] = v * k;
             }
         }
-        System.out.println(dp[t]);
+        int[] dp = new int[time + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = time; j >= C[i]; j--) {
+                dp[j] = Integer.max(dp[j], dp[j - C[i]] + V[i]);
+            }
+        }
+        System.out.println(dp[time]);
     }
 
     private static int toMin(String s) {

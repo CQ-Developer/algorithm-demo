0e0fb2178563766dc0b78eb3740a68358b38fe22
NoahChen <noah.chen@batechworks.com>
2024-08-07 09:09:08 +0800

    1312.Insertion Steps to Make a String Palindrome

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n1312/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.hard.n1312;
+
+/**
+ * <a href="https://leetcode.cn/problems/minimum-insertion-steps-to-make-a-string-palindrome/description/">Minimum
+ * Insertion Steps to Make a String Palindrome
+ * </a>
+ */
+interface Solution {
+
+    int minInsertions(String s);
+
+}

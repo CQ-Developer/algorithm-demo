082fb550dc985626ed519d88fdac8c46bc899aee
NoahChen <noah.chen@batechworks.com>
2024-08-08 13:42:10 +0800

    1039. Minimum Score Triangulation of Polygon alg: dp

---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/n1039/DPTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.dynamic.programming.medium.n1039;
+
+class DPTest extends SolutionTest {
+
+    @Override
+    protected Solution getSolution() {
+        return new DP();
+    }
+
+}

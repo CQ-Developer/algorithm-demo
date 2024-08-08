a8cf41e59987211286c06ead23e0b1d2771a5f09
NoahChen <noah.chen@batechworks.com>
2024-08-08 08:45:59 +0800

    486. Predict the Winner

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n486/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.medium.n486;
+
+/**
+ * <a href="https://leetcode.cn/problems/predict-the-winner/description/">
+ * Predict the Winner
+ * </a>
+ */
+interface Solution {
+
+    boolean predictTheWinner(int[] nums);
+
+}
---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/n486/SolutionTest.java
@@ -0,0 +1,30 @@
+package io.huhu.leetcode.dynamic.programming.medium.n486;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+abstract class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setup() {
+        solution = getSolution();
+    }
+
+    protected abstract Solution getSolution();
+
+    @Test
+    void test_1() {
+        int[] nums = {1, 5, 2};
+        Assertions.assertFalse(solution.predictTheWinner(nums));
+    }
+
+    @Test
+    void test_2() {
+        int[] nums = {1, 5, 233, 7};
+        Assertions.assertTrue(solution.predictTheWinner(nums));
+    }
+
+}

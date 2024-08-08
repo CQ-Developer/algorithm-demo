37181bc9bc839f8aab1ae40a41a1efc267bd160f
NoahChen <noah.chen@batechworks.com>
2024-08-08 17:17:30 +0800

    1547. Minimum Cost to Cut a Stick alg: dp

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n1547/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n1547/DP.java
@@ -7,20 +7,24 @@ public class DP implements Solution {
     @Override
     public int minCost(int n, int[] cuts) {
         Arrays.sort(cuts);
-        int k = cuts.length;
-        int[] arr = new int[k + 2];
-        System.arraycopy(cuts, 0, arr, 1, k);
-        arr[k + 1] = n;
-        int[][] dp = new int[k + 2][k + 2];
-        for (int l = k; l >= 1; l--) {
-            for (int r = l; r <= k; r++) {
-                dp[l][r] = Integer.MAX_VALUE;
-                for (int m = l; m <= r; m++) {
-                    dp[l][r] = Math.min(dp[l][r], dp[l][m - 1] + dp[m + 1][r] + arr[r + 1] - arr[l - 1]);
+        int m = cuts.length;
+        int[] arr = new int[m + 2];
+        System.arraycopy(cuts, 0, arr, 1, m);
+        arr[m + 1] = n;
+        int[][] dp = new int[m + 2][m + 2];
+        for (int i = 1; i <= m; i++) {
+            dp[i][i] = arr[i + 1] - arr[i - 1];
+        }
+        for (int l = m - 1; l >= 1; l--) {
+            for (int r = l + 1; r <= m; r++) {
+                int t = Integer.MAX_VALUE;
+                for (int k = l; k <= r; k++) {
+                    t = Math.min(t, dp[l][k - 1] + dp[k + 1][r]);
+                    dp[l][r] = arr[r + 1] - arr[l - 1] + t;
                 }
             }
         }
-        return dp[1][k];
+        return dp[1][m];
     }
 
 }

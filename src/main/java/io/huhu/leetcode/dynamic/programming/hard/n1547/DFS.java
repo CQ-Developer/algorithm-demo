773e851fd46f7b469b97ebc4fafa27f091e5d653
NoahChen <noah.chen@batechworks.com>
2024-08-08 16:26:07 +0800

    1547. Minimum Cost to Cut a Stick alg: dfs opt: memory search

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n1547/DFS.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n1547/DFS.java
@@ -11,18 +11,25 @@ class DFS implements Solution {
         int[] arr = new int[m + 2];
         System.arraycopy(cuts, 0, arr, 1, m);
         arr[m + 1] = n;
-        return dfs(arr, 1, m);
+        int[][] f = new int[m + 1][m + 1];
+        for (int[] a : f) {
+            Arrays.fill(a, -1);
+        }
+        return dfs(arr, 1, m, f);
     }
 
-    private int dfs(int[] arr, int l, int r) {
+    private int dfs(int[] arr, int l, int r, int[][] f) {
         if (l > r) {
             return 0;
         }
-        int ans = Integer.MAX_VALUE;
-        for (int m = l; m <= r; m++) {
-            ans = Math.min(ans, dfs(arr, l, m - 1) + dfs(arr, m + 1, r) + arr[r + 1] - arr[l - 1]);
+        if (f[l][r] != -1) {
+            return f[l][r];
         }
-        return ans;
+        f[l][r] = Integer.MAX_VALUE;
+        for (int m = l; m <= r; m++) {
+            f[l][r] = Math.min(f[l][r], dfs(arr, l, m - 1, f) + dfs(arr, m + 1, r, f) + arr[r + 1] - arr[l - 1]);
+        }
+        return f[l][r];
     }
 
 }

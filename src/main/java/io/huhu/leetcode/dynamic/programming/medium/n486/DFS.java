9b5f1be8735bec08ec9868debf2cd34c52a7c8d0
NoahChen <noah.chen@batechworks.com>
2024-08-08 10:52:19 +0800

    486. Predict the Winner alg: dfs opt: mem search

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n486/DFS.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n486/DFS.java
@@ -1,5 +1,7 @@
 package io.huhu.leetcode.dynamic.programming.medium.n486;
 
+import java.util.Arrays;
+
 class DFS implements Solution {
 
     @Override
@@ -8,7 +10,12 @@ class DFS implements Solution {
         for (int num : nums) {
             sum += num;
         }
-        int p1 = dfs(nums, 0, nums.length - 1);
+        int n = nums.length;
+        int[][] f = new int[n][n];
+        for (int[] a : f) {
+            Arrays.fill(a, -1);
+        }
+        int p1 = dfs(nums, 0, nums.length - 1, f);
         int p2 = sum - p1;
         return p1 >= p2;
     }
@@ -16,7 +23,7 @@ class DFS implements Solution {
     /**
      * p1的选择
      */
-    private int dfs(int[] nums, int l, int r) {
+    private int dfs(int[] nums, int l, int r, int[][] f) {
         // 仅剩下一个数, p1一定选择
         if (l == r) {
             return nums[l];
@@ -25,12 +32,16 @@ class DFS implements Solution {
         if (l + 1 == r) {
             return Math.max(nums[l], nums[r]);
         }
+        // 缓存命中
+        if (f[l][r] != -1) {
+            return f[l][r];
+        }
         // 当前轮p1要不选择l, 要不选择r
         // 下一轮是p2选择, p2一定希望最大结果, 那么p1必然获得最小结果
-        int a = nums[l] + Math.min(dfs(nums, l + 1, r - 1), dfs(nums, l + 2, r));
-        int b = nums[r] + Math.min(dfs(nums, l + 1, r - 1), dfs(nums, l, r - 2));
+        int a = nums[l] + Math.min(dfs(nums, l + 1, r - 1, f), dfs(nums, l + 2, r, f));
+        int b = nums[r] + Math.min(dfs(nums, l + 1, r - 1, f), dfs(nums, l, r - 2, f));
         // p1选择最大结果
-        return Math.max(a, b);
+        return f[l][r] = Math.max(a, b);
     }
 
 }

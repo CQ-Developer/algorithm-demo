ae63f9dfde2280de7ceee9639e6fb5777bac3478
NoahChen <noah.chen@batechworks.com>
2024-08-08 15:17:52 +0800

    1547. Minimum Cost to Cut a Stick

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n1547/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.hard.n1547;
+
+/**
+ * <a href="https://leetcode.cn/problems/minimum-cost-to-cut-a-stick/description/">
+ * Minimum Cost to Cut a Stick
+ * </a>
+ */
+interface Solution {
+
+    int minCost(int n, int[] cuts);
+
+}

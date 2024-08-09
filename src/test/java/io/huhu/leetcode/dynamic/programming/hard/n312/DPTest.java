4f5405a9fd9dec22b6d37cbf81733e3a208ac18f
NoahChen <noah.chen@batechworks.com>
2024-08-09 16:18:45 +0800

    312. Burst Balloons alg: dynamic programming

---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/hard/n312/DPTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.dynamic.programming.hard.n312;
+
+class DPTest extends SolutionTest {
+
+    @Override
+    protected Solution getSolution() {
+        return new DP();
+    }
+
+}

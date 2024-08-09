6c5706c8d417611941e2af09114e4345696c574d
NoahChen <noah.chen@batechworks.com>
2024-08-09 15:57:13 +0800

    312. Burst Balloons alg: depth first search ps: TLA

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n312/DFS.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n312/DFS.java
@@ -8,32 +8,21 @@ class DFS implements Solution {
         int[] arr = new int[n + 2];
         arr[0] = arr[n + 1] = 1;
         System.arraycopy(nums, 0, arr, 1, n);
-        return dfs(arr, new boolean[n + 2], nums.length);
+        return dfs(arr, 1, n);
     }
 
-    private int dfs(int[] nums, boolean[] used, int n) {
-        if (n == 0) {
-            return 0;
+    private int dfs(int[] nums, int l, int r) {
+        if (l == r) {
+            return nums[l - 1] * nums[l] * nums[l + 1];
         }
-        int ans = Integer.MIN_VALUE;
-        for (int i = 1; i < nums.length - 1; i++) {
-            if (used[i]) {
-                continue;
-            }
-            int l = i - 1;
-            while (nums[l] == 0) {
-                l--;
-            }
-            int r = i + 1;
-            while (nums[r] == 0) {
-                r++;
-            }
-            int c = nums[l] * nums[i] * nums[r];
-            nums[i] = 0;
-            used[i] = true;
-            ans = Math.max(ans, dfs(nums, used, n - 1) + c);
-            nums[i] = c / nums[l] / nums[r];
-            used[i] = false;
+        int ans = Math.max(
+                // 选择 l 位置最后打爆
+                nums[l - 1] * nums[l] * nums[r + 1] + dfs(nums, l + 1, r),
+                // 选择 r 位置最后打爆
+                nums[l - 1] * nums[r] * nums[r + 1] + dfs(nums, l, r - 1));
+        // 选择 (l,r) 位置最后打爆
+        for (int i = l + 1; i < r; i++) {
+            ans = Math.max(ans, nums[l - 1] * nums[i] * nums[r + 1] + dfs(nums, l, i - 1) + dfs(nums, i + 1, r));
         }
         return ans;
     }

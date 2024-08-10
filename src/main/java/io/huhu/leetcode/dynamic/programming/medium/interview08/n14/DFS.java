1219f2d8533e430dd9d459f5d9e5af2ac4d846ac
chen <18551681083@163.com>
2024-08-10 17:02:09 +0800

    interview08.14. Boolean Evaluation LCCI alg: depth first search opt: memory search

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/interview08/n14/DFS.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/interview08/n14/DFS.java
@@ -5,19 +5,23 @@ class DFS implements Solution {
     @Override
     public int countEval(String s, int result) {
         char[] c = s.toCharArray();
-        int[] ans = dfs(c, 0, c.length - 1);
+        int n = c.length;
+        int[] ans = dfs(c, 0, n - 1, new int[n][n][]);
         return ans[result];
     }
 
-    private int[] dfs(char[] s, int i, int j) {
+    private int[] dfs(char[] s, int i, int j, int[][][] m) {
+        if (m[i][j] != null) {
+            return m[i][j];
+        }
         int f = 0, t = 0;
         if (i == j) {
             f = s[i] == '0' ? 1 : 0;
             t = s[i] == '1' ? 1 : 0;
         } else {
             for (int k = i + 1; k < j; k += 2) {
-                int[] l = dfs(s, i, k - 1);
-                int[] r = dfs(s, k + 1, j);
+                int[] l = dfs(s, i, k - 1, m);
+                int[] r = dfs(s, k + 1, j, m);
                 int a = l[0], b = l[1];
                 int c = r[0], d = r[1];
                 switch (s[k]) {
@@ -36,7 +40,7 @@ class DFS implements Solution {
                 }
             }
         }
-        return new int[]{f, t};
+        return m[i][j] = new int[]{f, t};
     }
 
 }

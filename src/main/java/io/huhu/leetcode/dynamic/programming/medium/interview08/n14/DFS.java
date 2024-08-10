5d8576347514ac19484cf99f3eb5ce5ee6c8c950
chen <18551681083@163.com>
2024-08-10 16:58:38 +0800

    interview08.14. Boolean Evaluation LCCI alg: dfs

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/interview08/n14/DFS.java
@@ -0,0 +1,42 @@
+package io.huhu.leetcode.dynamic.programming.medium.interview08.n14;
+
+class DFS implements Solution {
+
+    @Override
+    public int countEval(String s, int result) {
+        char[] c = s.toCharArray();
+        int[] ans = dfs(c, 0, c.length - 1);
+        return ans[result];
+    }
+
+    private int[] dfs(char[] s, int i, int j) {
+        int f = 0, t = 0;
+        if (i == j) {
+            f = s[i] == '0' ? 1 : 0;
+            t = s[i] == '1' ? 1 : 0;
+        } else {
+            for (int k = i + 1; k < j; k += 2) {
+                int[] l = dfs(s, i, k - 1);
+                int[] r = dfs(s, k + 1, j);
+                int a = l[0], b = l[1];
+                int c = r[0], d = r[1];
+                switch (s[k]) {
+                    case '&' -> {
+                        f += a * c + a * d + c * b;
+                        t += b * d;
+                    }
+                    case '|' -> {
+                        f += a * c;
+                        t += a * d + c * b + d * b;
+                    }
+                    default -> {
+                        f += a * c + b * d;
+                        t += a * d + c * b;
+                    }
+                }
+            }
+        }
+        return new int[]{f, t};
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/medium/interview08/n14/DFSTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.dynamic.programming.medium.interview08.n14;
+
+class DFSTest extends SolutionTest {
+
+    @Override
+    protected Solution getSolution() {
+        return new DFS();
+    }
+
+}

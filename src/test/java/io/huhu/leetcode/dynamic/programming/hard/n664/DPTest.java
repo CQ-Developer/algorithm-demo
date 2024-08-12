a9b9918e9e3302051613f8a24c42f9825be4e492
NoahChen <noah.chen@batechworks.com>
2024-08-12 09:25:12 +0800

    664. Strange Printer alg: dynamic programming

---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/hard/n664/DPTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.dynamic.programming.hard.n664;
+
+class DPTest extends SolutionTest {
+
+    @Override
+    protected Solution getSolution() {
+        return new DP();
+    }
+
+}

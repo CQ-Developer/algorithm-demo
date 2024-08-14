98bfa7278fd6967c33e77963793b035ae0e64a8e
NoahChen <noah.chen@batechworks.com>
2024-08-14 16:41:04 +0800

    1000. Minimum Cost to Merge Stones alg: memory search

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n1000/MemorySearch.java
@@ -0,0 +1,38 @@
+package io.huhu.leetcode.dynamic.programming.hard.n1000;
+
+import java.util.Arrays;
+
+class MemorySearch implements Solution {
+
+    @Override
+    public int mergeStones(int[] stones, int k) {
+        int n = stones.length;
+        if ((n - 1) % (k - 1) != 0) {
+            return -1;
+        }
+        int[][] m = new int[n][n];
+        for (int[] a : m) {
+            Arrays.fill(a, -1);
+        }
+        int[] p = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            p[i + 1] = p[i] + stones[i];
+        }
+        return f(p, k, m, 0, n - 1);
+    }
+
+    private int f(int[] p, int k, int[][] m, int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = l; i < r; i += k - 1) {
+            ans = Math.min(ans, f(p, k, m, l, i) + f(p, k, m, i + 1, r));
+        }
+        if ((r - l) % (k - 1) == 0) {
+            ans += p[r + 1] - p[l];
+        }
+        return m[l][r] = ans;
+    }
+
+}

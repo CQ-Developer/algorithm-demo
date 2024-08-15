783f1008d3b6e95a765e480ee565af372de694d0
NoahChen <noah.chen@batechworks.com>
2024-08-15 15:43:32 +0800

    144. Binary Tree Preorder Traversal alg: stack

---
+++ src/main/java/io/huhu/leetcode/binary/tree/n114/Stack.java
@@ -0,0 +1,31 @@
+package io.huhu.leetcode.binary.tree.n114;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+class Stack implements Solution {
+
+    @Override
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.addFirst(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.removeFirst();
+            ans.add(cur.val);
+            if (cur.right != null) {
+                stack.addFirst(cur.right);
+            }
+            if (cur.left != null) {
+                stack.addFirst(cur.left);
+            }
+        }
+        return ans;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/binary/tree/n114/StackTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.binary.tree.n114;
+
+class StackTest extends SolutionTest {
+
+    @Override
+    Solution getSolution() {
+        return new Stack();
+    }
+
+}

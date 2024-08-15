4d318be4ffd3dad944cd94ff0da6d6a86255cac6
NoahChen <noah.chen@batechworks.com>
2024-08-15 08:12:30 +0800

    730. Count Different Palindromic Subsequences

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n730/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.hard.n730;
+
+/**
+ * <a href="https://leetcode.cn/problems/count-different-palindromic-subsequences/description/">
+ * Count Different Palindromic Subsequences
+ * </a>
+ */
+interface Solution {
+
+    int countPalindromicSubsequences(String s);
+
+}

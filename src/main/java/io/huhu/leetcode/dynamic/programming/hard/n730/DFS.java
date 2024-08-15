a077c723e680f0481fd59132e748fb2734dfddb9
NoahChen <noah.chen@batechworks.com>
2024-08-15 09:57:52 +0800

    730. Count Different Palindromic Subsequences alg: depth first search opt: memory search

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n730/DFS.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n730/DFS.java
@@ -11,52 +11,50 @@ class DFS implements Solution {
         char[] c = s.toCharArray();
         int n = c.length;
         int[] last = new int[101];
-        // i位置左侧最近的相同字符位置, 默认-1
         Arrays.fill(last, -1);
         int[] q = new int[n];
         for (int i = 0; i < n; i++) {
             q[i] = last[c[i]];
             last[c[i]] = i;
         }
-        // i位置右侧最近相同字符位置, 默认n
         Arrays.fill(last, n);
         int[] p = new int[n];
         for (int i = n - 1; i >= 0; i--) {
             p[i] = last[c[i]];
             last[c[i]] = i;
         }
-        return (int) f(c, 0, n - 1, q, p);
+        long[][] m = new long[n][n];
+        for (var a : m) {
+            Arrays.fill(a, -1);
+
+        }
+        return (int) f(c, 0, n - 1, q, p, m);
     }
 
-    private long f(char[] s, int l, int r, int[] q, int[] p) {
+    private long f(char[] s, int l, int r, int[] q, int[] p, long[][] m) {
         if (l > r) {
             return 0;
         }
         if (l == r) {
             return 1;
         }
+        if (m[l][r] != -1) {
+            return m[l][r];
+        }
         long ans;
-        // 两端字符相同
         if (s[l] != s[r]) {
-            ans = f(s, l + 1, r, q, p) + f(s, l, r - 1, q, p) - f(s, l + 1, r - 1, q, p) + MOD;
-        }
-        // 两端字符不同
-        else {
+            ans = f(s, l + 1, r, q, p, m) + f(s, l, r - 1, q, p, m) - f(s, l + 1, r - 1, q, p, m) + MOD;
+        } else {
             int i = p[l], j = q[r];
-            // 不存在
             if (i > j) {
-                ans = f(s, l + 1, r - 1, q, p) * 2 + 2;
-            }
-            // 存在1个
-            else if (i == j) {
-                ans = f(s, l + 1, r - 1, q, p) * 2 + 1;
-            }
-            // 存在n个
-            else {
-                ans = f(s, l + 1, r - 1, q, p) * 2 - f(s, i + 1, j - 1, q, p) + MOD;
+                ans = f(s, l + 1, r - 1, q, p, m) * 2 + 2;
+            } else if (i == j) {
+                ans = f(s, l + 1, r - 1, q, p, m) * 2 + 1;
+            } else {
+                ans = f(s, l + 1, r - 1, q, p, m) * 2 - f(s, i + 1, j - 1, q, p, m) + MOD;
             }
         }
-        return ans % MOD;
+        return m[l][r] = ans % MOD;
     }
 
 }

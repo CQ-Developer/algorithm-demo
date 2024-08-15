b448d592dc20386e9310d0675642fdc8b7e2e387
NoahChen <noah.chen@batechworks.com>
2024-08-15 16:21:45 +0800

    94. Binary Tree Inorder Traversal alg: recurse

---
+++ src/main/java/io/huhu/leetcode/binary/tree/n94/Recurse.java
@@ -0,0 +1,24 @@
+package io.huhu.leetcode.binary.tree.n94;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Recurse implements Solution {
+
+    @Override
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        recurse(root, ans);
+        return ans;
+    }
+
+    private void recurse(TreeNode root, List<Integer> ans) {
+        if (root == null) {
+            return;
+        }
+        recurse(root.left, ans);
+        ans.add(root.val);
+        recurse(root.right, ans);
+    }
+
+}

0be25784a4aa070cbdad9d3c25b35f1aa7c02628
NoahChen <noah.chen@batechworks.com>
2024-08-16 16:42:37 +0800

    662. Maximum Width of Binary Tree

---
+++ src/main/java/io/huhu/leetcode/binary/tree/medium/n662/Solution.java
@@ -0,0 +1,14 @@
+package io.huhu.leetcode.binary.tree.medium.n662;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-width-of-binary-tree/description/">
+ * Maximum Width of Binary Tree
+ * </a>
+ */
+class Solution {
+
+    public int widthOfBinaryTree(TreeNode root) {
+        return 0;
+    }
+
+}

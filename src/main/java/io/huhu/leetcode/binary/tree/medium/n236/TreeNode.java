2b41a5cc214800440d4179a927fce18edae5e6ba
NoahChen <noah.chen@batechworks.com>
2024-08-19 14:48:56 +0800

    236. Lowest Common Ancestor of a Binary Tree

---
+++ src/main/java/io/huhu/leetcode/binary/tree/medium/n236/TreeNode.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.binary.tree.medium.n236;
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+}

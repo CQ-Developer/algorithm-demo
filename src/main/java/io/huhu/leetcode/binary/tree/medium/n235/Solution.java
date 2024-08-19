306ebc92d9f0456922aa572cf288fdb4c0fd647f
NoahChen <noah.chen@batechworks.com>
2024-08-19 15:51:23 +0800

    nothing here.

--- src/main/java/io/huhu/leetcode/binary/tree/medium/n235/Solution.java
+++ src/main/java/io/huhu/leetcode/binary/tree/medium/n235/Solution.java
@@ -8,8 +8,8 @@ package io.huhu.leetcode.binary.tree.medium.n235;
 class Solution {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        while (p.val != root.val && q.val != root.val) {
-            if (Math.min(q.val, p.val) < root.val && Math.max(q.val, p.val) > root.val) {
+        while (root.val != q.val && root.val != p.val) {
+            if (root.val > Math.min(q.val, p.val) && root.val < Math.max(q.val, p.val)) {
                 break;
             }
             root = root.val < Math.min(q.val, p.val) ? root.right : root.left;

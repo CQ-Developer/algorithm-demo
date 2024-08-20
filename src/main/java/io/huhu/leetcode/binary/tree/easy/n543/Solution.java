42dd5509b7284ace05d76a7409a2e3879d54fdbf
chen <18551681083@163.com>
2024-08-21 07:25:51 +0800

    543. Diameter of Binary Tree

---
+++ src/main/java/io/huhu/leetcode/binary/tree/easy/n543/Solution.java
@@ -0,0 +1,14 @@
+package io.huhu.leetcode.binary.tree.easy.n543;
+
+/**
+ * <a href="https://leetcode.cn/problems/diameter-of-binary-tree/description/">
+ * Diameter of Binary Tree
+ * </a>
+ */
+class Solution {
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/binary/tree/easy/n543/SolutionTest.java
@@ -0,0 +1,26 @@
+package io.huhu.leetcode.binary.tree.easy.n543;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setup() {
+        solution = new Solution();
+    }
+
+    @Test
+    void test_1() {
+        TreeNode root = new TreeNode(1,
+                new TreeNode(2,
+                        new TreeNode(4),
+                        new TreeNode(5)),
+                new TreeNode(3));
+        Assertions.assertEquals(3, solution.diameterOfBinaryTree(root));
+    }
+
+}

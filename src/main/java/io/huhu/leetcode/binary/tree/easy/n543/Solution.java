e65abe0ac09084db86847e7f8548ad9f323b1247
NoahChen <noah.chen@batechworks.com>
2024-08-21 08:45:25 +0800

    543. Diameter of Binary Tree alg: dp

--- src/main/java/io/huhu/leetcode/binary/tree/easy/n543/Solution.java
+++ src/main/java/io/huhu/leetcode/binary/tree/easy/n543/Solution.java
@@ -8,7 +8,29 @@ package io.huhu.leetcode.binary.tree.easy.n543;
 class Solution {
 
     public int diameterOfBinaryTree(TreeNode root) {
-        return 0;
+        return f(root).diameter;
+    }
+
+    private Info f(TreeNode root) {
+        if (root == null) {
+            return new Info(0, 0);
+        }
+        Info left = f(root.left), right = f(root.right);
+        int height = Math.max(left.height, right.height) + 1;
+        int diameter = Math.max(left.height + right.height, Math.max(left.diameter, right.diameter));
+        return new Info(diameter, height);
+    }
+
+    static class Info {
+
+        final int diameter;
+        final int height;
+
+        Info(int diameter, int height) {
+            this.diameter = diameter;
+            this.height = height;
+        }
+
     }
 
 }

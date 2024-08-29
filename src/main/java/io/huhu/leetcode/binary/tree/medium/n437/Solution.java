a267df335e4f971e31108ea2b97a44ab39823a3d
NoahChen <noah.chen@batechworks.com>
2024-08-29 08:50:57 +0800

    437. Path Sum III alg: back tracing

--- src/main/java/io/huhu/leetcode/binary/tree/medium/n437/Solution.java
+++ src/main/java/io/huhu/leetcode/binary/tree/medium/n437/Solution.java
@@ -1,5 +1,8 @@
 package io.huhu.leetcode.binary.tree.medium.n437;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <a href="https://leetcode.cn/problems/path-sum-iii/description/">
  * Path Sum III
@@ -8,7 +11,22 @@ package io.huhu.leetcode.binary.tree.medium.n437;
 class Solution {
 
     public int pathSum(TreeNode root, int targetSum) {
-        return 0;
+        Map<Long, Integer> pre = new HashMap<>();
+        pre.put(0L, 1);
+        return f(root, targetSum, 0L, pre);
+    }
+
+    private int f(TreeNode root, int target, long sum, Map<Long, Integer> preSum) {
+        if (root == null) {
+            return 0;
+        }
+        sum += root.val;
+        int ans = preSum.getOrDefault(sum - target, 0);
+        preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        ans += f(root.left, target, sum, preSum);
+        ans += f(root.right, target, sum, preSum);
+        preSum.put(sum, preSum.get(sum) - 1);
+        return ans;
     }
 
 }

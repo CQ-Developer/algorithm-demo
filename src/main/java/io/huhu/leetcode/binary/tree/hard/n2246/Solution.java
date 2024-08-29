70c1965016183bc7688c686a977030b388367c61
NoahChen <noah.chen@batechworks.com>
2024-08-29 16:22:35 +0800

    2246. Longest Path With Different Adjacent Characters alg: chain forward star

--- src/main/java/io/huhu/leetcode/binary/tree/hard/n2246/Solution.java
+++ src/main/java/io/huhu/leetcode/binary/tree/hard/n2246/Solution.java
@@ -1,7 +1,6 @@
 package io.huhu.leetcode.binary.tree.hard.n2246;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * <a href="https://leetcode.cn/problems/longest-path-with-different-adjacent-characters/description/">
@@ -11,28 +10,14 @@ import java.util.List;
 class Solution {
 
     public int longestPath(int[] parent, String s) {
-        int n = parent.length;
-        var graph = buildGraph(n, parent);
+        var graph = new Graph(parent.length).build(parent);
         return f(graph, s.toCharArray(), 0).maxPath;
     }
 
-    private List<List<Integer>> buildGraph(int n, int[] parent) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int i = 1; i < n; i++) {
-            graph.get(parent[i]).add(i);
-        }
-        return graph;
-    }
-
-    private Info f(List<List<Integer>> graph, char[] s, int f) {
-        if (graph.get(f).isEmpty()) {
-            return new Info(1, 1);
-        }
+    private Info f(Graph graph, char[] s, int f) {
         int max1 = 0, max2 = 0, maxPath = 1;
-        for (int t : graph.get(f)) {
+        for (int e = graph.head[f]; e != -1; e = graph.next[e]) {
+            int t = graph.to[e];
             var info = f(graph, s, t);
             maxPath = Math.max(maxPath, info.maxPath);
             if (s[f] != s[t]) {
@@ -46,6 +31,32 @@ class Solution {
         }
         maxPath = Math.max(maxPath, max1 + max2 + 1);
         return new Info(maxPath, max1 + 1);
+    }
+
+    private class Graph {
+
+        final int[] head;
+        final int[] next;
+        final int[] to;
+
+        Graph(int n) {
+            head = new int[n];
+            next = new int[n];
+            to = new int[n];
+        }
+
+        Graph build(int[] parent) {
+            int e = 0;
+            Arrays.fill(head, -1);
+            for (int t = 1; t < parent.length; t++) {
+                int f = parent[t];
+                next[e] = head[f];
+                to[e] = t;
+                head[f] = e++;
+            }
+            return this;
+        }
+
     }
 
     private class Info {

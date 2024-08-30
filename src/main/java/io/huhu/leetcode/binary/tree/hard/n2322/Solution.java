3bb97767ee452dc2c557d5fe4350769190783f7e
NoahChen <noah.chen@batechworks.com>
2024-08-30 16:36:16 +0800

    2322. Minimum Score After Removals on a Tree alg: dfn + chain forward star

--- src/main/java/io/huhu/leetcode/binary/tree/hard/n2322/Solution.java
+++ src/main/java/io/huhu/leetcode/binary/tree/hard/n2322/Solution.java
@@ -1,9 +1,5 @@
 package io.huhu.leetcode.binary.tree.hard.n2322;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * <a href="https://leetcode.cn/problems/minimum-score-after-removals-on-a-tree/description/">
  * Minimum Score After Removals on a Tree
@@ -11,41 +7,22 @@ import java.util.List;
  */
 class Solution {
 
-    private static final int MAXN = 1001;
-
-    private static final int[] dfn = new int[MAXN];
-    private static final int[] size = new int[MAXN];
-    private static final int[] xor = new int[MAXN];
-
-    private int dfnCnt;
-
     public int minimumScore(int[] nums, int[][] edges) {
-        int n = nums.length;
-        Arrays.fill(dfn, 0, n, 0);
-        var graph = buildGraph(n, edges);
-        f(nums, graph, 0);
+        var graph = new Graph(nums.length, edges.length).init(edges);
+        var dfn = new DFN(nums.length).init(graph, nums);
         int ans = Integer.MAX_VALUE;
         for (int i = 0; i < edges.length; i++) {
-            int a = Math.max(dfn[edges[i][0]], dfn[edges[i][1]]);
+            int a = Math.max(dfn.dfn[edges[i][0]], dfn.dfn[edges[i][1]]);
             for (int j = i + 1; j < edges.length; j++) {
-                int b = Math.max(dfn[edges[j][0]], dfn[edges[j][1]]);
-                int p, q;
-                if (a < b) {
-                    p = a;
-                    q = b;
+                int b = Math.max(dfn.dfn[edges[j][0]], dfn.dfn[edges[j][1]]);
+                int p = Math.min(a, b), q = Math.max(a, b);
+                int x = dfn.xor[q], y, z;
+                if (p + dfn.size[p] > q) {
+                    y = dfn.xor[p] ^ x;
+                    z = dfn.xor[1] ^ dfn.xor[p];
                 } else {
-                    p = b;
-                    q = a;
-                }
-                int y, z;
-                int x = xor[q];
-                // q is subtree of p
-                if (q < p + size[p]) {
-                    y = xor[q] ^ xor[p];
-                    z = xor[1] ^ xor[p];
-                } else {
-                    y = xor[p];
-                    z = xor[1] ^ x ^ y;
+                    y = dfn.xor[p];
+                    z = dfn.xor[1] ^ y ^ x;
                 }
                 ans = Math.min(ans, Math.max(x, Math.max(y, z)) - Math.min(x, Math.min(y, z)));
             }
@@ -53,30 +30,67 @@ class Solution {
         return ans;
     }
 
-    private List<List<Integer>> buildGraph(int n, int[][] edges) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+    class Graph {
+
+        final int[] head;
+        final int[] next;
+        final int[] to;
+
+        Graph(int n, int m) {
+            head = new int[n + 1];
+            next = new int[m * 2 + 1];
+            to = new int[m * 2 + 1];
         }
-        for (int[] edge : edges) {
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
+
+        Graph init(int[][] edges) {
+            int i = 1;
+            for (var edge : edges) {
+                int f = edge[0], t = edge[1];
+                next[i] = head[f];
+                to[i] = t;
+                head[f] = i++;
+                next[i] = head[t];
+                to[i] = f;
+                head[t] = i++;
+            }
+            return this;
         }
-        return graph;
+
     }
 
-    private void f(int[] nums, List<List<Integer>> graph, int f) {
-        int i = ++dfnCnt;
-        dfn[f] = i;
-        size[i] = 1;
-        xor[i] = nums[f];
-        for (int t : graph.get(f)) {
-            if (dfn[t] == 0) {
-                f(nums, graph, t);
-                size[i] += size[dfn[t]];
-                xor[i] ^= xor[dfn[t]];
+    class DFN {
+
+        int dfnCnt;
+        final int[] dfn;
+        final int[] size;
+        final int[] xor;
+
+        DFN(int n) {
+            dfn = new int[n + 1];
+            size = new int[n + 1];
+            xor = new int[n + 1];
+        }
+
+        DFN init(Graph graph, int[] nums) {
+            f(graph, nums, 0);
+            return this;
+        }
+
+        void f(Graph graph, int[] nums, int f) {
+            int i = ++dfnCnt;
+            dfn[f] = i;
+            size[i] = 1;
+            xor[i] = nums[f];
+            for (int e = graph.head[f]; e != 0; e = graph.next[e]) {
+                int t = graph.to[e];
+                if (dfn[t] == 0) {
+                    f(graph, nums, t);
+                    size[i] += size[dfn[t]];
+                    xor[i] ^= xor[dfn[t]];
+                }
             }
         }
+
     }
 
 }

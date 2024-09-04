0697223bd97e62c016e43942c62409ddde27e5e3
NoahChen <noah.chen@batechworks.com>
2024-09-04 12:58:47 +0800

    1994. The Number of Good Subsets alg: dfs + status compression

---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/hard/n1994/SolutionTest.java
@@ -0,0 +1,28 @@
+package io.huhu.leetcode.dynamic.programming.hard.n1994;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    Solution solution;
+
+    @BeforeEach
+    void setup() {
+        solution = new Solution();
+    }
+
+    @Test
+    void test_1() {
+        int[] nums = {1, 2, 3, 4};
+        Assertions.assertEquals(6, solution.numberOfGoodSubsets(nums));
+    }
+
+    @Test
+    void test_2() {
+        int[] nums = {4, 2, 3, 15};
+        Assertions.assertEquals(5, solution.numberOfGoodSubsets(nums));
+    }
+
+}

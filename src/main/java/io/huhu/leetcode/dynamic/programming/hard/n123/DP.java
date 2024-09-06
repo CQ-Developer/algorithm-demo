75d86ae1b2502f03b57a8c722a3fe5dd0c34c2d1
NoahChen <noah.chen@batechworks.com>
2024-09-06 10:20:40 +0800

    122. Best Time to Buy and Sell Stock III alg: dp opt: merge traversal

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n123/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n123/DP.java
@@ -5,20 +5,16 @@ class DP implements Solution {
     @Override
     public int maxProfit(int[] prices) {
         int n = prices.length;
+        int ans = 0;
         int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        int[] best = new int[n];
+        best[0] = dp1[0] - prices[0];
         int min = prices[0];
         for (int i = 1; i < n; i++) {
             min = Math.min(min, prices[i]);
             dp1[i] = Math.max(dp1[i - 1], prices[i] - min);
-        }
-        int[] best = new int[n];
-        best[0] = dp1[0] - prices[0];
-        for (int i = 1; i < n; i++) {
             best[i] = Math.max(best[i - 1], dp1[i] - prices[i]);
-        }
-        int ans = 0;
-        int[] dp2 = new int[n];
-        for (int i = 1; i < n; i++) {
             dp2[i] = best[i] + prices[i];
             ans = Math.max(ans, dp2[i]);
         }

3c714f1cf278d782fbd20e48e8dd56f22d30ab0e
chen <18551681083@163.com>
2024-09-06 19:13:57 +0800

    714. Best Time to Buy and Sell Stock with Transaction Fee alg: dp opt: states compression

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n714/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n714/DP.java
@@ -4,15 +4,13 @@ class DP implements Solution {
 
     @Override
     public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        int[][] dp = new int[n][2];
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        int sold = 0, hold = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            int tmp = sold;
+            sold = Math.max(sold, hold + prices[i] - fee);
+            hold = Math.max(hold, tmp - prices[i]);
         }
-        return dp[n - 1][0];
+        return sold;
     }
 
 }

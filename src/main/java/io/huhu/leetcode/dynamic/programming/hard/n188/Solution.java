81e58d32b6c91d4d07cd81f82391ea3dfdb94535
NoahChen <noah.chen@batechworks.com>
2024-09-06 11:03:11 +0800

    188. Best Time to Buy and Sell Stock IV

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n188/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.hard.n188;
+
+/**
+ * <a href="https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iv/description/">
+ * Best Time to Buy and Sell Stock IV
+ * </a>
+ */
+interface Solution {
+
+    int maxProfit(int k, int[] prices);
+
+}

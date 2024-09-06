4ec0eb066682666b5ef37ca9a0c8312969b156d7
NoahChen <noah.chen@batechworks.com>
2024-09-06 08:35:20 +0800

    122. Best Time to Buy and Sell Stock II

--- src/main/java/io/huhu/leetcode/dynamic/programming/medium/n122/Solution.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/medium/n122/Solution.java
@@ -1,22 +1,12 @@
 package io.huhu.leetcode.dynamic.programming.medium.n122;
 
 /**
- * <a href="https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/description/">买卖股票的最佳时机II</a>
+ * <a href="https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/description/">
+ * Best Time to Buy and Sell Stock II
+ * </a>
  */
-class Solution {
+interface Solution {
 
-    public int maxProfit(int[] prices) {
-        if (prices.length < 2) {
-            return 0;
-        }
-        int res = 0;
-        for (int i = 1; i < prices.length; i++) {
-            int dif = prices[i] - prices[i - 1];
-            if (dif > 0) {
-                res += dif;
-            }
-        }
-        return res;
-    }
+    int maxProfit(int[] prices);
 
 }

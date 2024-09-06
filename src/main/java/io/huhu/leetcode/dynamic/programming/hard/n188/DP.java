f6b7f84a3d64b1fd49f6b68f7389bb74c411e8a1
NoahChen <noah.chen@batechworks.com>
2024-09-06 14:38:33 +0800

    188. Best Time to Buy and Sell Stock IV alg: dp opt: prune

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n188/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n188/DP.java
@@ -5,6 +5,9 @@ class DP implements Solution {
     @Override
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
+        if (k >= (n >> 1)) {
+            return infinity(prices);
+        }
         int[][] dp = new int[k + 1][n];
         for (int i = 1; i <= k; i++) {
             for (int j = 1; j < n; j++) {
@@ -15,6 +18,17 @@ class DP implements Solution {
             }
         }
         return dp[k][n - 1];
+    }
+
+    /**
+     * 相当于可以进行无限制次数的交易
+     */
+    private int infinity(int[] prices) {
+        int ans = 0;
+        for (int i = 1; i < prices.length; i++) {
+            ans += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return ans;
     }
 
 }

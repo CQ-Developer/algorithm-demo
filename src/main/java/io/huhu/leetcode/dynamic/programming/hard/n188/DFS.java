2645ecbb5ad502ead34d224da662ecaef8be422e
NoahChen <noah.chen@batechworks.com>
2024-09-06 13:36:00 +0800

    188. Best Time to Buy and Sell Stock IV alg: dfs opt: memory search

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n188/DFS.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n188/DFS.java
@@ -1,21 +1,31 @@
 package io.huhu.leetcode.dynamic.programming.hard.n188;
 
+import java.util.Arrays;
+
 class DFS implements Solution {
 
     @Override
     public int maxProfit(int k, int[] prices) {
-        return f(k, prices, prices.length - 1);
+        int n = prices.length;
+        int[][] dp = new int[k + 1][n];
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+        return f(k, prices, n - 1, dp);
     }
 
-    private int f(int k, int[] prices, int i) {
-        if (k == 0 || i == -1) {
+    private int f(int k, int[] prices, int i, int[][] dp) {
+        if (k == 0 || i < 0) {
             return 0;
         }
-        int ans = f(k, prices, i - 1);
-        for (int j = 0; j < i; j++) {
-            ans = Math.max(ans, prices[i] - prices[j] + f(k - 1, prices, j));
+        if (dp[k][i] != -1) {
+            return dp[k][i];
         }
-        return ans;
+        int ans = f(k, prices, i - 1, dp);
+        for (int j = 0; j < i; j++) {
+            ans = Math.max(ans, prices[i] - prices[j] + f(k - 1, prices, j, dp));
+        }
+        return dp[k][i] = ans;
     }
 
 }

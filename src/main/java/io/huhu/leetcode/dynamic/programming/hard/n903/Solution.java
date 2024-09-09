3720ecf0625708d508f99610e7dbe5a9174892b1
NoahChen <noah.chen@batechworks.com>
2024-09-09 13:45:05 +0800

    903. Valid Permutations for DI Sequence

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n903/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.hard.n903;
+
+/**
+ * <a href="https://leetcode.cn/problems/valid-permutations-for-di-sequence/description/">
+ * Valid Permutations for DI Sequence
+ * </a>
+ */
+interface Solution {
+
+    int numPermsDISequence(String s);
+
+}

e8f31d176b5eca2df2ecdda247ca000e33ccc9fb
NoahChen <noah.chen@batechworks.com>
2024-09-11 14:54:54 +0800

    629. K Inverse Pairs Array opt: dp opt: states compression

--- src/main/java/io/huhu/leetcode/dynamic/programming/hard/n629/DP.java
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n629/DP.java
@@ -6,20 +6,25 @@ class DP implements Solution {
 
     @Override
     public int kInversePairs(int n, int k) {
-        int[][] dp = new int[n + 1][k + 1];
-        dp[0][0] = 1;
+        int[] up = new int[k + 1];
+        int[] dp = new int[k + 1];
+        up[0] = 1;
         for (int i = 1; i <= n; i++) {
-            dp[i][0] = 1;
+            dp[0] = 1;
             long window = 1;
             for (int j = 1; j <= k; j++) {
-                window += dp[i - 1][j];
+                window += up[j];
                 if (j >= i) {
-                    window -= dp[i - 1][j - i];
+                    window -= up[j - i];
                 }
-                dp[i][j] = (int) (window % M);
+                dp[j] = (int) (window % M);
+            }
+            for (int p = 0; p <= k; p++) {
+                up[p] = dp[p];
+                dp[p] = 0;
             }
         }
-        return dp[n][k];
+        return up[k];
     }
 
 }

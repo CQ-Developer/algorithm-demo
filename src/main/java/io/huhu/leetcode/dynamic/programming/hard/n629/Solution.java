0e3845e1fbca43a1452a450b33f954e08a3ae80f
NoahChen <noah.chen@batechworks.com>
2024-09-11 13:06:46 +0800

    629. K Inverse Pairs Array

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n629/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.hard.n629;
+
+/**
+ * <a href="https://leetcode.cn/problems/k-inverse-pairs-array/description/">
+ * K Inverse Pairs Array
+ * </a>
+ */
+interface Solution {
+
+    int kInversePairs(int n, int k);
+
+}

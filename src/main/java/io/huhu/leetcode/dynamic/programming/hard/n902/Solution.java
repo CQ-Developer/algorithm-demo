0d22cfc471d28db63b2af582049a0a9c061d4af1
NoahChen <noah.chen@batechworks.com>
2024-09-13 15:59:05 +0800

    902. Numbers At Most N Given Digit Set

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n902/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.hard.n902;
+
+/**
+ * <a href="https://leetcode.cn/problems/numbers-at-most-n-given-digit-set/description/">
+ * Numbers At Most N Given Digit Set
+ * </a>
+ */
+interface Solution {
+
+    int atMostNGivenDigitSet(String[] digits, int n);
+
+}

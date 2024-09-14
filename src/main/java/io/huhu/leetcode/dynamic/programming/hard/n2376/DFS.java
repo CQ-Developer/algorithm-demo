29de5040e742c6db81436593f9db8e7aba239628
NoahChen <noah.chen@batechworks.com>
2024-09-14 17:18:36 +0800

    2376. Count Special Integers alg: dfs sta: todo

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n2376/DFS.java
@@ -0,0 +1,11 @@
+package io.huhu.leetcode.dynamic.programming.hard.n2376;
+
+class DFS implements Solution {
+
+    @Override
+    public int countSpecialNumbers(int n) {
+        // todo
+        return -1;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/hard/n2376/DFSTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.dynamic.programming.hard.n2376;
+
+class DFSTest extends SolutionTest {
+
+    @Override
+    Solution getSolution() {
+        return new DFS();
+    }
+
+}

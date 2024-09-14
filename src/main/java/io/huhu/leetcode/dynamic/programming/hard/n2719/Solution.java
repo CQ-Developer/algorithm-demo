3a2ce1b0da581acc185ad60c76106c3708d8b532
NoahChen <noah.chen@batechworks.com>
2024-09-14 09:13:04 +0800

    2719. Count of Integers

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n2719/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.hard.n2719;
+
+/**
+ * <a href="https://leetcode.cn/problems/count-of-integers/description/">
+ * Count of Integers
+ * </a>
+ */
+interface Solution {
+
+    int count(String num1, String num2, int min_sum, int max_sum);
+
+}

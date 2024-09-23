499e7843fb0543ddd08c075e52ed26eef84421fc
NoahChen <noah.chen@batechworks.com>
2024-09-23 11:55:49 +0800

    1187. Make Array Strictly Increasing

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n1187/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.dynamic.programming.hard.n1187;
+
+/**
+ * <a href="https://leetcode.cn/problems/make-array-strictly-increasing/description/">
+ * Make Array Strictly Increasing
+ * </a>
+ */
+interface Solution {
+
+    int makeArrayIncreasing(int[] arr1, int[] arr2);
+
+}

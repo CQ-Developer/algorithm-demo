c5ec3087fdd1d1fd82544f224c8d0da16191732c
NoahChen <noah.chen@batechworks.com>
2024-09-23 14:32:48 +0800

    1187. Make Array Strictly Increasing alg: dfs

---
+++ src/main/java/io/huhu/leetcode/dynamic/programming/hard/n1187/DFS.java
@@ -0,0 +1,62 @@
+package io.huhu.leetcode.dynamic.programming.hard.n1187;
+
+import java.util.Arrays;
+
+class DFS implements Solution {
+
+    @Override
+    public int makeArrayIncreasing(int[] arr1, int[] arr2) {
+        Arrays.sort(arr2);
+        int m = 1;
+        for (int i = 1; i < arr2.length; i++) {
+            if (arr2[i] != arr2[m - 1]) {
+                arr2[m++] = arr2[i];
+            }
+        }
+        int n = arr1.length;
+        int ans = f(arr1, arr2, n, m, 0);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    private int f(int[] arr1, int[] arr2, int n, int m, int i) {
+        if (i == n) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int pre = i == 0 ? Integer.MIN_VALUE : arr1[i - 1];
+        int find = find(arr2, m, pre);
+        for (int j = i, k = 0; j <= n; j++, k++) {
+            if (j == n) {
+                ans = Math.min(ans, k);
+            } else {
+                if (pre < arr1[j]) {
+                    int next = f(arr1, arr2, n, m, j + 1);
+                    if (next != Integer.MAX_VALUE) {
+                        ans = Math.min(ans, next + k);
+                    }
+                }
+                if (find != -1 && find < m) {
+                    pre = arr2[find++];
+                } else {
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
+    private int find(int[] arr, int len, int num) {
+        int l = 0, r = len -1, ans = -1;
+        while (l <= r) {
+            int m = (l + r) >> 1;
+            if (arr[m] > num) {
+                ans = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return ans;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/dynamic/programming/hard/n1187/DFSTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.dynamic.programming.hard.n1187;
+
+class DFSTest extends SolutionTest {
+
+    @Override
+    Solution getSolution() {
+        return new DFS();
+    }
+
+}

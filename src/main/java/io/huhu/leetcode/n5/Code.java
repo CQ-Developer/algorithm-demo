8d752351ac4329efa14fb4f85c70117bab0ef6dd
NoahChen <noah.chen@batechworks.com>
2024-09-24 15:06:21 +0800

    5. Longest Palindromic Substring

---
+++ src/main/java/io/huhu/leetcode/n5/Code.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.n5;
+
+/**
+ * <a href="https://leetcode.cn/problems/longest-palindromic-substring/description/">Longest Palindromic Substring</a>
+ */
+interface Code {
+
+    String longestPalindrome(String s);
+
+}
---
+++ src/test/java/io/huhu/leetcode/n5/CodeTest.java
@@ -0,0 +1,28 @@
+package io.huhu.leetcode.n5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+abstract class CodeTest {
+
+    Code code;
+
+    abstract Code getCode();
+
+    @BeforeEach
+    void setup() {
+        code = getCode();
+    }
+
+    @Test
+    void test_1() {
+        Assertions.assertEquals("bab", code.longestPalindrome("babad"));
+    }
+
+    @Test
+    void test_2() {
+        Assertions.assertEquals("bb", code.longestPalindrome("cbbd"));
+    }
+
+}

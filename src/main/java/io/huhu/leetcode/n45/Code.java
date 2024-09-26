469f59937ecfce9949bcd6249da8941ccdce9bc5
NoahChen <noah.chen@batechworks.com>
2024-09-26 15:44:36 +0800

    nothing here.

--- src/main/java/io/huhu/leetcode/n45/Code.java
+++ src/main/java/io/huhu/leetcode/n45/Code.java
@@ -1,7 +1,9 @@
 package io.huhu.leetcode.n45;
 
 /**
- * <a href="https://leetcode.cn/problems/jump-game-ii/description/">Jump Game II</a>
+ * <a href="https://leetcode.cn/problems/jump-game-ii/description/">
+ * Jump Game II
+ * </a>
  */
 interface Code {
 

2c8be437435ba98fd2e3b0bd0887a67c33c4f0e5
NoahChen <noah.chen@batechworks.com>
2024-09-27 09:21:02 +0800

    nothing here.

--- src/main/java/io/huhu/leetcode/n45/Greedy.java
+++ src/main/java/io/huhu/leetcode/n45/Greedy.java
@@ -4,15 +4,13 @@ class Greedy implements Code {
 
     @Override
     public int jump(int[] nums) {
+        int n = nums.length - 1;
         int step = 0, farthest = 0, position = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < n && position != n; i++) {
             farthest = Math.max(farthest, i + nums[i]);
             if (i == position) {
                 position = farthest;
                 step++;
-            }
-            if (position == nums.length - 1) {
-                break;
             }
         }
         return step;

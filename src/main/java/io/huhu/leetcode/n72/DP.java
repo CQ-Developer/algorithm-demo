b3c72f979c2a39ee3dc514182fe0785ea6a35c36
chen <18551681083@163.com>
2024-09-29 16:55:56 +0800

    72. Edit Distance alg: DP

---
+++ src/main/java/io/huhu/leetcode/n72/DP.java
@@ -0,0 +1,28 @@
+package io.huhu.leetcode.n72;
+
+class DP implements Code {
+
+    @Override
+    public int minDistance(String word1, String word2) {
+        char[] w1 = word1.toCharArray(), w2 = word2.toCharArray();
+        int m = w1.length, n = w2.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (w1[i - 1] == w2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/n72/DPTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.n72;
+
+class DPTest extends CodeTest {
+
+    @Override
+    Code getCode() {
+        return new DP();
+    }
+
+}

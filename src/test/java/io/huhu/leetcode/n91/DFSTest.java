f293272199fdb3b5311240f2a044a195b7743100
chen <18551681083@163.com>
2024-09-29 22:50:35 +0800

    nothing here.

--- src/test/java/io/huhu/leetcode/n91/DFSTest.java
+++ src/test/java/io/huhu/leetcode/n91/DFSTest.java
@@ -1,18 +1,10 @@
 package io.huhu.leetcode.n91;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 class DFSTest extends CodeTest {
 
     @Override
     Code getCode() {
         return new DFS();
-    }
-
-    @Test
-    void test_4() {
-        Assertions.assertEquals(4, code.numDecodings("2611055971756562"));
     }
 
 }

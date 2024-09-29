b8e2d8248fe7f722088df8e0b15ed1ddbb897b60
chen <18551681083@163.com>
2024-09-29 16:22:21 +0800

    72. Edit Distance

---
+++ src/main/java/io/huhu/leetcode/n72/Code.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.n72;
+
+/**
+ * <a href="https://leetcode.cn/problems/edit-distance/description/">
+ * Edit Distance
+ * </a>
+ */
+interface Code {
+
+    int minDistance(String word1, String word2);
+
+}

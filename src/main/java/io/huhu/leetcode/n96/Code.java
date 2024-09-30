68bad6306d0dd2c77073f0ed412b4bd1f940687f
NoahChen <noah.chen@batechworks.com>
2024-09-30 08:54:11 +0800

    96. Unique Binary Search Trees

---
+++ src/main/java/io/huhu/leetcode/n96/Code.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.n96;
+
+/**
+ * <a href="https://leetcode.cn/problems/unique-binary-search-trees/description/">
+ * Unique Binary Search Tree
+ * </a>
+ */
+interface Code {
+
+    int numTrees(int n);
+
+}
---
+++ src/test/java/io/huhu/leetcode/n96/CodeTest.java
@@ -0,0 +1,28 @@
+package io.huhu.leetcode.n96;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+abstract class CodeTest {
+
+    Code code;
+
+    abstract Code getCode();
+
+    @BeforeEach
+    void setup() {
+        code = getCode();
+    }
+
+    @Test
+    void test_1() {
+        Assertions.assertEquals(5, code.numTrees(3));
+    }
+
+    @Test
+    void test_2() {
+        Assertions.assertEquals(1, code.numTrees(1));
+    }
+
+}

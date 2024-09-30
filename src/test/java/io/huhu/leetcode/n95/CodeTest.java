8ef8a29e76c212095143efde6bfedc88a5d9e20d
NoahChen <noah.chen@batechworks.com>
2024-09-30 11:19:37 +0800

    nothing here.

--- src/test/java/io/huhu/leetcode/n95/CodeTest.java
+++ src/test/java/io/huhu/leetcode/n95/CodeTest.java
@@ -1,5 +1,6 @@
 package io.huhu.leetcode.n95;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
@@ -25,13 +26,33 @@ abstract class CodeTest {
                 new TreeNode(2, new TreeNode(1), new TreeNode(3)),
                 new TreeNode(3, new TreeNode(2, new TreeNode(1), null), null),
                 new TreeNode(3, new TreeNode(1, null, new TreeNode(2)), null));
-        Assertions.assertIterableEquals(expected, code.generateTrees(3));
+        var a = toString(expected);
+        var b = toString(code.generateTrees(3));
+        Assertions.assertIterableEquals(a, b);
     }
 
     @Test
     void test_2() {
         List<TreeNode> expected = List.of(new TreeNode(1));
-        Assertions.assertIterableEquals(expected, code.generateTrees(1));
+        var a = toString(expected);
+        var b = toString(code.generateTrees(1));
+        Assertions.assertIterableEquals(a, b);
+    }
+
+    List<String> toString(List<TreeNode> nodes) {
+        List<String> list = new ArrayList<>();
+        for (var node : nodes) {
+            list.add(toString(node));
+        }
+        list.sort(String::compareTo);
+        return list;
+    }
+
+    String toString(TreeNode node) {
+        if (node == null) {
+            return "null";
+        }
+        return node.val + toString(node.left) + toString(node.right);
     }
 
 }

011c18a24431fc5d402ee50d8a699a8ae1da4882
NoahChen <noah.chen@batechworks.com>
2024-09-30 14:07:20 +0800

    97. Interleaving String alg: DP

---
+++ src/main/java/io/huhu/leetcode/n97/DP.java
@@ -0,0 +1,28 @@
+package io.huhu.leetcode.n97;
+
+class DP implements Code {
+
+    @Override
+    public boolean isInterleave(String s1, String s2, String s3) {
+        char[] c1 = s1.toCharArray(), c2 = s2.toCharArray(), c3 = s3.toCharArray();
+        int m = c1.length, n = c2.length;
+        if (m + n != c3.length) {
+            return false;
+        }
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= n && c2[j - 1] == c3[j - 1]; j++) {
+            dp[0][j] = true;
+        }
+        for (int i = 1; i <= m && c1[i - 1] == c3[i - 1]; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (c1[i - 1] == c3[i + j - 1] && dp[i - 1][j]) || (c2[j - 1] == c3[i + j - 1] && dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/n97/DPTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.n97;
+
+class DPTest extends CodeTest {
+
+    @Override
+    Code getCode() {
+        return new DP();
+    }
+
+}

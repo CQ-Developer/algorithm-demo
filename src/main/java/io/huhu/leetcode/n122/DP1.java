28782cb737dbcba7ed9fba69524c45b939d2623a
chen <18551681083@163.com>
2024-09-30 19:37:25 +0800

    122. Best Time to Buy and Sell Stock II alg: DP opt: Space Compression

---
+++ src/main/java/io/huhu/leetcode/n122/DP1.java
@@ -0,0 +1,14 @@
+package io.huhu.leetcode.n122;
+
+class DP1 implements Code {
+
+    @Override
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        for (int i = 1; i < prices.length; i++) {
+            ans += Math.max(prices[i] - prices[i - 1], 0);
+        }
+        return ans;
+    }
+
+}

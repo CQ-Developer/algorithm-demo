686966d13f3f72a7003b4bffa1db12cc3da702a8
NoahChen <noah.chen@batechworks.com>
2024-09-30 10:18:04 +0800

    96. Unique Binary Search Trees alg: DP

---
+++ src/main/java/io/huhu/leetcode/n96/DP.java
@@ -0,0 +1,17 @@
+package io.huhu.leetcode.n96;
+
+class DP implements Code {
+
+    @Override
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+
+}

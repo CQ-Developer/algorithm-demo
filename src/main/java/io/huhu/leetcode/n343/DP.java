d2800e814af64b36f5d3192e1b9e7a76ca26b974
NoahChen <noah.chen@batechworks.com>
2024-10-08 18:06:26 +0800

    343. Integer Break alg: DP

---
+++ src/main/java/io/huhu/leetcode/n343/DP.java
@@ -0,0 +1,17 @@
+package io.huhu.leetcode.n343;
+
+class DP implements Code {
+
+    @Override
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i / 2; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/n343/DPTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.n343;
+
+class DPTest extends CodeTest {
+
+    @Override
+    Code getCode() {
+        return new DP();
+    }
+
+}

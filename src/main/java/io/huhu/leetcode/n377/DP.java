34086e8eec18a95d128efdfd72adfac5cc7cd0e6
NoahChen <noah.chen@batechworks.com>
2024-10-10 11:41:56 +0800

    377. Combination Sum IV alg: DynamicProgramming opt: Sort

--- src/main/java/io/huhu/leetcode/n377/DP.java
+++ src/main/java/io/huhu/leetcode/n377/DP.java
@@ -1,16 +1,17 @@
 package io.huhu.leetcode.n377;
 
+import java.util.Arrays;
+
 class DP implements Code {
 
     @Override
     public int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
         int[] dp = new int[target + 1];
         dp[0] = 1;
-        for (int i = 1; i <= target; i++) {
-            for (int num : nums) {
-                if (num <= i) {
-                    dp[i] += dp[i - num];
-                }
+        for (int i = nums[0]; i <= target; i++) {
+            for (int j = 0; j < nums.length && i >= nums[j]; j++) {
+                dp[i] += dp[i - nums[j]];
             }
         }
         return dp[target];

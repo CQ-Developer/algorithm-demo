293783fb00492fcfda681ecff4ab1b0ca604694f
NoahChen <noah.chen@batechworks.com>
2024-10-10 10:25:12 +0800

    376. Wiggle Subsequence alg: DynamicProgramming

---
+++ src/main/java/io/huhu/leetcode/n376/DP.java
@@ -0,0 +1,19 @@
+package io.huhu.leetcode.n376;
+
+class DP implements Code {
+
+    @Override
+    public int wiggleMaxLength(int[] nums) {
+        int up = 1, down = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            }
+            if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+        return Math.max(up, down);
+    }
+
+}

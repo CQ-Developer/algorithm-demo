cf22f5b906f07390f535681a163f270f617621b8
NoahChen <noah.chen@batechworks.com>
2024-10-10 11:46:46 +0800

    377. Combination Sum IV alg: DepthFirstSearch opt: Sort

--- src/main/java/io/huhu/leetcode/n377/DFS.java
+++ src/main/java/io/huhu/leetcode/n377/DFS.java
@@ -8,6 +8,7 @@ class DFS implements Code {
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target + 1];
         Arrays.fill(dp, -1);
+        Arrays.sort(nums);
         return f(nums, target, dp);
     }
 
@@ -22,10 +23,8 @@ class DFS implements Code {
             return dp[target];
         }
         dp[target] = 0;
-        for (int num : nums) {
-            if (num <= target) {
-                dp[target] += f(nums, target - num, dp);
-            }
+        for (int i = 0; i < nums.length && nums[i] <= target; i++) {
+            dp[target] += f(nums, target - nums[i], dp);
         }
         return dp[target];
     }

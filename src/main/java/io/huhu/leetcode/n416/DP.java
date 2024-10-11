89fc528a9882f460fb9e03eaad57e4a164d5f577
NoahChen <noah.chen@batechworks.com>
2024-10-11 17:12:24 +0800

    416. Partition Equal Subset Sum alg: DynamicProgramming opt: SpaceCompression

--- src/main/java/io/huhu/leetcode/n416/DP.java
+++ src/main/java/io/huhu/leetcode/n416/DP.java
@@ -11,20 +11,17 @@ class DP implements Code {
         if ((sum & 1) == 1) {
             return false;
         }
-        int n = nums.length, avg = sum >> 1;
-        boolean[][] dp = new boolean[n][avg + 1];
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
-        }
-        for (int j = 1; j <= avg; j++) {
-            dp[0][j] = nums[0] == j;
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= avg; j++) {
-                dp[i][j] = j >= nums[i] && dp[i - 1][j - nums[i]] || dp[i - 1][j];
+        int n = sum >> 1;
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int j = n; j >= 1; j--) {
+                if (j >= num && dp[j - num]) {
+                    dp[j] = true;
+                }
             }
         }
-        return dp[n - 1][avg];
+        return dp[n];
     }
 
 }

c0c5b951b372b49ee0c1553f9d4136d288e03f81
NoahChen <noah.chen@batechworks.com>
2024-10-11 17:16:39 +0800

    416. Partition Equal Subset Sum alg: DynamicProgramming opt: MergeBranch

--- src/main/java/io/huhu/leetcode/n416/DP.java
+++ src/main/java/io/huhu/leetcode/n416/DP.java
@@ -4,19 +4,28 @@ class DP implements Code {
 
     @Override
     public boolean canPartition(int[] nums) {
-        int sum = 0;
+        if (nums.length < 2) {
+            return false;
+        }
+        int sum = 0, max = Integer.MIN_VALUE;
         for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
             sum += num;
         }
         if ((sum & 1) == 1) {
             return false;
         }
         int n = sum >> 1;
+        if (max > n) {
+            return false;
+        }
         boolean[] dp = new boolean[n + 1];
         dp[0] = true;
         for (int num : nums) {
-            for (int j = n; j >= 1; j--) {
-                if (j >= num && dp[j - num]) {
+            for (int j = n; j >= num; j--) {
+                if (dp[j - num]) {
                     dp[j] = true;
                 }
             }

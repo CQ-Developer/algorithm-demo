a0c01aa6fc54a76172ff532ae1696ea696426f7d
chen <18551681083@163.com>
2024-10-13 15:01:57 +0800

    1553. Minimum Number of Days to Eat N Oranges alg: DepthFirstSearch opt: Memoization

---
+++ src/main/java/io/huhu/leetcode/greedy/n1553/DFS.java
@@ -0,0 +1,25 @@
+package io.huhu.leetcode.greedy.n1553;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class DFS implements Code {
+
+    @Override
+    public int minDays(int n) {
+        return f(n, new HashMap<>());
+    }
+
+    private int f(int n, Map<Integer, Integer> map) {
+        if (n <= 1) {
+            return n;
+        }
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        int ans = Math.min(n % 2 + f(n / 2, map) + 1, n % 3 + f(n / 3, map) + 1);
+        map.put(n, ans);
+        return ans;
+    }
+
+}

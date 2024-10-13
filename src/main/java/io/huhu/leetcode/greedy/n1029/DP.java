1aecd0424ef083337a71e535386badaac9bdf384
chen <18551681083@163.com>
2024-10-13 09:33:07 +0800

    1029.Two City Scheduling alg: DynamicProgramming

---
+++ src/main/java/io/huhu/leetcode/greedy/n1029/DP.java
@@ -0,0 +1,26 @@
+package io.huhu.leetcode.greedy.n1029;
+
+class DP implements Code {
+
+    @Override
+    public int twoCitySchedCost(int[][] costs) {
+        int n = costs.length, m = n >> 1;
+        int[][][] dp = new int[n + 1][m + 1][m + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int a = 0; a <= m; a++) {
+                for (int b = 0; b <= m; b++) {
+                    int ans = Integer.MAX_VALUE;
+                    if (a > 0) {
+                        ans = Math.min(ans, dp[i + 1][a - 1][b] + costs[i][0]);
+                    }
+                    if (b > 0) {
+                        ans = Math.min(ans, dp[i + 1][a][b - 1] + costs[i][1]);
+                    }
+                    dp[i][a][b] = ans;
+                }
+            }
+        }
+        return dp[0][m][m];
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/greedy/n1029/DPTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.greedy.n1029;
+
+class DPTest extends CodeTest {
+
+    @Override
+    Code getCode() {
+        return new DP();
+    }
+
+}

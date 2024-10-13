e4c3b7c45be12bacb6a694e5359f7e598f17c102
chen <18551681083@163.com>
2024-10-13 09:36:04 +0800

    1029.Two City Scheduling alg: DynamicProgramming opt: SpaceCompression

--- src/main/java/io/huhu/leetcode/greedy/n1029/DP.java
+++ src/main/java/io/huhu/leetcode/greedy/n1029/DP.java
@@ -5,22 +5,22 @@ class DP implements Code {
     @Override
     public int twoCitySchedCost(int[][] costs) {
         int n = costs.length, m = n >> 1;
-        int[][][] dp = new int[n + 1][m + 1][m + 1];
+        int[][] dp = new int[m + 1][m + 1];
         for (int i = n - 1; i >= 0; i--) {
-            for (int a = 0; a <= m; a++) {
-                for (int b = 0; b <= m; b++) {
+            for (int a = m; a >= 0; a--) {
+                for (int b = m; b >= 0; b--) {
                     int ans = Integer.MAX_VALUE;
                     if (a > 0) {
-                        ans = Math.min(ans, dp[i + 1][a - 1][b] + costs[i][0]);
+                        ans = Math.min(ans, dp[a - 1][b] + costs[i][0]);
                     }
                     if (b > 0) {
-                        ans = Math.min(ans, dp[i + 1][a][b - 1] + costs[i][1]);
+                        ans = Math.min(ans, dp[a][b - 1] + costs[i][1]);
                     }
-                    dp[i][a][b] = ans;
+                    dp[a][b] = ans;
                 }
             }
         }
-        return dp[0][m][m];
+        return dp[m][m];
     }
 
 }

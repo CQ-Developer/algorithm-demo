691e002a2c2629fccc0a06c0c2a090d5b3f7bedc
chen <18551681083@163.com>
2024-10-15 00:36:43 +0800

    518. Coin Change II alg: DynamicProgramming

--- src/main/java/io/huhu/leetcode/n518/DP.java
+++ src/main/java/io/huhu/leetcode/n518/DP.java
@@ -1,19 +1,19 @@
 package io.huhu.leetcode.n518;
 
-import java.util.Arrays;
-
 class DP implements Code {
 
     @Override
     public int change(int amount, int[] coins) {
-        Arrays.sort(coins);
         int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
+        int[][] dp = new int[n + 1][amount + 1];
+        dp[n][0] = 1;
         for (int i = n - 1; i >= 0; i--) {
             dp[i][0] = 1;
             for (int j = 1; j <= amount; j++) {
-                for (int k = i; k < n && coins[k] <= j; k++) {
-                    dp[i][j] += dp[k][j - coins[k]];
+                if (j < coins[i]) {
+                    dp[i][j] = dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i][j - coins[i]] + dp[i + 1][j];
                 }
             }
         }

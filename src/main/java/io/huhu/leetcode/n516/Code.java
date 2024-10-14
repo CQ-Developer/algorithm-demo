5f43de21f3c3adfd42bfe8357513913f1e9f9cac
NoahChen <noah.chen@batechworks.com>
2024-10-14 15:57:38 +0800

    516. Longest Palindromic Subsequence

---
+++ src/main/java/io/huhu/leetcode/n516/Code.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.n516;
+
+/**
+ * <a href="https://leetcode.cn/problems/longest-palindromic-subsequence/description/">
+ * Longest Palindromic Subsequence
+ * </a>
+ */
+interface Code {
+
+    int longestPalindromeSubseq(String s);
+
+}

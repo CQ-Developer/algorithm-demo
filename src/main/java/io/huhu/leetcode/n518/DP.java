c33a07484fbdb14485981d0be61fc85422b8d1b0
chen <18551681083@163.com>
2024-10-15 00:43:58 +0800

    518. Coin Change II alg: DynamicProgramming opt: SpaceCompression

--- src/main/java/io/huhu/leetcode/n518/DP.java
+++ src/main/java/io/huhu/leetcode/n518/DP.java
@@ -4,20 +4,14 @@ class DP implements Code {
 
     @Override
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        dp[n][0] = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            dp[i][0] = 1;
-            for (int j = 1; j <= amount; j++) {
-                if (j < coins[i]) {
-                    dp[i][j] = dp[i + 1][j];
-                } else {
-                    dp[i][j] = dp[i][j - coins[i]] + dp[i + 1][j];
-                }
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
             }
         }
-        return dp[0][amount];
+        return dp[amount];
     }
 
 }

1fc796994303c83ab15830f5db157241a251813d
NoahChen <noah.chen@batechworks.com>
2024-10-15 15:41:25 +0800

    542. 01 Matrix alg: DynamicProgramming

---
+++ src/main/java/io/huhu/leetcode/n542/DP.java
@@ -0,0 +1,39 @@
+package io.huhu.leetcode.n542;
+
+class DP implements Code {
+
+    @Override
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = mat[i][j] == 0 ? 0 : 10000;
+            }
+        }
+        // 从左上角推
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i - 1 >= 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                }
+                if (j - 1 >= 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                }
+            }
+        }
+        // 从右下角推
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i + 1 < m) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+                }
+                if (j + 1 < n) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+                }
+            }
+        }
+        return dp;
+    }
+
+}

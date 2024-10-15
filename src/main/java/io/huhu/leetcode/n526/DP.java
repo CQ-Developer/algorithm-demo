d0007f4ce9bf89005b2c061c1ee712d0f166e505
NoahChen <noah.chen@batechworks.com>
2024-10-15 10:02:09 +0800

    526. Beautiful Arrangement alg: DynamicProgramming opt: SpaceCompression

--- src/main/java/io/huhu/leetcode/n526/DP.java
+++ src/main/java/io/huhu/leetcode/n526/DP.java
@@ -7,18 +7,22 @@ class DP implements Code {
     @Override
     public int countArrangement(int n) {
         int m = 1 << (n + 1);
-        int[][] dp = new int[n + 1][m];
-        Arrays.fill(dp[0], 1);
+        int[] pre = new int[m], cur = new int[m];
+        Arrays.fill(pre, 1);
         for (int i = 1; i <= n; i++) {
             for (int j = m - 1; j >= 0; j--) {
                 for (int num = 1; num <= n; num++) {
                     if (((1 << num) & j) == 0 && (i % num == 0 || num % i == 0)) {
-                        dp[i][j] += dp[i - 1][(1 << num) | j];
+                        cur[j] += pre[(1 << num) | j];
                     }
                 }
             }
+            for (int j = 0; j < m; j++) {
+                pre[j] = cur[j];
+                cur[j] = 0;
+            }
         }
-        return dp[n][0];
+        return pre[0];
     }
 
 }

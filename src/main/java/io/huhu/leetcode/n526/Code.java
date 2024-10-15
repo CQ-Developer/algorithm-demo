649a93f6c67bb67145b12290a54c0dedb2aa549d
NoahChen <noah.chen@batechworks.com>
2024-10-15 08:16:51 +0800

    526. Beautiful Arrangement

---
+++ src/main/java/io/huhu/leetcode/n526/Code.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.n526;
+
+/**
+ * <a href="https://leetcode.cn/problems/beautiful-arrangement/description/">
+ * Beautiful Arrangement
+ * </a>
+ */
+interface Code {
+
+    int countArrangement(int n);
+
+}

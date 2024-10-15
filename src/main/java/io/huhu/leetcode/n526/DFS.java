63fd477c4be1d80314d7680f85d3bc27e43388a7
NoahChen <noah.chen@batechworks.com>
2024-10-15 08:57:41 +0800

    526. Beautiful Arrangement alg: DepthFirstSearch opt: Memoization

--- src/main/java/io/huhu/leetcode/n526/DFS.java
+++ src/main/java/io/huhu/leetcode/n526/DFS.java
@@ -1,23 +1,32 @@
 package io.huhu.leetcode.n526;
 
+import java.util.Arrays;
+
 class DFS implements Code {
 
     @Override
     public int countArrangement(int n) {
-        return f(1, 0, n);
+        int[][] dp = new int[n + 1][1 << (n + 1)];
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+        return f(n, 0, n, dp);
     }
 
-    private int f(int i, int s, int n) {
-        if (i == n + 1) {
+    private int f(int i, int j, int n, int[][] dp) {
+        if (i == 0) {
             return 1;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
         int ans = 0;
         for (int num = 1; num <= n; num++) {
-            if (((1 << num) & s) == 0 && (i % num == 0 || num % i == 0)) {
-                ans += f(i + 1, (1 << num) | s, n);
+            if (((1 << num) & j) == 0 && (i % num == 0 || num % i == 0)) {
+                ans += f(i - 1, (1 << num) | j, n, dp);
             }
         }
-        return ans;
+        return dp[i][j] = ans;
     }
 
 }

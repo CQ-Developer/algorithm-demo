c5dd52ee0c723f7c6154403cd952e900afc9d516
NoahChen <noah.chen@batechworks.com>
2024-10-17 10:01:51 +0800

    nothing here

--- src/main/java/io/huhu/leetcode/n650/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n650/DynamicProgramming.java
@@ -2,29 +2,45 @@ package io.huhu.leetcode.n650;
 
 class DynamicProgramming implements Code {
 
+    /**
+     * f(i)表示得到i个A的最少操作次数
+     * 假设当前拥有j个A, 由于只能进行全量复制, 所有j必须能被i整除, i % j == 0
+     * 复制1次 + 粘贴i/j-1次即可得到i
+     * f(i) = Min( f(j) + i / j - 1 + 1 )
+     * = Min( f(j) + i / j )
+     */
     @Override
     public int minSteps(int n) {
-        int[][] dp = new int[n + 1][n + 1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
-                dp[i][j] = 1001;
-            }
-        }
-        dp[1][0] = 0;
-        dp[1][1] = 1;
+        int[] dp = new int[n + 1];
         for (int i = 2; i <= n; i++) {
-            int min = 1001;
-            for (int j = 0; j <= i / 2; j++) {
-                dp[i][j] = dp[i - j][j] + 1;
-                min = Math.min(min, dp[i][j]);
+            // 从1个A一个个粘过来
+            dp[i] = i;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    /*
+                     * i % j = 0
+                     * 必然有 j 和 i / j 都是 i 的因数
+                     *
+                     * 假设 a = j     必然 i % a = 0
+                     * 假设 b = i / j 必然 i % b = 0
+                     *
+                     * 情况a:
+                     * f(i) = f(a) + i / a
+                     * f(i) = f(j) + i / j
+                     *
+                     * 情况b:
+                     * f(i) = f(b) + i / b
+                     * f(i) = f(i / j) + j
+                     *
+                     * dp[i] = Math.min(dp[i], dp[j] + i / j);
+                     * dp[i] = Math.min(dp[i], dp[i / j] + j);
+                     */
+                    dp[i] = dp[j] + dp[i / j];
+                    break;
+                }
             }
-            dp[i][i] = min + 1;
         }
-        int ans = 1001;
-        for (int j = 0; j <= n; j++) {
-            ans = Math.min(ans, dp[n][j]);
-        }
-        return ans;
+        return dp[n];
     }
 
 }
--- src/main/java/io/huhu/leetcode/n650/Mathematics.java
+++ src/main/java/io/huhu/leetcode/n650/Mathematics.java
@@ -2,6 +2,16 @@ package io.huhu.leetcode.n650;
 
 class Mathematics implements Code {
 
+    /**
+     * 举例
+     * 4 -> 2 + f(2) 复制1次 粘贴1次
+     * 5
+     * 6 -> 2 + f(3) 复制1次 粘贴1次
+     * 7
+     * 8 -> 2 + f(4) 复制1次 粘贴1次
+     * 9 -> 3 + f(3) 复制1次 粘贴2次
+     * 本质就是质因数分解
+     */
     @Override
     public int minSteps(int n) {
         if (n == 1) {

12b1cfef9c65f432666c6f1b3d5aa434acab8169
NoahChen <noah.chen@batechworks.com>
2024-10-17 18:12:42 +0800

    LCR132. 砍竹子II

---
+++ src/main/java/io/huhu/leetcode/greedy/lcr132/Code.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.greedy.lcr132;
+
+/**
+ * <a href="https://leetcode.cn/problems/jian-sheng-zi-ii-lcof/description/">
+ * 砍竹子 II
+ * </a>
+ */
+interface Code {
+
+    int cuttingBamboo(int bamboo_len);
+
+}
---
+++ src/test/java/io/huhu/leetcode/greedy/lcr132/CodeTest.java
@@ -0,0 +1,23 @@
+package io.huhu.leetcode.greedy.lcr132;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+abstract class CodeTest {
+
+    Code code;
+
+    abstract Code getCode();
+
+    @BeforeEach
+    void setup() {
+        code = getCode();
+    }
+
+    @Test
+    void test_1() {
+        Assertions.assertEquals(81, code.cuttingBamboo(12));
+    }
+
+}

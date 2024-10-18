8b77acb2cb7b11ac5fdc8bcec2be08b5400b2dad
chen <18551681083@163.com>
2024-10-18 23:16:45 +0800

    712. Minimum ASCII Delete Sum for Two Strings alg: DFS opt: Memoization

--- src/main/java/io/huhu/leetcode/n712/DFS.java
+++ src/main/java/io/huhu/leetcode/n712/DFS.java
@@ -7,15 +7,14 @@ class DFS implements Code {
     @Override
     public int minimumDeleteSum(String s1, String s2) {
         char[] c1 = s1.toCharArray(), c2 = s2.toCharArray();
-        int m = c1.length, n = c2.length;
-        int[][] dp = new int[m + 1][n + 1];
-        for (int[] a : dp) {
+        int[][] m = new int[c1.length + 1][c2.length + 1];
+        for (int[] a : m) {
             Arrays.fill(a, -1);
         }
-        return f(c1, c2, 0, 0, dp);
+        return f(c1, c2, 0, 0, m);
     }
 
-    private int f(char[] c1, char[] c2, int i, int j, int[][] dp) {
+    private int f(char[] c1, char[] c2, int i, int j, int[][] m) {
         if (i == c1.length && j == c2.length) {
             return 0;
         }
@@ -35,18 +34,18 @@ class DFS implements Code {
             }
             return ans;
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        if (m[i][j] != -1) {
+            return m[i][j];
         }
         int ans;
         if (c1[i] == c2[j]) {
-            ans = f(c1, c2, i + 1, j + 1, dp);
+            ans = f(c1, c2, i + 1, j + 1, m);
         } else {
-            int a = f(c1, c2, i + 1, j, dp) + c1[i];
-            int b = f(c1, c2, i, j + 1, dp) + c2[j];
+            int a = f(c1, c2, i + 1, j, m) + c1[i];
+            int b = f(c1, c2, i, j + 1, m) + c2[j];
             ans = Math.min(a, b);
         }
-        return dp[i][j] = ans;
+        return m[i][j] = ans;
     }
 
 }

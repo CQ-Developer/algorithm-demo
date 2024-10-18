337f7d7bc706432e8f353df1d5805b0202075648
NoahChen <noah.chen@batechworks.com>
2024-10-18 14:58:52 +0800

    688. Knight Probability in Chessboard alg: DFS opt: Memoization

--- src/main/java/io/huhu/leetcode/n688/Dfs.java
+++ src/main/java/io/huhu/leetcode/n688/Dfs.java
@@ -1,29 +1,40 @@
 package io.huhu.leetcode.n688;
 
+import java.util.Arrays;
+
 public class Dfs implements Code {
 
     @Override
     public double knightProbability(int n, int k, int row, int column) {
-        return f(n, k, row, column);
+        double[][][] m = new double[k + 1][n][n];
+        for (double[][] a : m) {
+            for (double[] b : a) {
+                Arrays.fill(b, -1);
+            }
+        }
+        return f(n, k, row, column, m);
     }
 
-    public double f(int n, int k, int i, int j) {
+    public double f(int n, int k, int i, int j, double[][][] m) {
         if (i < 0 || i >= n || j < 0 || j >= n) {
             return 0;
         }
         if (k == 0) {
             return 1;
         }
+        if (m[k][i][j] != -1) {
+            return m[k][i][j];
+        }
         double ans = 0;
-        ans += f(n, k - 1, i + 1, j + 2) / 8;
-        ans += f(n, k - 1, i + 1, j - 2) / 8;
-        ans += f(n, k - 1, i - 1, j + 2) / 8;
-        ans += f(n, k - 1, i - 1, j - 2) / 8;
-        ans += f(n, k - 1, i + 2, j + 1) / 8;
-        ans += f(n, k - 1, i + 2, j - 1) / 8;
-        ans += f(n, k - 1, i - 2, j + 1) / 8;
-        ans += f(n, k - 1, i - 2, j - 1) / 8;
-        return ans;
+        ans += f(n, k - 1, i + 1, j + 2, m) / 8;
+        ans += f(n, k - 1, i + 1, j - 2, m) / 8;
+        ans += f(n, k - 1, i - 1, j + 2, m) / 8;
+        ans += f(n, k - 1, i - 1, j - 2, m) / 8;
+        ans += f(n, k - 1, i + 2, j + 1, m) / 8;
+        ans += f(n, k - 1, i + 2, j - 1, m) / 8;
+        ans += f(n, k - 1, i - 2, j + 1, m) / 8;
+        ans += f(n, k - 1, i - 2, j - 1, m) / 8;
+        return m[k][i][j] = ans;
     }
 
 }

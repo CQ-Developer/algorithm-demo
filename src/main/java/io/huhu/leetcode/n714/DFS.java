f6bdf3771c9c3e03a345c0afa75e4327322a1ff1
chen <18551681083@163.com>
2024-10-19 14:20:38 +0800

    how to convert to DP

--- src/main/java/io/huhu/leetcode/n714/DFS.java
+++ src/main/java/io/huhu/leetcode/n714/DFS.java
@@ -29,4 +29,56 @@ class DFS implements Code {
         return m[i][j] = ans;
     }
 
+    private int dp1(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] f = new int[n + 1][2];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 1; j >= 0; j--) {
+                int ans = f[i + 1][j];
+                if (j == 1) {
+                    ans = Math.max(ans, f[i + 1][0] + prices[i] - fee);
+                } else {
+                    ans = Math.max(ans, f[i + 1][1] - prices[i]);
+                }
+                f[i][j] = ans;
+            }
+        }
+        return f[0][0];
+    }
+
+    private int dp2(int[] prices, int fee) {
+        int[] f = new int[2];
+        for (int i = prices.length - 1; i >= 0; i--) {
+            for (int j = 1; j >= 0; j--) {
+                int _f1 = f[1];
+                if (j == 1) {
+                    f[1] = Math.max(f[1], f[0] + prices[i] - fee);
+                } else {
+                    f[0] = Math.max(f[0], _f1 - prices[i]);
+                }
+            }
+        }
+        return f[0];
+    }
+
+    private int dp3(int[] prices, int fee) {
+        int[] f = new int[2];
+        for (int i = prices.length - 1; i >= 0; i--) {
+            int _f1 = f[1];
+            f[1] = Math.max(f[1], f[0] + prices[i] - fee);
+            f[0] = Math.max(f[0], _f1 - prices[i]);
+        }
+        return f[0];
+    }
+
+    private int dp4(int[] prices, int fee) {
+        int f0 = 0, f1 = 0;
+        for (int i = prices.length - 1; i >= 0; i--) {
+            int _f1 = f1;
+            f1 = Math.max(f1, f0 + prices[i] - fee);
+            f0 = Math.max(f0, _f1 - prices[i]);
+        }
+        return f0;
+    }
+
 }

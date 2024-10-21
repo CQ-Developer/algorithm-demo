1e69dfeebdab5e42a159f493cd9e0ea67562007a
NoahChen <noah.chen@batechworks.com>
2024-10-21 11:34:34 +0800

    764. Largest Plus Sign alg: DP

---
+++ src/main/java/io/huhu/leetcode/n764/DP.java
@@ -0,0 +1,68 @@
+package io.huhu.leetcode.n764;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class DP implements Code {
+
+    @Override
+    public int orderOfLargestPlusSign(int n, int[][] mines) {
+        Set<Integer> set = new HashSet<>();
+        for (int[] mine : mines) {
+            set.add(mine[0] * n + mine[1]);
+        }
+        int[][] f = new int[n][n];
+        for (int[] a : f) {
+            Arrays.fill(a, n);
+        }
+        for (int i = 0; i < n; i++) {
+            // left -> right
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (set.contains(i * n + j)) {
+                    count = 0;
+                } else {
+                    count++;
+                }
+                f[i][j] = Math.min(f[i][j], count);
+            }
+            // right -> left
+            count = 0;
+            for (int j = n - 1; j >= 0; j--) {
+                if (set.contains(i * n + j)) {
+                    count = 0;
+                } else {
+                    count++;
+                }
+                f[i][j] = Math.min(f[i][j], count);
+            }
+        }
+        int ans = 0;
+        for (int j = 0; j < n; j++) {
+            // up -> down
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (set.contains(i * n + j)) {
+                    count = 0;
+                } else {
+                    count++;
+                }
+                f[i][j] = Math.min(f[i][j], count);
+            }
+            // down -> up
+            count = 0;
+            for (int i = n - 1; i >= 0; i--) {
+                if (set.contains(i * n + j)) {
+                    count = 0;
+                } else {
+                    count++;
+                }
+                f[i][j] = Math.min(f[i][j], count);
+                ans = Math.max(ans, f[i][j]);
+            }
+        }
+        return ans;
+    }
+
+}

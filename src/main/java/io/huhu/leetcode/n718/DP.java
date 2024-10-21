c331bf18464891fc61ef2d6924b35729e5b2231d
NoahChen <noah.chen@batechworks.com>
2024-10-21 08:34:54 +0800

    718. Maximum Length of Repeated Subarray alg: DP opt: Reduce Dimension

--- src/main/java/io/huhu/leetcode/n718/DP.java
+++ src/main/java/io/huhu/leetcode/n718/DP.java
@@ -5,12 +5,12 @@ class DP implements Code {
     @Override
     public int findLength(int[] nums1, int[] nums2) {
         int m = nums1.length, n = nums2.length;
-        int[][] dp = new int[m + 1][n + 1];
+        int[] f = new int[n + 1];
         int ans = 0;
         for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = nums1[i - 1] == nums2[j - 1] ? dp[i - 1][j - 1] + 1 : 0;
-                ans = Math.max(ans, dp[i][j]);
+            for (int j = n; j >= 1; j--) {
+                f[j] = nums1[i - 1] == nums2[j - 1] ? f[j - 1] + 1 : 0;
+                ans = Math.max(ans, f[j]);
             }
         }
         return ans;

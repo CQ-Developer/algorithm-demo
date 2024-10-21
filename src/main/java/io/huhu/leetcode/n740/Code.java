b27c7dbdcb2da607ee07224584e17b0165784967
NoahChen <noah.chen@batechworks.com>
2024-10-21 08:52:38 +0800

    740. Delete and Earn

---
+++ src/main/java/io/huhu/leetcode/n740/Code.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.n740;
+
+/**
+ * <a href="https://leetcode.cn/problems/delete-and-earn/description/">
+ * Delete and Earn
+ * </a>
+ */
+interface Code {
+
+    int deleteAndEarn(int[] nums);
+
+}

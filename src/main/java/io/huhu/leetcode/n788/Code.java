c3e7d45aaaf1745dbc5616de04779dfaf7278458
NoahChen <noah.chen@batechworks.com>
2024-10-22 14:01:16 +0800

    788. Rotated Digits alg: DFS

--- src/main/java/io/huhu/leetcode/n788/Code.java
+++ src/main/java/io/huhu/leetcode/n788/Code.java
@@ -3,6 +3,7 @@ package io.huhu.leetcode.n788;
 /**
  * <a href="https://leetcode.cn/problems/rotated-digits/description/">
  * Rotated Digits
+ * 旋转数字
  * </a>
  */
 interface Code {
---
+++ src/test/java/io/huhu/leetcode/n788/DFSTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.n788;
+
+class DFSTest extends CodeTest {
+
+    @Override
+    Code getCode() {
+        return new DFS();
+    }
+
+}

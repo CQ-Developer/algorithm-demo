0a374d7caf4454e9a0914b01527b9ac6c87b653c
NoahChen <noah.chen@batechworks.com>
2024-10-24 09:30:42 +0800

    799. Champagne Tower alg: Dynamic Programming opt: Reduce Dimension

--- src/main/java/io/huhu/leetcode/n799/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n799/DynamicProgramming.java
@@ -4,18 +4,23 @@ class DynamicProgramming implements Code {
 
     @Override
     public double champagneTower(int poured, int query_row, int query_glass) {
-        double[][] f = new double[query_row + 1][query_row + 1];
-        f[0][0] = poured;
+        double[] n = new double[query_row + 1];
+        double[] f = new double[query_row + 1];
+        f[0] = poured;
         for (int i = 0; i < query_row; i++) {
             for (int j = 0; j <= i; j++) {
-                if (f[i][j] > 1) {
-                    double overflow = (f[i][j] - 1) / 2;
-                    f[i + 1][j] += overflow;
-                    f[i + 1][j + 1] += overflow;
+                if (f[j] > 1) {
+                    double overflow = (f[j] - 1) / 2;
+                    n[j] += overflow;
+                    n[j + 1] += overflow;
                 }
             }
+            for (int j = 0; j <= query_row; j++) {
+                f[j] = n[j];
+                n[j] = 0;
+            }
         }
-        return Math.min(1, f[query_row][query_glass]);
+        return Math.min(1.0, f[query_glass]);
     }
 
 }

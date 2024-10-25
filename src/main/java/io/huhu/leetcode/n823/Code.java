399908cfe0956e23a2f51e753a2ce52e76e87a75
NoahChen <noah.chen@batechworks.com>
2024-10-25 10:24:21 +0800

    823. Binary Trees With Factors

---
+++ src/main/java/io/huhu/leetcode/n823/Code.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.n823;
+
+/**
+ * <a href="https://leetcode.cn/problems/binary-trees-with-factors/description/">
+ * Binary Trees With Factors - 带因子的二叉树
+ * </a>
+ */
+interface Code {
+
+    int numFactoredBinaryTrees(int[] arr);
+
+}

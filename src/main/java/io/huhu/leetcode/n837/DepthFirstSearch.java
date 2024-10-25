d64aae8ce559f21ca2a7225ea1d13629d402499a
NoahChen <noah.chen@batechworks.com>
2024-10-25 16:41:08 +0800

    837. New 21 Game alg: Depth First Search opt: Memoization ps: TLE

--- src/main/java/io/huhu/leetcode/n837/DepthFirstSearch.java
+++ src/main/java/io/huhu/leetcode/n837/DepthFirstSearch.java
@@ -1,21 +1,31 @@
 package io.huhu.leetcode.n837;
 
+import java.util.Arrays;
+
+/**
+ * TLE
+ */
 class DepthFirstSearch implements Code {
 
     @Override
     public double new21Game(int n, int k, int maxPts) {
-        return f(n, k, maxPts, 0);
+        double[] dp = new double[k];
+        Arrays.fill(dp, -1);
+        return f(n, k, maxPts, 0, dp);
     }
 
-    private double f(int n, int k, int p, int c) {
+    private double f(int n, int k, int p, int c, double[] dp) {
         if (c >= k) {
             return c <= n ? 1 : 0;
         }
-        double ans = 0;
-        for (int i = 1; i <= p; i++) {
-            ans += f(n, k, p, c + i);
+        if (dp[c] != -1) {
+            return dp[c];
         }
-        return ans / p;
+        double ans = 0;
+        for (int i = 1; i <= p && i + c <= n; i++) {
+            ans += f(n, k, p, c + i, dp);
+        }
+        return dp[c] = ans / p;
     }
 
 }

0a8c36c812bea781e77a310872e6d17aa7dbd8c8
NoahChen <noah.chen@batechworks.com>
2024-10-25 09:05:05 +0800

    813. Largest Sum of Averages alg: Dynamic Programming opt: Offset Index

--- src/main/java/io/huhu/leetcode/n813/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n813/DynamicProgramming.java
@@ -9,22 +9,16 @@ class DynamicProgramming implements Code {
         for (int i = 0; i < n; i++) {
             p[i + 1] = p[i] + nums[i];
         }
-        double[][] f = new double[n][k + 1];
-        for (int i = 0; i < n; i++) {
-            f[i][1] = 1.0 * p[i + 1] / (i + 1);
-        }
-        for (int i = 0; i < n; i++) {
+        double[][] f = new double[n + 1][k + 1];
+        for (int i = 1; i <= n; i++) {
+            f[i][1] = 1.0 * p[i] / i;
             for (int j = 2; j <= k; j++) {
-                for (int _i = 0; _i <= i; _i++) {
-                    if (_i - 1 == -1) {
-                        f[i][j] = 0;
-                    } else {
-                        f[i][j] = Math.max(f[i][j], 1.0 * (p[i + 1] - p[_i]) / (i - _i + 1) + f[_i - 1][j - 1]);
-                    }
+                for (int _i = 1; _i <= i; _i++) {
+                    f[i][j] = Math.max(f[i][j], 1.0 * (p[i] - p[_i - 1]) / (i - _i + 1) + f[_i - 1][j - 1]);
                 }
             }
         }
-        return f[n - 1][k];
+        return f[n][k];
     }
 
 }

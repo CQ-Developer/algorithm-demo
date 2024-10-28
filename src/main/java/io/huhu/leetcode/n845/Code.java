021dfc08b0e625da824d72a734fc86b8654322e5
NoahChen <noah.chen@batechworks.com>
2024-10-28 08:51:19 +0800

    845. Longest Mountain in Array

---
+++ src/main/java/io/huhu/leetcode/n845/Code.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.n845;
+
+/**
+ * <a href="https://leetcode.cn/problems/longest-mountain-in-array/description/">
+ * Longest Mountain in Array - 数组中的最长山脉
+ * </a>
+ */
+interface Code {
+
+    int longestMountain(int[] arr);
+
+}

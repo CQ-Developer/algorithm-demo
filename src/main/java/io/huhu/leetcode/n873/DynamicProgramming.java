82712a97c2c9cc5198bbfa04d8ba531c75efcd92
NoahChen <noah.chen@batechworks.com>
2024-10-28 14:44:44 +0800

    873. Length of Longest Fibonacci Subsequence alg: Dynamic Programming

---
+++ src/main/java/io/huhu/leetcode/n873/DynamicProgramming.java
@@ -0,0 +1,32 @@
+package io.huhu.leetcode.n873;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class DynamicProgramming implements Code {
+
+    @Override
+    public int lenLongestFibSubseq(int[] arr) {
+        int n = arr.length;
+        Map<Integer, Integer> table = HashMap.newHashMap(n);
+        for (int i = 0; i < n; i++) {
+            table.put(arr[i], i);
+        }
+        int ans = 0;
+        int[][] f = new int[n][n];
+        for (int k = 0; k < n; k++) {
+            for (int j = k - 1; j >= 0; j--) {
+                int i = table.getOrDefault(arr[k] - arr[j], -1);
+                if (i >= j) {
+                    break;
+                }
+                if (i != -1) {
+                    f[j][k] = Math.max(f[i][j] + 1, 3);
+                }
+                ans = Math.max(ans, f[j][k]);
+            }
+        }
+        return ans;
+    }
+
+}

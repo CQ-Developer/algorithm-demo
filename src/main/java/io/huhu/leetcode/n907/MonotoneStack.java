f2829ebe249876c907b95f2d080051a4a58f7999
NoahChen <noah.chen@batechworks.com>
2024-10-30 15:29:25 +0800

    907. Sum of Subarray Minimums alg: Monotone Stack opt: Reduce Loop

--- src/main/java/io/huhu/leetcode/n907/MonotoneStack.java
+++ src/main/java/io/huhu/leetcode/n907/MonotoneStack.java
@@ -1,7 +1,6 @@
 package io.huhu.leetcode.n907;
 
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
 
 class MonotoneStack implements Code {
@@ -10,21 +9,16 @@ class MonotoneStack implements Code {
 
     @Override
     public int sumSubarrayMins(int[] arr) {
-        int n = arr.length;
-        int[] left = new int[n], right = new int[n];
-        Arrays.fill(right, n);
+        long ans = 0;
         Deque<Integer> stack = new ArrayDeque<>();
         stack.addFirst(-1);
-        for (int i = 0; i < n; i++) {
-            while (stack.size() > 1 && arr[stack.getFirst()] >= arr[i]) {
-                right[stack.removeFirst()] = i;
+        for (int r = 0; r <= arr.length; r++) {
+            int x = r < arr.length ? arr[r] : -1;
+            while (stack.size() > 1 && arr[stack.getFirst()] >= x) {
+                int i = stack.removeFirst();
+                ans = (ans + (long) arr[i] * (i - stack.getFirst()) * (r - i)) % M;
             }
-            left[i] = stack.getFirst();
-            stack.addFirst(i);
-        }
-        long ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = (ans + (long) arr[i] * (i - left[i]) * (right[i] - i)) % M;
+            stack.addFirst(r);
         }
         return (int) ans;
     }

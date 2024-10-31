e0e8e968bdc68ae066a23afa905ec96014c6968f
chen <18551681083@163.com>
2024-10-31 22:15:22 +0800

    926. Flip String to Monotone Increasing alg: Dynamic Programming opt: Reduce Temporary Variable

--- src/main/java/io/huhu/leetcode/n926/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n926/DynamicProgramming.java
@@ -6,9 +6,8 @@ class DynamicProgramming implements Code {
     public int minFlipsMonoIncr(String s) {
         int p0 = 0, p1 = 0;
         for (char c : s.toCharArray()) {
-            int _p0 = p0 + (c == '0' ? 0 : 1);
-            p1 = Math.min(p0, p1 + (c == '0' ? 1 : 0));
-            p0 = _p0;
+            p1 = Math.min(p0, p1) + ('1' - c);
+            p0 += c - '0';
         }
         return Math.min(p0, p1);
     }

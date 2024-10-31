2279ab100706deb32d63e0243b450764a81084a1
chen <18551681083@163.com>
2024-10-31 23:03:53 +0800

    931. Minimum Falling Path Sum alg: Depth First Search opt: Memoization

--- src/main/java/io/huhu/leetcode/n931/DepthFirstSearch.java
+++ src/main/java/io/huhu/leetcode/n931/DepthFirstSearch.java
@@ -1,31 +1,40 @@
 package io.huhu.leetcode.n931;
 
-import io.huhu.TLE;
+import io.huhu.AC;
 
-@TLE
+import java.util.Arrays;
+
+@AC
 class DepthFirstSearch implements Code {
 
     @Override
     public int minFallingPathSum(int[][] matrix) {
-        int ans = Integer.MAX_VALUE;
-        for (int j = 0; j < matrix[0].length; j++) {
-            ans = Math.min(ans, f(matrix, 0, j));
+        int ans = Integer.MAX_VALUE, n = matrix.length;
+        int[][] dp = new int[n][n];
+        for (int[] _dp : dp) {
+            Arrays.fill(_dp, ans);
+        }
+        for (int j = 0; j < n; j++) {
+            ans = Math.min(ans, f(matrix, n, 0, j, dp));
         }
         return ans;
     }
 
-    private int f(int[][] matrix, int i, int j) {
-        if (i == matrix.length) {
+    private int f(int[][] matrix, int n, int i, int j, int[][] dp) {
+        if (i == n) {
             return 0;
         }
-        int ans = f(matrix, i + 1, j);
+        if (dp[i][j] != Integer.MAX_VALUE) {
+            return dp[i][j];
+        }
+        int ans = f(matrix, n, i + 1, j, dp);
         if (j - 1 >= 0) {
-            ans = Math.min(ans, f(matrix, i + 1, j - 1));
+            ans = Math.min(ans, f(matrix, n, i + 1, j - 1, dp));
         }
-        if (j + 1 < matrix[i].length) {
-            ans = Math.min(ans, f(matrix, i + 1, j + 1));
+        if (j + 1 < n) {
+            ans = Math.min(ans, f(matrix, n, i + 1, j + 1, dp));
         }
-        return ans + matrix[i][j];
+        return dp[i][j] = ans + matrix[i][j];
     }
 
 }

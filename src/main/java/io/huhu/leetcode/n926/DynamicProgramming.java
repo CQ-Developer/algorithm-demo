97dcbcf705ba00bf1c3c727a74c1600ce03d3c20
NoahChen <noah.chen@batechworks.com>
2024-10-31 17:24:29 +0800

    926. Flip String to Monotone Increasing alg: Dynamic Programming opt: use different status definition

--- src/main/java/io/huhu/leetcode/n926/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n926/DynamicProgramming.java
@@ -9,21 +9,28 @@ class DynamicProgramming implements Code {
     public int minFlipsMonoIncr(String s) {
         char[] c = s.toCharArray();
         int n = c.length;
-        int[] f = new int[n];
-        for (int i = n - 2; i >= 0; i--) {
-            int leftDown = i - 1 < 0 ? 0 : f[i - 1];
-            f[i + 1] = c[i] <= c[i + 1] ? 0 : 1;
-            for (int j = i + 2; j < n; j++) {
-                int _leftDown = f[j];
-                if (c[i] == c[j]) {
-                    f[j] = Math.min(leftDown + 1, c[i] == '0' ? f[j] : f[j - 1]);
-                } else {
-                    f[j] = c[i] == '0' ? leftDown : Math.min(f[j] + 1, f[j - 1] + 1);
-                }
-                leftDown = _leftDown;
-            }
+        int[][] f = new int[n + 1][2];
+        for (int i = 0; i < n; i++) {
+            /*
+             * f[i][0]表示第i个字符为0的翻转次数
+             * 前一个字符必须为0, 即翻转次数f[i-1][0]
+             *     如果当前字符为0: f[i][0] = f[i-1][0]
+             * 前一个字符必须为0, 即翻转次数f[i-1][0]
+             *     如果当前字符为1: f[i][0] = f[i-1][0] + 1
+             */
+            f[i + 1][0] = f[i][0] + (c[i] == '0' ? 0 : 1);
+            /*
+             * f[i][1]表示第i个字符为1的翻转次数
+             * 如果前一个字符是0, 即翻转次数f[i-1][0]
+             *     那么不需要翻转, 即f[i][1] = f[i-1][0]
+             * 如果前一个字符是1, 即翻转次数f[i-1][1]
+             *     且当前字符为1, 那么不需要翻转: f[i][1] = f[i-1][1]
+             * 如果前一个字符是1, 即翻转次数f[i-1][1]
+             *     且当前字符为0, 那么必须翻转: f[i][1] = f[i-1][1] + 1
+             */
+            f[i + 1][1] = Math.min(f[i][0], f[i][1] + (c[i] == '0' ? 1 : 0));
         }
-        return f[n - 1];
+        return Math.min(f[n][0], f[n][1]);
     }
 
 }

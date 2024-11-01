63c81682786e36fb38bf6859c8077df3448e2efa
chen <18551681083@163.com>
2024-11-01 22:36:56 +0800

    983. Minimum Cost For Tickets alg: Dynamic Programming

---
+++ src/main/java/io/huhu/leetcode/n983/DynamicProgramming.java
@@ -0,0 +1,23 @@
+package io.huhu.leetcode.n983;
+
+class DynamicProgramming implements Code {
+
+    @Override
+    public int mincostTickets(int[] days, int[] costs) {
+        int n = days[days.length - 1];
+        int[] f = new int[n + 1];
+        for (int i = 1, j = 0; i <= n; i++) {
+            if (i == days[j]) {
+                int a = costs[0] + (i >= 1 ? f[i - 1] : 0);
+                int b = costs[1] + (i >= 7 ? f[i - 7] : 0);
+                int c = costs[2] + (i >= 30 ? f[i - 30] : 0);
+                f[i] = Math.min(a, Math.min(b, c));
+                j++;
+            } else {
+                f[i] = f[i - 1];
+            }
+        }
+        return f[n];
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/n983/DynamicProgrammingTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.n983;
+
+class DynamicProgrammingTest extends CodeTest {
+
+    @Override
+    Code getCode() {
+        return new DynamicProgramming();
+    }
+
+}

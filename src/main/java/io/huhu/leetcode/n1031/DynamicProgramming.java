aa936bb2d7643cf44bbbcc44aee463d8481ef1d8
NoahChen <noah.chen@batechworks.com>
2024-11-07 17:09:12 +0800

    1031. Maximum Sum of Two Non-Overlapping Subarrays alg: Dynamic Programming opt: Reduce Loops

--- src/main/java/io/huhu/leetcode/n1031/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1031/DynamicProgramming.java
@@ -9,20 +9,11 @@ class DynamicProgramming implements Code {
         for (int i = 0; i < nums.length; i++) {
             preSum[i + 1] = preSum[i] + nums[i];
         }
-        return Math.max(f(preSum, firstLen, secondLen), f(preSum, secondLen, firstLen));
-    }
-
-    /**
-     * 假设b的区间为[i-secondLen,i), 则b区间的和为perSum[i]-preSum[i-secondLen]
-     * 由于左侧需要给a预留空间, 所以i>=firstLen+secondLen,
-     * 得到a的区间为[i-firstLen-secondLen,i-secondLen),
-     * 则a区间的和为 perSum[i-firstLen]-perSum[i-firstLen-secondLen]
-     */
-    private int f(int[] sum, int len1, int len2) {
-        int maxA = 0, ans = 0;
-        for (int i = len1 + len2; i < sum.length; i++) {
-            maxA = Math.max(maxA, sum[i - len2] - sum[i - len2 - len1]);
-            ans = Math.max(ans, maxA + sum[i] - sum[i - len2]);
+        int maxA = 0, maxB = 0, ans = 0;
+        for (int i = firstLen + secondLen; i <= n; i++) {
+            maxA = Math.max(maxA, preSum[i - secondLen] - preSum[i - secondLen - firstLen]);
+            maxB = Math.max(maxB, preSum[i - firstLen] - preSum[i - secondLen - firstLen]);
+            ans = Math.max(ans, Math.max(maxA + preSum[i] - preSum[i - secondLen], maxB + preSum[i] - preSum[i - firstLen]));
         }
         return ans;
     }

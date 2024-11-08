693709b0897e319f931a64739bd0008f13cc8a88
NoahChen <noah.chen@batechworks.com>
2024-11-08 08:40:12 +0800

    1035. Uncrossed Lines alg: Depth First Search opt: Memoization

--- src/main/java/io/huhu/leetcode/n1035/DepthFirstSearch.java
+++ src/main/java/io/huhu/leetcode/n1035/DepthFirstSearch.java
@@ -1,23 +1,32 @@
 package io.huhu.leetcode.n1035;
 
+import java.util.Arrays;
+
 public class DepthFirstSearch implements Code {
 
     @Override
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        return f(nums1, nums2, nums1.length - 1, nums2.length - 1);
+        int m = nums1.length, n = nums2.length;
+        int[][] dp = new int[m][n];
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+        return f(nums1, nums2, dp, m - 1, n - 1);
     }
 
-    private int f(int[] a, int[] b, int i, int j) {
+    private int f(int[] a, int[] b, int[][] dp, int i, int j) {
         if (i < 0 || j < 0) {
             return 0;
         }
-        int ans;
-        if (a[i] == b[j]) {
-            ans = f(a, b, i - 1, j - 1) + 1;
-        } else {
-            ans = Math.max(f(a, b, i - 1, j), f(a, b, i, j - 1));
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
-        return ans;
+        if (a[i] == b[j]) {
+            dp[i][j] = f(a, b, dp, i - 1, j - 1) + 1;
+        } else {
+            dp[i][j] = Math.max(f(a, b, dp, i - 1, j), f(a, b, dp, i, j - 1));
+        }
+        return dp[i][j];
     }
 
 }

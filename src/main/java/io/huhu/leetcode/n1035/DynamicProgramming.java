76d6f7633615c7c1f369549a00c665ac78d15f72
NoahChen <noah.chen@batechworks.com>
2024-11-08 08:54:56 +0800

    1035. Uncrossed Lines alg: Dynamic Programming opt: Reduce Dimensions

--- src/main/java/io/huhu/leetcode/n1035/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1035/DynamicProgramming.java
@@ -5,17 +5,20 @@ class DynamicProgramming implements Code {
     @Override
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         int m = nums1.length, n = nums2.length;
-        int[][] dp = new int[m + 1][n + 1];
+        int[] dp = new int[n + 1];
         for (int i = 1; i <= m; i++) {
+            int leftUp = dp[0];
             for (int j = 1; j <= n; j++) {
+                int _leftUp = dp[j];
                 if (nums1[i - 1] == nums2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[j] = leftUp + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
                 }
+                leftUp = _leftUp;
             }
         }
-        return dp[m][n];
+        return dp[n];
     }
 
 }

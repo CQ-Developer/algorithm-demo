1dc503d7b9c9bdb680ceaea80e89c9d794c62fac
NoahChen <noah.chen@batechworks.com>
2024-11-11 09:11:46 +0800

    1043. Partition Array of Maximum Sum alg: Depth First Search opt: Memoization

--- src/main/java/io/huhu/leetcode/n1043/DepthFirstSearch.java
+++ src/main/java/io/huhu/leetcode/n1043/DepthFirstSearch.java
@@ -1,19 +1,30 @@
 package io.huhu.leetcode.n1043;
 
+import java.util.Arrays;
+
 class DepthFirstSearch implements Code {
 
     @Override
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        return f(arr, k, arr.length - 1);
+        int n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return f(arr, k, dp, n - 1);
     }
 
-    private int f(int[] arr, int k, int i) {
+    private int f(int[] arr, int k, int[] dp, int i) {
+        if (i < 0) {
+            return 0;
+        }
+        if (dp[i] != -1) {
+            return dp[i];
+        }
         int ans = 0, max = 0;
         for (int j = i; j >= 0 && i - j + 1 <= k; j--) {
             max = Math.max(max, arr[j]);
-            ans = Math.max(ans, max * (i - j + 1) + f(arr, k, j - 1));
+            ans = Math.max(ans, max * (i - j + 1) + f(arr, k, dp, j - 1));
         }
-        return ans;
+        return dp[i] = ans;
     }
 
 }

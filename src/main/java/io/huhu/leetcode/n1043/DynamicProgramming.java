132c5a0d7c493e2d512aca254f9af6203f9e1371
NoahChen <noah.chen@batechworks.com>
2024-11-11 09:56:49 +0800

    1043. Partition Array of Maximum Sum alg: Dynamic Programming opt: Reduce Space

--- src/main/java/io/huhu/leetcode/n1043/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1043/DynamicProgramming.java
@@ -5,16 +5,16 @@ class DynamicProgramming implements Code {
     @Override
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n = arr.length;
-        int[] f = new int[n + 1];
+        int[] f = new int[k];
         for (int i = 0; i < n; i++) {
             int ans = 0, max = 0;
             for (int j = i; j >= 0 && j > i - k; j--) {
                 max = Math.max(max, arr[j]);
-                ans = Math.max(ans, max * (i - j + 1) + f[j]);
+                ans = Math.max(ans, max * (i - j + 1) + f[j % k]);
             }
-            f[i + 1] = ans;
+            f[(i + 1) % k] = ans;
         }
-        return f[n];
+        return f[n % k];
     }
 
 }

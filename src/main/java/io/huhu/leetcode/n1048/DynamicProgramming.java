c2ae2ae020b8967e31c3fd7e78b2e685fb649ce8
NoahChen <noah.chen@batechworks.com>
2024-11-11 14:54:04 +0800

    1048. Longest String Chain alg: Dynamic Programming ps: find pre range and check

--- src/main/java/io/huhu/leetcode/n1048/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1048/DynamicProgramming.java
@@ -1,26 +1,51 @@
 package io.huhu.leetcode.n1048;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import io.huhu.AC;
 
+import java.util.Arrays;
+
+@AC
 class DynamicProgramming implements Code {
 
     @Override
     public int longestStrChain(String[] words) {
         Arrays.sort(words, (a, b) -> a.length() - b.length());
-        Map<String, Integer> map = HashMap.newHashMap(words.length);
-        int ans = 0;
-        for (String word : words) {
-            int max = 0;
-            for (int i = 0; i < word.length(); i++) {
-                String p = word.substring(0, i) + word.substring(i + 1);
-                max = Math.max(max, map.getOrDefault(p, 0));
+        int n = words.length, ans = 0;
+        int[] f = new int[n];
+        for (int i = 0, l = 0; i < n; i++) {
+            f[i] = 1;
+            while (l < i && words[l].length() + 1 < words[i].length()) {
+                l++;
             }
-            map.put(word, max + 1);
-            ans = Math.max(ans, max + 1);
+            int r = l;
+            while (r < i && words[r].length() + 1 == words[i].length()) {
+                r++;
+            }
+            for (int j = l; j < r; j++) {
+                if (isChain(words[j], words[i])) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            ans = Math.max(ans, f[i]);
         }
         return ans;
+    }
+
+    private boolean isChain(String pre, String cur) {
+        int i = 0;
+        while (i < pre.length()) {
+            if (pre.charAt(i) != cur.charAt(i)) {
+                break;
+            }
+            i++;
+        }
+        while (i < pre.length()) {
+            if (pre.charAt(i) != cur.charAt(i + 1)) {
+                return false;
+            }
+            i++;
+        }
+        return true;
     }
 
 }

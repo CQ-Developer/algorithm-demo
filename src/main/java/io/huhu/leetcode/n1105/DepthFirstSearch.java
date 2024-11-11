563236a772236811a91d18b78fa2732be9449839
chen <18551681083@163.com>
2024-11-12 00:15:47 +0800

    1105. Filling Bookcase Shelves alg: Depth First Search opt: Memoization

--- src/main/java/io/huhu/leetcode/n1105/DepthFirstSearch.java
+++ src/main/java/io/huhu/leetcode/n1105/DepthFirstSearch.java
@@ -1,21 +1,26 @@
 package io.huhu.leetcode.n1105;
 
-import io.huhu.TLE;
+import io.huhu.AC;
 
-@TLE
+import java.util.Arrays;
+
+@AC
 class DepthFirstSearch implements Code {
 
     @Override
     public int minHeightShelves(int[][] books, int shelfWidth) {
-        return f(books, shelfWidth, books.length - 1);
+        int n = books.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return f(books, shelfWidth, dp, n - 1);
     }
 
-    /**
-     * f(i)表示以第i本数结尾的书架的整体最小高度
-     */
-    private int f(int[][] books, int width, int i) {
+    private int f(int[][] books, int width, int[] dp, int i) {
         if (i < 0) {
             return 0;
+        }
+        if (dp[i] != -1) {
+            return dp[i];
         }
         int ans = Integer.MAX_VALUE;
         int w = 0, h = 0;
@@ -25,9 +30,9 @@ class DepthFirstSearch implements Code {
                 break;
             }
             h = Math.max(h, books[j][1]);
-            ans = Math.min(ans, f(books, width, j - 1) + h);
+            ans = Math.min(ans, f(books, width, dp, j - 1) + h);
         }
-        return ans;
+        return dp[i] = ans;
     }
 
 }

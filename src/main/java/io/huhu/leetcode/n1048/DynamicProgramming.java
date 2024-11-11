eca49c4b52cc6fe82e67a97e3cd7d8abe380cbbf
NoahChen <noah.chen@batechworks.com>
2024-11-11 14:40:12 +0800

    1048. Longest String Chain alg: Dynamic Programming ps: find pre string's longest chain length

--- src/main/java/io/huhu/leetcode/n1048/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1048/DynamicProgramming.java
@@ -1,42 +1,26 @@
 package io.huhu.leetcode.n1048;
 
-import io.huhu.TLE;
-
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-@TLE
 class DynamicProgramming implements Code {
 
     @Override
     public int longestStrChain(String[] words) {
         Arrays.sort(words, (a, b) -> a.length() - b.length());
-        int ans = 1;
-        int n = words.length;
-        int[] f = new int[n];
-        Arrays.fill(f, 1);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (words[j].length() + 1 == words[i].length() && check(words[j], words[i])) {
-                    f[i] = Math.max(f[i], f[j] + 1);
-                }
+        Map<String, Integer> map = HashMap.newHashMap(words.length);
+        int ans = 0;
+        for (String word : words) {
+            int max = 0;
+            for (int i = 0; i < word.length(); i++) {
+                String p = word.substring(0, i) + word.substring(i + 1);
+                max = Math.max(max, map.getOrDefault(p, 0));
             }
-            ans = Math.max(ans, f[i]);
+            map.put(word, max + 1);
+            ans = Math.max(ans, max + 1);
         }
         return ans;
-    }
-
-    private boolean check(String w1, String w2) {
-        return f(w1, w2, 0, 0) == 1;
-    }
-
-    private int f(String w1, String w2, int i, int j) {
-        if (i == w1.length() || j == w2.length()) {
-            return w1.length() - i + w2.length() - j;
-        }
-        if (w1.charAt(i) == w2.charAt(j)) {
-            return f(w1, w2, i + 1, j + 1);
-        }
-        return Math.min(f(w1, w2, i + 1, j), f(w1, w2, i, j + 1)) + 1;
     }
 
 }

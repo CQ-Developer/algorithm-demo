13f584dc59734db6703d01dcc474e6ffab45900c
NoahChen <noah.chen@batechworks.com>
2024-11-12 11:15:13 +0800

    1130. Minimum Cost Tree From Leaf Values

---
+++ src/main/java/io/huhu/leetcode/n1130/Code.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.n1130;
+
+/**
+ * <a href="https://leetcode.cn/problems/minimum-cost-tree-from-leaf-values/description/">
+ * Minimum Cost Tree From Leaf Values - 叶值的最小代价生成树
+ * </a>
+ */
+interface Code {
+
+    int mctFromLeafValues(int[] arr);
+
+}

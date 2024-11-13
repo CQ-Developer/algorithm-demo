ee5b4dfc271297f7220719fe2a90ac6ae2dcbd5b
chen <18551681083@163.com>
2024-11-13 19:21:44 +0800

    1143. Longest Common Subsequence alg: dynamic programming opt: reduce dimension

--- src/main/java/io/huhu/leetcode/n1143/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1143/DynamicProgramming.java
@@ -9,17 +9,20 @@ class DynamicProgramming implements Code {
     public int longestCommonSubsequence(String text1, String text2) {
         char[] c1 = text1.toCharArray(), c2 = text2.toCharArray();
         int m = c1.length, n = c2.length;
-        int[][] f = new int[m + 1][n + 1];
+        int[] f = new int[n + 1];
         for (int i = m - 1; i >= 0; i--) {
+            int rightDown = 0;
             for (int j = n - 1; j >= 0; j--) {
+                int _rightDown = f[j];
                 if (c1[i] == c2[j]) {
-                    f[i][j] = f[i + 1][j + 1] + 1;
+                    f[j] = rightDown + 1;
                 } else {
-                    f[i][j] = Math.max(f[i + 1][j], f[i][j + 1]);
+                    f[j] = Math.max(f[j], f[j + 1]);
                 }
+                rightDown = _rightDown;
             }
         }
-        return f[0][0];
+        return f[0];
     }
 
 }

27d0349ad873778e779d10504c9630f1389a96f0
NoahChen <noah.chen@batechworks.com>
2024-11-14 18:20:48 +0800

    1162. As Far from Land as Possible alg: dynamic programming opt: reduce loop

--- src/main/java/io/huhu/leetcode/n1162/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1162/DynamicProgramming.java
@@ -2,6 +2,8 @@ package io.huhu.leetcode.n1162;
 
 class DynamicProgramming implements Code {
 
+    private static final int INF = Integer.MAX_VALUE - 1;
+
     @Override
     public int maxDistance(int[][] grid) {
         int m = grid.length, n = grid[0].length;
@@ -9,7 +11,7 @@ class DynamicProgramming implements Code {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0) {
-                    f[i][j] = Integer.MAX_VALUE - 1;
+                    f[i][j] = INF;
                 }
             }
         }
@@ -25,6 +27,7 @@ class DynamicProgramming implements Code {
                 }
             }
         }
+        int ans = -1;
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
                 if (grid[i][j] == 0) {
@@ -34,14 +37,9 @@ class DynamicProgramming implements Code {
                     if (j + 1 < n) {
                         f[i][j] = Math.min(f[i][j], f[i][j + 1] + 1);
                     }
-                }
-            }
-        }
-        int ans = -1;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0 && f[i][j] != Integer.MAX_VALUE - 1) {
-                    ans = Math.max(ans, f[i][j]);
+                    if (f[i][j] != INF) {
+                        ans = Math.max(ans, f[i][j]);
+                    }
                 }
             }
         }

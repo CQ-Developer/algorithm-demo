5beb874de7a66c001c09ece72b6f2c0065ad7b93
chen <18551681083@163.com>
2024-11-16 18:10:38 +0800

    stickers to spell word

---
+++ src/main/java/io/huhu/breadth/first/search/n691/Code.java
@@ -0,0 +1,12 @@
+package io.huhu.breadth.first.search.n691;
+
+/**
+ * <a href="https://leetcode.cn/problems/stickers-to-spell-word/description/">
+ * stickers to spell word - 贴纸拼词
+ * </a>
+ */
+interface Code {
+
+    int minStickers(String[] stickers, String target);
+
+}

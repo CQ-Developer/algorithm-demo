d6948c956b5a547259795b2f960d55efcfc0f907
NoahChen <noah.chen@batechworks.com>
2024-11-19 17:44:16 +0800

    1262. greatest sum divisible by three alg: dynamic programming opt: reduce dimension

--- src/main/java/io/huhu/leetcode/n1262/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1262/DynamicProgramming.java
@@ -8,14 +8,14 @@ class DynamicProgramming implements Code {
     @Override
     public int maxSumDivThree(int[] nums) {
         int n = nums.length;
-        int[][] f = new int[n + 3][3];
+        int[][] f = new int[2][3];
         f[0][1] = f[0][2] = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
-            for (int j = 2; j >= 0; j--) {
-                f[i + 1][j] = Math.max(f[i][j], f[i][(j + nums[i]) % 3] + nums[i]);
+            for (int j = 0; j < 3; j++) {
+                f[(i + 1) % 2][j] = Math.max(f[i % 2][j], f[i % 2][(j + nums[i]) % 3] + nums[i]);
             }
         }
-        return f[n][0];
+        return f[n % 2][0];
     }
 
 }

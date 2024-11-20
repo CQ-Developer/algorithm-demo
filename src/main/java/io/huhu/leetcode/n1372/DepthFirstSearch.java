8dc65bd42777328c855e0f452818944edc9e9fbf
chen <18551681083@163.com>
2024-11-20 22:27:31 +0800

    nothing here.

--- src/main/java/io/huhu/leetcode/n1372/DepthFirstSearch.java
+++ src/main/java/io/huhu/leetcode/n1372/DepthFirstSearch.java
@@ -10,15 +10,19 @@ class DepthFirstSearch implements Code {
         return f(root, 0) - 1;
     }
 
+    /**
+     * dir = 0: left or right
+     * dir = 1: left
+     * dir = 2: right
+     */
     private int f(TreeNode node, int dir) {
         if (node == null) {
             return 0;
         }
         int ans;
         if (dir == 0) {
-            int a = Math.max(f(node.left, 2), f(node.right, 1)) + 1;
-            int b = Math.max(a, Math.max(f(node.left, 0), f(node.right, 0)));
-            ans = Math.max(a, b);
+            ans = Math.max(f(node.left, 2), f(node.right, 1)) + 1;
+            ans = Math.max(ans, Math.max(f(node.left, 0), f(node.right, 0)));
         } else {
             ans = dir == 1 ? f(node.left, 2) + 1 : f(node.right, 1) + 1;
         }

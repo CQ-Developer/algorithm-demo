6ea79e0c50872441013fcb21d82befdaa7e878d4
NoahChen <noah.chen@batechworks.com>
2024-11-20 13:53:13 +0800

    1334. find the city with the smallest number of neighbors at a threshold distance alg: dijkstra opt: reduce space and loop

--- src/main/java/io/huhu/leetcode/n1334/Dijkstra.java
+++ src/main/java/io/huhu/leetcode/n1334/Dijkstra.java
@@ -2,6 +2,8 @@ package io.huhu.leetcode.n1334;
 
 import io.huhu.AC;
 
+import java.util.Arrays;
+
 @AC
 class Dijkstra implements Code {
 
@@ -9,41 +11,41 @@ class Dijkstra implements Code {
 
     @Override
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        boolean[][] visited = new boolean[n][n];
-        int[][] distances = new int[n][n], graph = new int[n][n];
+        int[][] graph = new int[n][n];
         // setup
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                distances[i][j] = graph[i][j] = INF;
-            }
+        for (int[] _g : graph) {
+            Arrays.fill(_g, INF);
         }
         // build graph
         for (int[] edge : edges) {
             int f = edge[0], t = edge[1], d = edge[2];
             graph[f][t] = graph[t][f] = d;
         }
+        // setup ans
+        int cnt = INF, ans = -1;
         // dijkstra
-        for (int i = 0; i < n; i++) {
-            distances[i][i] = 0;
+        int[] distance = new int[n];
+        boolean[] visited = new boolean[n];
+        for (int i = n - 1; i >= 0; i--) {
+            Arrays.fill(distance, INF);
+            distance[i] = 0;
             for (int j = 0; j < n; j++) {
                 int t = -1;
                 for (int k = 0; k < n; k++) {
-                    if (!visited[i][k] && (t == -1 || distances[i][k] < distances[i][t])) {
+                    if (!visited[k] && (t == -1 || distance[k] < distance[t])) {
                         t = k;
                     }
                 }
-                visited[i][t] = true;
+                visited[t] = true;
                 for (int k = 0; k < n; k++) {
-                    distances[i][k] = Math.min(distances[i][k], distances[i][t] + graph[t][k]);
+                    distance[k] = Math.min(distance[k], distance[t] + graph[t][k]);
                 }
             }
-        }
-        // find the least reachable city with the biggest number
-        int cnt = INF, ans = -1;
-        for (int i = n - 1; i >= 0; i--) {
+            Arrays.fill(visited, false);
+            // find the least reachable city with the biggest number
             int _cnt = 0;
             for (int j = 0; j < n; j++) {
-                if (distances[i][j] <= distanceThreshold) {
+                if (distance[j] <= distanceThreshold) {
                     _cnt++;
                 }
             }

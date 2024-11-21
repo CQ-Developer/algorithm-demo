3ad2f2a098f583e1fe770b2a3d837b28131b0276
NoahChen <noah.chen@batechworks.com>
2024-11-21 10:49:03 +0800

    1387. sort integers by the power value alg: depth first search opt: increase performance

--- src/main/java/io/huhu/leetcode/n1387/DepthFirstSearch.java
+++ src/main/java/io/huhu/leetcode/n1387/DepthFirstSearch.java
@@ -1,27 +1,30 @@
 package io.huhu.leetcode.n1387;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 class DepthFirstSearch implements Code {
 
     private static final Map<Integer, Integer> MEM = new HashMap<>();
 
-    @Override
-    public int getKth(int lo, int hi, int k) {
-        List<Integer> nums = new ArrayList<>();
-        for (int i = lo; i <= hi; i++) {
-            nums.add(i);
+    private record Info(int num, int wei) implements Comparable<Info> {
+
+        @Override
+        public int compareTo(Info other) {
+            return this.wei == other.wei ? this.num - other.num : this.wei - other.wei;
         }
-        nums.sort(this::compare);
-        return nums.get(k - 1);
+
     }
 
-    private int compare(int a, int b) {
-        int _a = f(a), _b = f(b);
-        return _a == _b ? a - b : _a - _b;
+    @Override
+    public int getKth(int lo, int hi, int k) {
+        Info[] arr = new Info[hi - lo + 1];
+        for (int i = 0, j = lo; j <= hi; i++, j++) {
+            arr[i] = new Info(j, f(j));
+        }
+        Arrays.sort(arr);
+        return arr[k - 1].num();
     }
 
     private int f(int num) {
@@ -31,7 +34,7 @@ class DepthFirstSearch implements Code {
         if (MEM.containsKey(num)) {
             return MEM.get(num);
         }
-        int ans = num % 2 == 0 ? f(num / 2) + 1 : f(num * 3 + 1) + 1;
+        int ans = (num & 1) == 0 ? f(num >> 1) + 1 : f(num * 3 + 1) + 1;
         MEM.put(num, ans);
         return ans;
     }

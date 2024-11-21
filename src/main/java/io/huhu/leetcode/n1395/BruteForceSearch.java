c0438bf28a5795258482d6d1fa40b77922217b11
NoahChen <noah.chen@batechworks.com>
2024-11-21 13:12:30 +0800

    1395. count number of teams alg: brute force search

---
+++ src/main/java/io/huhu/leetcode/n1395/BruteForceSearch.java
@@ -0,0 +1,24 @@
+package io.huhu.leetcode.n1395;
+
+class BruteForceSearch implements Code {
+
+    @Override
+    public int numTeams(int[] rating) {
+        int ans = 0;
+        int n = rating.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (rating[i] < rating[j] && rating[j] < rating[k]) {
+                        ans++;
+                    }
+                    if (rating[i] > rating[j] && rating[j] > rating[k]) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+}

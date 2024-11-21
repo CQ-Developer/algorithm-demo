ce4383e70e2505e9abe5093a6051fbcb6a53acc5
NoahChen <noah.chen@batechworks.com>
2024-11-21 08:54:53 +0800

    1387. sort integers by the power value alg: simulation

---
+++ src/main/java/io/huhu/leetcode/n1387/Simulation.java
@@ -0,0 +1,22 @@
+package io.huhu.leetcode.n1387;
+
+import java.util.Arrays;
+
+class Simulation implements Code {
+
+    @Override
+    public int getKth(int lo, int hi, int k) {
+        int[][] arr = new int[hi - lo + 1][];
+        for (int i = 0, num = lo; num <= hi; i++, num++) {
+            int _num = num, step = 0;
+            while (_num > 1) {
+                _num = _num % 2 == 0 ? _num / 2 : _num * 3 + 1;
+                step++;
+            }
+            arr[i] = new int[]{num, step};
+        }
+        Arrays.sort(arr, (a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+        return arr[k - 1][0];
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/n1387/SimulationTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.n1387;
+
+class SimulationTest extends CodeTest {
+
+    @Override
+    Code getCode() {
+        return new Simulation();
+    }
+
+}

511ec50555aca4bc7b2be2bf5dc9f257dd529a9e
chen <18551681083@163.com>
2024-11-22 23:36:10 +0800

    1524. number of sub-arrays with odd sum

---
+++ src/main/java/io/huhu/leetcode/n1524/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1524;
+
+/**
+ * <a href="https://leetcode.cn/problems/number-of-sub-arrays-with-odd-sum/description/">
+ * number of sub-arrays with odd sum
+ * 和为奇数的子数组数目
+ * </a>
+ */
+interface Code {
+
+    int numOfSubarrays(int[] arr);
+
+}

7eecd26552ac663f41cc7f8a5ad491443d9856c6
NoahChen <noah.chen@batechworks.com>
2024-11-22 11:45:00 +0800

    1504. count submatrices with all ones alg: simulation opt: reduce loop

--- src/main/java/io/huhu/leetcode/n1504/Simulation.java
+++ src/main/java/io/huhu/leetcode/n1504/Simulation.java
@@ -7,20 +7,20 @@ class Simulation implements Code {
 
     @Override
     public int numSubmat(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
+        int ans = 0, m = mat.length, n = mat[0].length;
         int[][] widths = new int[m][n];
         for (int i = 0; i < m; i++) {
-            widths[i][0] = mat[i][0];
-            for (int j = 1; j < n; j++) {
-                widths[i][j] = mat[i][j] == 0 ? 0 : widths[i][j - 1] + 1;
-            }
-        }
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int width = widths[i][j];
-                for (int k = i; k >= 0 && width != 0; k--) {
-                    ans += width = Math.min(width, widths[k][j]);
+                // this acts like dynamic programming
+                if (j == 0) {
+                    widths[i][j] = mat[i][j];
+                } else {
+                    widths[i][j] = mat[i][j] == 0 ? 0 : widths[i][j - 1] + 1;
+                }
+                // find the narrowest till the top
+                int width = n;
+                for (int _i = i; _i >= 0 && width != 0; _i--) {
+                    ans += width = Math.min(width, widths[_i][j]);
                 }
             }
         }

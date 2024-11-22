6309cbbe4f92e350e0e6330d8a8f8acaa08e49c2
NoahChen <noah.chen@batechworks.com>
2024-11-22 09:07:27 +0800

    1493. longest subarray of 1's after deleting one element alg: dynamic programming opt: reduce loop

--- src/main/java/io/huhu/leetcode/n1493/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1493/DynamicProgramming.java
@@ -4,29 +4,25 @@ class DynamicProgramming implements Code {
 
     /**
      * pre[i]表示以i位置结尾的连续1的最长子数组长度
-     * suf[i]表示以i位置开始的连续1的最长子数组长度
+     * oneDeletionPre[i]表示以i位置结尾且可以删除一个0后得到的连续1的最长子数组
      */
     @Override
     public int longestSubarray(int[] nums) {
-        int n = nums.length;
-        // dp from head
-        int[] pre = new int[n];
-        pre[0] = nums[0];
+        int n = nums.length, ans = 0;
+        int[] pre = new int[n], oneDeletionPre = new int[n];
+        pre[0] = oneDeletionPre[0] = nums[0];
         for (int i = 1; i < n; i++) {
-            pre[i] = nums[i] == 1 ? pre[i - 1] + 1 : 0;
+            if (nums[i] == 1) {
+                pre[i] = pre[i - 1] + 1;
+                oneDeletionPre[i] = oneDeletionPre[i - 1] + 1;
+            } else {
+                pre[i] = 0;
+                oneDeletionPre[i] = pre[i - 1];
+            }
+            ans = Math.max(ans, oneDeletionPre[i]);
         }
-        // dp from end
-        int[] suf = new int[n];
-        suf[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suf[i] = nums[i] == 1 ? suf[i + 1] + 1 : 0;
-        }
-        // searching where to delete
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int _pre = i == 0 ? 0 : pre[i - 1];
-            int _suf = i == n - 1 ? 0 : suf[i + 1];
-            ans = Math.max(ans, _pre + _suf);
+        if (ans == n) {
+            ans--;
         }
         return ans;
     }

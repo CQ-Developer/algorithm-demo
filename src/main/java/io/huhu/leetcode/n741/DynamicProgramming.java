863855e13163a42c5beff52309759135484f7539
NoahChen <noah.chen@batechworks.com>
2024-11-25 10:10:23 +0800

    714. cherry pickup alg: dynamic programming opt: pre-init

--- src/main/java/io/huhu/leetcode/n741/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n741/DynamicProgramming.java
@@ -9,13 +9,28 @@ class DynamicProgramming implements Code {
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
         int[][][] f = new int[n + 1][n + 1][n + 1];
-        for (int a = n; a >= 0; a--) {
-            for (int b = n; b >= 0; b--) {
-                for (int c = n; c >= 0; c--) {
+        for (int b = 0; b <= n; b++) {
+            for (int c = 0; c <= n; c++) {
+                f[n][b][c] = -1;
+            }
+        }
+        for (int a = 0; a <= n; a++) {
+            for (int c = 0; c <= n; c++) {
+                f[a][n][c] = -1;
+            }
+        }
+        for (int a = 0; a <= n; a++) {
+            for (int b = 0; b <= n; b++) {
+                f[a][b][n] = -1;
+            }
+        }
+        for (int a = n - 1; a >= 0; a--) {
+            for (int b = n - 1; b >= 0; b--) {
+                for (int c = n - 1; c >= 0; c--) {
                     int d = a + b - c;
                     if (d >= 0 && d <= n) {
                         int ans;
-                        if (a == n || b == n || c == n || d == n || grid[a][b] == -1 || grid[c][d] == -1) {
+                        if (d == n || grid[a][b] == -1 || grid[c][d] == -1) {
                             ans = -1;
                         } else if (a == n - 1 && b == n - 1) {
                             ans = grid[a][b];

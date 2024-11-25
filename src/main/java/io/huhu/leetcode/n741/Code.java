0afe9b5f21828dbb97e3d2ed28f5c83240022314
NoahChen <noah.chen@batechworks.com>
2024-11-25 08:42:19 +0800

    714. cherry pickup

---
+++ src/main/java/io/huhu/leetcode/n741/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n741;
+
+/**
+ * <a href="https://leetcode.cn/problems/cherry-pickup/description/">
+ * cherry pickup
+ * 摘樱桃
+ * </a>
+ */
+interface Code {
+
+    int cherryPickup(int[][] grid);
+
+}

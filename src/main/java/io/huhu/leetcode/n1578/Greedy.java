d2b5f77a7feb3f25a1681b2f894adf27db50b3ca
NoahChen <noah.chen@batechworks.com>
2024-11-26 15:47:34 +0800

    1578. minimum time to make rope colorful alg: greedy

---
+++ src/main/java/io/huhu/leetcode/n1578/Greedy.java
@@ -0,0 +1,25 @@
+package io.huhu.leetcode.n1578;
+
+class Greedy implements Code {
+
+    /**
+     * 找出连续相同的颜色的气球且只留下移除代价最高的一个
+     */
+    @Override
+    public int minCost(String colors, int[] neededTime) {
+        char[] cs = colors.toCharArray();
+        int i = 0, n = cs.length, ans = 0;
+        while (i < n) {
+            char c = cs[i];
+            int sum = 0, max = 0;
+            while (i < n && cs[i] == c) {
+                sum += neededTime[i];
+                max = Math.max(max, neededTime[i]);
+                i++;
+            }
+            ans += sum - max;
+        }
+        return ans;
+    }
+
+}
---
+++ src/test/java/io/huhu/leetcode/n1578/GreedyTest.java
@@ -0,0 +1,10 @@
+package io.huhu.leetcode.n1578;
+
+class GreedyTest extends CodeTest {
+
+    @Override
+    Code getCode() {
+        return new Greedy();
+    }
+
+}

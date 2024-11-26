0e012e8123e6f7415b568b4d931823a1e0684068
NoahChen <noah.chen@batechworks.com>
2024-11-26 14:22:20 +0800

    1567. maximum length of subarray with positive product alg: dynamic programming opt: reduce dimension

--- src/main/java/io/huhu/leetcode/n1567/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1567/DynamicProgramming.java
@@ -7,19 +7,21 @@ class DynamicProgramming implements Code {
 
     @Override
     public int getMaxLen(int[] nums) {
-        int n = nums.length;
-        int[] positive = new int[n], negative = new int[n];
-        int ans = positive[0] = nums[0] > 0 ? 1 : 0;
-        negative[0] = nums[0] < 0 ? 1 : 0;
-        for (int i = 1; i < n; i++) {
+        int p = nums[0] > 0 ? 1 : 0;
+        int n = nums[0] < 0 ? 1 : 0;
+        int ans = p;
+        for (int i = 1; i < nums.length; i++) {
             if (nums[i] > 0) {
-                positive[i] = positive[i - 1] + 1;
-                negative[i] = negative[i - 1] == 0 ? 0 : negative[i - 1] + 1;
+                p++;
+                n = n == 0 ? 0 : n + 1;
             } else if (nums[i] < 0) {
-                negative[i] = positive[i - 1] + 1;
-                positive[i] = negative[i - 1] == 0 ? 0 : negative[i - 1] + 1;
+                int _n = n;
+                n = p + 1;
+                p = _n == 0 ? 0 : _n + 1;
+            } else {
+                p = n = 0;
             }
-            ans = Math.max(ans, positive[i]);
+            ans = Math.max(ans, p);
         }
         return ans;
     }

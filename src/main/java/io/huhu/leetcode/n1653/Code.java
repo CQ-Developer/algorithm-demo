0401ca35bee4ed1871ded7ba17d603640b2406ab
NoahChen <noah.chen@batechworks.com>
2024-11-27 16:56:35 +0800

    1653. minimum deletions to make string balanced

---
+++ src/main/java/io/huhu/leetcode/n1653/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1653;
+
+/**
+ * <a href="https://leetcode.cn/problems/minimum-deletions-to-make-string-balanced/description/">
+ * minimum deletions to make string balanced
+ * 使字符串平衡的最少删除次数
+ * </a>
+ */
+interface Code {
+
+    int minimumDeletions(String s);
+
+}

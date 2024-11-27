ed140fcfe2a2281de6c71b27bc34deec4244b843
NoahChen <noah.chen@batechworks.com>
2024-11-27 08:31:47 +0800

    1621. number of sets of k non-overlapping line segments

---
+++ src/main/java/io/huhu/leetcode/n1621/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1621;
+
+/**
+ * <a href="https://leetcode.cn/problems/number-of-sets-of-k-non-overlapping-line-segments/description/">
+ * number of sets of k non-overlapping line segments
+ * 大小为k的不重叠线段的数目
+ * </a>
+ */
+interface Code {
+
+    int numberOfSets(int n, int k);
+
+}

d1a3d235a32b4699bc0c404d555873856c3ab94b
NoahChen <noah.chen@batechworks.com>
2024-11-29 08:20:41 +0800

    1749. maximum absolute sum of any subarray

---
+++ src/main/java/io/huhu/leetcode/n1749/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1749;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-absolute-sum-of-any-subarray/description/">
+ * maximum absolute sum of any subarray
+ * 任意子数组和的绝对值的最大值
+ * </a>
+ */
+interface Code {
+
+    int maxAbsoluteSum(int[] nums);
+
+}

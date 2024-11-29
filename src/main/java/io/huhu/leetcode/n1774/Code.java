16059902a27a41da5914715a28e1f0dd21f61034
NoahChen <noah.chen@batechworks.com>
2024-11-29 09:03:48 +0800

    1774. closest dessert cost

---
+++ src/main/java/io/huhu/leetcode/n1774/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1774;
+
+/**
+ * <a href="https://leetcode.cn/problems/closest-dessert-cost/description/">
+ * closest dessert cost
+ * 最接近目标价格的甜点成本
+ * </a>
+ */
+interface Code {
+
+    int closestCost(int[] baseCosts, int[] toppingCosts, int target);
+
+}

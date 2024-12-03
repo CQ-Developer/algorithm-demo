0b652bc2e7e7790e9b375a5cf932821d956ac52b
NoahChen <noah.chen@batechworks.com>
2024-12-03 16:22:38 +0800

    1824. minimum sideway jumps alg: dynamic programming opt: reduce dimension

--- src/main/java/io/huhu/leetcode/n1824/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1824/DynamicProgramming.java
@@ -1,7 +1,5 @@
 package io.huhu.leetcode.n1824;
 
-import java.util.Arrays;
-
 class DynamicProgramming implements Code {
 
     private static final int INF = 0x3f3f3f3f;
@@ -9,33 +7,28 @@ class DynamicProgramming implements Code {
     @Override
     public int minSideJumps(int[] obstacles) {
         int n = obstacles.length;
-        int[][] f = new int[n][3];
-        for (int[] _f : f) {
-            Arrays.fill(_f, INF);
-        }
-        f[0][1] = 0;
-        f[0][0] = f[0][2] = 1;
+        int f0 = 1, f1 = 0, f2 = 1;
         for (int i = 1; i < n; i++) {
+            if (obstacles[i] == 1) {
+                f0 = INF;
+            }
+            if (obstacles[i] == 2) {
+                f1 = INF;
+            }
+            if (obstacles[i] == 3) {
+                f2 = INF;
+            }
             if (obstacles[i] != 1) {
-                f[i][0] = f[i - 1][0];
+                f0 = Math.min(f0, Math.min(f1, f2) + 1);
             }
             if (obstacles[i] != 2) {
-                f[i][1] = f[i - 1][1];
+                f1 = Math.min(f1, Math.min(f0, f2) + 1);
             }
             if (obstacles[i] != 3) {
-                f[i][2] = f[i - 1][2];
-            }
-            if (obstacles[i] != 1) {
-                f[i][0] = Math.min(f[i][0], Math.min(f[i][1], f[i][2]) + 1);
-            }
-            if (obstacles[i] != 2) {
-                f[i][1] = Math.min(f[i][1], Math.min(f[i][0], f[i][2]) + 1);
-            }
-            if (obstacles[i] != 3) {
-                f[i][2] = Math.min(f[i][2], Math.min(f[i][0], f[i][1]) + 1);
+                f2 = Math.min(f2, Math.min(f0, f1) + 1);
             }
         }
-        return Math.min(f[n - 1][0], Math.min(f[n - 1][1], f[n - 1][2]));
+        return Math.min(f0, Math.min(f1, f2));
     }
 
 }

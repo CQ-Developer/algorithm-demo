9921e323a928fd5af74432a97bbc7fed94ad02a8
NoahChen <noah.chen@batechworks.com>
2024-12-03 17:13:02 +0800

    1871. jump game VII

---
+++ src/main/java/io/huhu/leetcode/n1871/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1871;
+
+/**
+ * <a href="https://leetcode.cn/problems/jump-game-vii/description/">
+ * jump game VII
+ * 跳跃游戏VII
+ * </a>
+ */
+interface Code {
+
+    boolean canReach(String s, int minJump, int maxJump);
+
+}
---
+++ src/test/java/io/huhu/leetcode/n1871/CodeTest.java
@@ -0,0 +1,36 @@
+package io.huhu.leetcode.n1871;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+abstract class CodeTest {
+
+    Code code;
+
+    abstract Code getCode();
+
+    @BeforeEach
+    void setup() {
+        code = getCode();
+    }
+
+    @ParameterizedTest
+    @MethodSource("params")
+    void test(String s, int minJump, int maxJump, boolean ans) {
+        assertEquals(ans, code.canReach(s, minJump, maxJump));
+    }
+
+    static Stream<Arguments> params() {
+        return Stream.of(
+                arguments("011010", 2, 3, true),
+                arguments("01101110", 2, 3, false));
+    }
+
+}

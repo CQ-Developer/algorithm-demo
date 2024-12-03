5af6eb6c6372cd2dbe64408591068121ea6149d5
NoahChen <noah.chen@batechworks.com>
2024-12-03 16:19:16 +0800

    1824. minimum sideway jumps alg: dynamic programming

---
+++ src/main/java/io/huhu/leetcode/n1824/DynamicProgramming.java
@@ -0,0 +1,41 @@
+package io.huhu.leetcode.n1824;
+
+import java.util.Arrays;
+
+class DynamicProgramming implements Code {
+
+    private static final int INF = 0x3f3f3f3f;
+
+    @Override
+    public int minSideJumps(int[] obstacles) {
+        int n = obstacles.length;
+        int[][] f = new int[n][3];
+        for (int[] _f : f) {
+            Arrays.fill(_f, INF);
+        }
+        f[0][1] = 0;
+        f[0][0] = f[0][2] = 1;
+        for (int i = 1; i < n; i++) {
+            if (obstacles[i] != 1) {
+                f[i][0] = f[i - 1][0];
+            }
+            if (obstacles[i] != 2) {
+                f[i][1] = f[i - 1][1];
+            }
+            if (obstacles[i] != 3) {
+                f[i][2] = f[i - 1][2];
+            }
+            if (obstacles[i] != 1) {
+                f[i][0] = Math.min(f[i][0], Math.min(f[i][1], f[i][2]) + 1);
+            }
+            if (obstacles[i] != 2) {
+                f[i][1] = Math.min(f[i][1], Math.min(f[i][0], f[i][2]) + 1);
+            }
+            if (obstacles[i] != 3) {
+                f[i][2] = Math.min(f[i][2], Math.min(f[i][0], f[i][1]) + 1);
+            }
+        }
+        return Math.min(f[n - 1][0], Math.min(f[n - 1][1], f[n - 1][2]));
+    }
+
+}

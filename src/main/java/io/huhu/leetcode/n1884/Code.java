d9ea9b1d843ae4030c034a6e616e39c9f6a975c4
NoahChen <noah.chen@batechworks.com>
2024-12-04 10:23:13 +0800

    1884. egg drop with 2 eggs and n floors

---
+++ src/main/java/io/huhu/leetcode/n1884/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1884;
+
+/**
+ * <a href="https://leetcode.cn/problems/egg-drop-with-2-eggs-and-n-floors/description/">
+ * egg drop with 2 eggs and n floors
+ * 鸡蛋掉落-两枚鸡蛋
+ * </a>
+ */
+interface Code {
+
+    int twoEggDrop(int n);
+
+}

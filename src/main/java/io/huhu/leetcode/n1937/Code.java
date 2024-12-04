95e62e1171695a996b983ace022c635a6ae28fe4
NoahChen <noah.chen@batechworks.com>
2024-12-04 16:54:43 +0800

    1937. maximum number of points with cost

---
+++ src/main/java/io/huhu/leetcode/n1937/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1937;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-number-of-points-with-cost/description/">
+ * maximum number of points with cost
+ * 扣分后的最大得分
+ * </a>
+ */
+interface Code {
+
+    long maxPoints(int[][] points);
+
+}
---
+++ src/test/java/io/huhu/leetcode/n1937/CodeTest.java
@@ -0,0 +1,36 @@
+package io.huhu.leetcode.n1937;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+abstract class CodeTest {
+
+    Code code;
+
+    abstract Code getCode();
+
+    @BeforeEach
+    void setup() {
+        code = getCode();
+    }
+
+    @ParameterizedTest
+    @MethodSource("params")
+    void test(int[][] points, long res) {
+        assertEquals(res, code.maxPoints(points));
+    }
+
+    static Stream<Arguments> params() {
+        return Stream.of(
+                arguments(new int[][]{{1, 2, 3}, {1, 5, 1}, {3, 1, 1}}, 9),
+                arguments(new int[][]{{1, 5}, {2, 3}, {4, 2}}, 11));
+    }
+
+}

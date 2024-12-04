3d1edd135f91626e63f3c65b2a697360350a4a10
NoahChen <noah.chen@batechworks.com>
2024-12-04 16:27:13 +0800

    1911. maximum alternating subsequence sum alg: dynamic programming opt: reduce dimension

--- src/main/java/io/huhu/leetcode/n1911/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n1911/DynamicProgramming.java
@@ -7,17 +7,13 @@ class DynamicProgramming implements Code {
 
     @Override
     public long maxAlternatingSum(int[] nums) {
-        int n = nums.length;
-        // f[i]: 从前i个元素中选出子序列, 且最后一个元素为奇数下标, 最大交替和
-        long[] f = new long[n + 1];
-        // g[i]: 从前i个元素中选出子序列, 且最后一个元素为偶数下标, 最大交替和
-        long[] g = new long[n + 1];
-        // dp
-        for (int i = 0; i < n; i++) {
-            f[i + 1] = Math.max(f[i], g[i] - nums[i]);
-            g[i + 1] = Math.max(g[i], f[i] + nums[i]);
+        long f = 0, g = 0;
+        for (int num : nums) {
+            long _f = f;
+            f = Math.max(f, g - num);
+            g = Math.max(g, _f + num);
         }
-        return Math.max(f[n], g[n]);
+        return Math.max(f, g);
     }
 
 }

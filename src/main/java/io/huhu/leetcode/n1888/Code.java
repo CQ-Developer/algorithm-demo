7e542040d19448a084389205824a9346396ef12b
NoahChen <noah.chen@batechworks.com>
2024-12-04 11:16:52 +0800

    1888. minimum number of flips to make the binary string alternating

---
+++ src/main/java/io/huhu/leetcode/n1888/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1888;
+
+/**
+ * <a href="https://leetcode.cn/problems/minimum-number-of-flips-to-make-the-binary-string-alternating/description/">
+ * minimum number of flips to make the binary string alternating
+ * 使二进制字符串交替的最少反转次数
+ * </a>
+ */
+interface Code {
+
+    int minFlips(String s);
+
+}

32b497baf5713331cd25b6fdd24a7d30573adfd0
NoahChen <noah.chen@batechworks.com>
2024-12-05 11:22:03 +0800

    1947. maximum compatibility score sum

---
+++ src/main/java/io/huhu/leetcode/n1947/Code.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1947;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-compatibility-score-sum/description/">
+ * maximum compatibility score sum
+ * 最大兼容性评分和
+ * </a>
+ */
+interface Code {
+
+    int maxCompatibilitySum(int[][] students, int[][] mentors);
+
+}

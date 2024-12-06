8f49571c6e75118925b5ac02b7ed056cde8fe5bf
NoahChen <noah.chen@batechworks.com>
2024-12-06 08:25:01 +0800

    1976. numbers of ways to arrive at destination

---
+++ src/main/java/io/huhu/leetcode/n1976/Solution.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n1976;
+
+/**
+ * <a href="https://leetcode.cn/problems/number-of-ways-to-arrive-at-destination/description/">
+ * number of ways to arrive at destination
+ * 到达目的地的方案数
+ * </a>
+ */
+interface Solution {
+
+    int countPaths(int n, int[][] roads);
+
+}
---
+++ src/test/java/io/huhu/leetcode/n1976/SolutionTest.java
@@ -0,0 +1,36 @@
+package io.huhu.leetcode.n1976;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+abstract class SolutionTest {
+
+    Solution solution;
+
+    abstract Solution getSolution();
+
+    @BeforeEach
+    void setup() {
+        solution = getSolution();
+    }
+
+    @ParameterizedTest
+    @MethodSource("params")
+    void test(int n, int[][] roads, int res) {
+        assertEquals(res, solution.countPaths(n, roads));
+    }
+
+    static Stream<Arguments> params() {
+        return Stream.of(
+                arguments(7, new int[][]{{0, 6, 7}, {0, 1, 2}, {1, 2, 3}, {1, 3, 3}, {6, 3, 3}, {3, 5, 1}, {6, 5, 1}, {2, 5, 1}, {0, 4, 5}, {4, 6, 2}}, 4),
+                arguments(2, new int[][]{{1, 0, 10}}, 1));
+    }
+
+}

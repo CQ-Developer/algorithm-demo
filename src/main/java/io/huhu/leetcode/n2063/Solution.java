0e27fb02f51e9f809e960205e0066a7df3dc0dab
NoahChen <noah.chen@batechworks.com>
2024-12-11 08:43:24 +0800

    2063. vowels of all substrings

---
+++ src/main/java/io/huhu/leetcode/n2063/Solution.java
@@ -0,0 +1,13 @@
+package io.huhu.leetcode.n2063;
+
+/**
+ * <a href="https://leetcode.cn/problems/vowels-of-all-substrings/description/">
+ * vowels of all substrings
+ * 所有字符串中的元音
+ * </a>
+ */
+interface Solution {
+
+    long countVowels(String word);
+
+}

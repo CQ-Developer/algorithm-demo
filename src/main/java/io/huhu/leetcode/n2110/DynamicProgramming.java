dc62648314b4d0b83020ae6898d26dd1531bb393
NoahChen <noah.chen@batechworks.com>
2024-12-13 10:36:39 +0800

    2110. number of smooth descent periods of a stack alg: dynamic programming opt: reduce dimension

--- src/main/java/io/huhu/leetcode/n2110/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n2110/DynamicProgramming.java
@@ -2,22 +2,20 @@ package io.huhu.leetcode.n2110;
 
 import io.huhu.AC;
 
-import java.util.Arrays;
-
 @AC
 class DynamicProgramming implements Solution {
 
     @Override
     public long getDescentPeriods(int[] prices) {
-        int n = prices.length;
-        int[] f = new int[n];
-        Arrays.fill(f, 1);
         long res = 1;
-        for (int i = 1; i < n; i++) {
+        int pre = 1;
+        for (int i = 1; i < prices.length; i++) {
             if (prices[i - 1] == prices[i] + 1) {
-                f[i] += f[i - 1];
+                pre++;
+            } else {
+                pre = 1;
             }
-            res += f[i];
+            res += pre;
         }
         return res;
     }

10bec2cbd4f9b56c601f5e65d8a6f5d654fa2efc
NoahChen <noah.chen@batechworks.com>
2024-12-16 08:39:09 +0800

    2222. number of ways to select buildings

---
+++ src/main/java/io/huhu/leetcode/n2222/Solution.java
@@ -0,0 +1,12 @@
+package io.huhu.leetcode.n2222;
+
+/**
+ * <a href="https://leetcode.cn/problems/number-of-ways-to-select-buildings/description/">
+ * number of ways to select buildings
+ * </a>
+ */
+interface Solution {
+
+    long numberOfWays(String s);
+
+}

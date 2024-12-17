9a3ef960568821466727741c4f11cbaeb629d3af
NoahChen <noah.chen@batechworks.com>
2024-12-17 11:35:22 +0800

    2304. minimum path cost in a grid alg: dynamic programming opg: O(1) space

--- src/main/java/io/huhu/leetcode/n2304/DynamicProgramming.java
+++ src/main/java/io/huhu/leetcode/n2304/DynamicProgramming.java
@@ -8,19 +8,18 @@ class DynamicProgramming implements Solution {
     @Override
     public int minPathCost(int[][] grid, int[][] moveCost) {
         int m = grid.length, n = grid[0].length;
-        int[][] f = new int[m][n];
-        f[m - 1] = grid[m - 1];
         for (int i = m - 2; i >= 0; i--) {
             for (int j = 0; j < n; j++) {
-                f[i][j] = Integer.MAX_VALUE;
+                int[] cost = moveCost[grid[i][j]];
+                int res = Integer.MAX_VALUE;
                 for (int k = 0; k < n; k++) {
-                    f[i][j] = Math.min(f[i][j], f[i + 1][k] + moveCost[grid[i][j]][k]);
+                    res = Math.min(res, grid[i + 1][k] + cost[k]);
                 }
-                f[i][j] += grid[i][j];
+                grid[i][j] += res;
             }
         }
         int res = Integer.MAX_VALUE;
-        for (int i : f[0]) {
+        for (int i : grid[0]) {
             res = Math.min(res, i);
         }
         return res;
